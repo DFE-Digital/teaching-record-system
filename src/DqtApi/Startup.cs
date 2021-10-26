b9ab5fe62c18ac8945f7a907e2da11849c2da788
James Gunn <james@gunn.io>
2021-10-26 12:32:40 +0100

    Add CRM ServiceClient to services

--- src/DqtApi/Startup.cs
+++ src/DqtApi/Startup.cs
@@ -1,14 +1,24 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.PowerPlatform.Dataverse.Client;
 
 namespace DqtApi
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -17,6 +27,8 @@ namespace DqtApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo() { Title = "DQT API", Version = "v1" });
             });
+
+            services.AddSingleton<ServiceClient>(GetCrmServiceClient());
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -48,5 +60,12 @@ namespace DqtApi
                 });
             }
         }
+
+        private ServiceClient GetCrmServiceClient() =>
+            new ServiceClient(
+                new Uri(Configuration["CrmUrl"]),
+                Configuration["CrmClientId"],
+                Configuration["CrmClientSecret"],
+                useUniqueInstance: true);
     }
 }
