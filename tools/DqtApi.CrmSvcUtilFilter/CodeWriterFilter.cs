3e0a721175af4a72b3691a090f380a13b0de6b55
James Gunn <james@gunn.io>
2021-10-29 15:00:48 +0100

    Limit the entities to generate proxy classes for
    For now, only generate a type for the account entity.
    
    (Inspired by
    http://erikpool.blogspot.com/2011/03/filtering-generated-entities-with.html)

---
+++ tools/DqtApi.CrmSvcUtilFilter/CodeWriterFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+using Microsoft.Crm.Services.Utility;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace DqtApi.CrmSvcUtilFilter
+{
+    public class CodeWriterFilter : ICodeWriterFilterService
+    {
+        private readonly ICodeWriterFilterService _defaultService;
+        private readonly HashSet<string> _validEntities;
+
+        public CodeWriterFilter(ICodeWriterFilterService defaultService)
+        {
+            _defaultService = defaultService;
+            _validEntities = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            LoadFilterData();
+        }
+
+        public bool GenerateAttribute(AttributeMetadata attributeMetadata, IServiceProvider services) =>
+            _defaultService.GenerateAttribute(attributeMetadata, services);
+
+        public bool GenerateEntity(EntityMetadata entityMetadata, IServiceProvider services) =>
+            _validEntities.Contains(entityMetadata.LogicalName);
+
+        public bool GenerateOption(OptionMetadata optionMetadata, IServiceProvider services) =>
+            _defaultService.GenerateOption(optionMetadata, services);
+
+        public bool GenerateOptionSet(OptionSetMetadataBase optionSetMetadata, IServiceProvider services) =>
+            _defaultService.GenerateOptionSet(optionSetMetadata, services);
+
+        public bool GenerateRelationship(RelationshipMetadataBase relationshipMetadata, EntityMetadata otherEntityMetadata, IServiceProvider services) =>
+            _defaultService.GenerateRelationship(relationshipMetadata, otherEntityMetadata, services);
+
+        public bool GenerateServiceContext(IServiceProvider services) =>
+            _defaultService.GenerateServiceContext(services);
+
+        private void LoadFilterData()
+        {
+            var filterFileLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "filter.xml");
+
+            XElement xml = XElement.Load(filterFileLocation);
+            XElement entitiesElement = xml.Element("entities");
+
+            foreach (XElement entityElement in entitiesElement.Elements("entity"))
+            {
+                _validEntities.Add(entityElement.Value.ToLowerInvariant());
+            }
+        }
+    }
+}
