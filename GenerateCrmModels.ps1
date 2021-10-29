2b4f4fe595b36dfc5a7b37e7de33a57138ac8fdc
James Gunn <james@gunn.io>
2021-10-29 11:58:52 +0100

    Add generated model types from CRM
    Also adds a script - GenerateCrmModels.ps1 - to (re)generate the file.

---
+++ GenerateCrmModels.ps1
@@ -0,0 +1,33 @@
+$ErrorActionPreference = "Stop"
+
+$userSecretsId = "DqtApi"
+
+function Get-UserSecret {
+    [CmdletBinding()]
+    param (
+        [string]$Key
+    )
+
+    $secrets = dotnet user-secrets --id $userSecretsId list
+
+    $value = $secrets -match "^${Key}" -split " = " | Select-Object -Skip 1
+
+    if ($null -eq $value) {
+        Write-Error "No secret found with key ${Key}"
+    }
+
+    return $value
+}
+
+$crmUrl = Get-UserSecret "CrmUrl"
+$crmClientId = Get-UserSecret "CrmClientId"
+$crmClientSecret = Get-UserSecret "CrmClientSecret"
+
+$connectionString = "AuthType=ClientSecret;url=${crmUrl};ClientId=${crmClientId};ClientSecret=${crmClientSecret}"
+
+$namespace = "DqtApi.Models"
+$output = Join-Path -Path $PSScriptRoot -ChildPath src DqtApi Models GeneratedCode.cs
+mkdir (Split-Path $output) -Force | Out-Null
+
+$crmSvcUtil = Join-Path -Path $PSScriptRoot -ChildPath tools coretools "CrmSvcUtil.exe"
+& $crmSvcUtil /connectionstring:${connectionString} /out:${output} /namespace:${namespace} /emitfieldsclasses
