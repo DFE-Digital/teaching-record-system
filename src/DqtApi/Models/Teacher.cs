95430bf4f1fc9fe747cff772b268fc8a824089d3
Jonathan Wacks <jonathan@wacksworld.co.uk>
2021-10-29 14:07:01 +0100

    Added more fields - need to check actual attribute names in Dataverse though

--- src/DqtApi/Models/Teacher.cs
+++ src/DqtApi/Models/Teacher.cs
@@ -3,8 +3,16 @@ using System.Text.Json.Serialization;
 using Microsoft.Xrm.Sdk;
 
 namespace DqtApi.Models {
+
     public class Teacher {
         private Entity _entity;
+
+        // TODO fix attribute names - don't think these are currently right
+        public string Trn => _entity.GetAttributeValue<string>("dfetn_trn");
+
+        public string Name => _entity.GetAttributeValue<string>("fullname");
+
+        public string NationalInsuranceNumber => _entity.GetAttributeValue<string>("dfetn_nino");
         // TODO formatting through model binder?
         public string DateOfBirth => _entity.GetAttributeValue<DateTime>("dateofbirth").ToString();
 
