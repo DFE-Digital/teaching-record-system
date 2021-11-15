fbb0a3a68c4d95c60eff2dcd2f84a031d77525a4
James Gunn <james@gunn.io>
2021-11-15 18:33:21 +0000

    Add configuration source that retrieves data from a JSON-encoded env var

---
+++ src/DqtApi/Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DqtApi.Configuration
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddJsonEnvironmentVariable(
+            this IConfigurationBuilder builder,
+            string environmentVariableName,
+            string configurationKeyPrefix = null)
+        {
+            if (builder is null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (string.IsNullOrEmpty(environmentVariableName))
+            {
+                throw new ArgumentException("Environment variable name must be specified.", nameof(environmentVariableName));
+            }
+
+            return builder.Add(new EnvironmentVariableJsonConfigurationSource()
+            {
+                EnvironmentVariableName = environmentVariableName,
+                ConfigurationKeyPrefix = configurationKeyPrefix
+            });
+        }
+    }
+}
