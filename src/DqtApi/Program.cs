c340b241431bb4ced157fe804164f625b45479a8
James Gunn <james@gunn.io>
2021-12-10 13:10:20 +0000

    Tidy generated schema names for generated CRM model types

--- src/DqtApi/Program.cs
+++ src/DqtApi/Program.cs
@@ -79,6 +79,20 @@ namespace DqtApi
                 c.EnableAnnotations();
                 c.OperationFilter<ResponseContentTypeOperationFilter>();
 
+                c.CustomSchemaIds(type =>
+                {
+                    // Generated CRM models for custom entities have a weird type name; fix that up here
+                    // e.g. for the 'dfeta_inductionState' type use 'InductionState' in the API spec
+
+                    if (type.Name.StartsWith("dfeta_"))
+                    {
+                        var prefixTrimmedTypeName = type.Name.Substring("dfeta_".Length);
+                        return prefixTrimmedTypeName[0..1].ToUpper() + prefixTrimmedTypeName[1..];
+                    }
+
+                    return type.Name;
+                });
+
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
                     In = ParameterLocation.Header,
