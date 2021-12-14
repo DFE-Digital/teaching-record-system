3a11d2b7427ae14117595a6ccf9c756c2e26ac3e
James Gunn <james@gunn.io>
2021-12-14 10:50:26 +0000

    Add helper methods on ApiTestBase for working with DbContext

--- tests/DqtApi.Tests/ApiTestBase.cs
+++ tests/DqtApi.Tests/ApiTestBase.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using DqtApi.DataStore.Sql;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace DqtApi.Tests
@@ -29,5 +31,19 @@ namespace DqtApi.Tests
         public virtual Task DisposeAsync() => Task.CompletedTask;
 
         public virtual Task InitializeAsync() => ApiFixture.DbHelper.ClearData();
+
+        public virtual async Task<T> WithDbContext<T>(Func<DqtContext, Task<T>> action)
+        {
+            await using var scope = ApiFixture.Services.GetRequiredService<IServiceScopeFactory>().CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<DqtContext>();
+            return await action(dbContext);
+        }
+
+        public virtual Task WithDbContext(Func<DqtContext, Task> action) =>
+            WithDbContext(async dbContext =>
+            {
+                await action(dbContext);
+                return 0;
+            });
     }
 }
