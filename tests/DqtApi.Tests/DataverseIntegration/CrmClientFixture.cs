750cd2a945f750f92a1073bd088cf21a43bfecc4
James Gunn <james@gunn.io>
2021-12-23 11:35:20 +0000

    Rename existing DataverseAdaptor tests to GetMatchingTeachers
    Also separates out the methods that get a CRM ServiceClient so we can
    re-use in other tests.

---
+++ tests/DqtApi.Tests/DataverseIntegration/CrmClientFixture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DqtApi.DAL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+namespace DqtApi.Tests.DataverseIntegration
+{
+    public class CrmClientFixture : IDisposable
+    {
+        public CrmClientFixture()
+        {
+            Configuration = GetConfiguration();
+            ServiceClient = GetCrmServiceClient();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public ServiceClient ServiceClient { get; }
+
+        public DataverseAdaptor CreateDataverseAdaptor() => new(ServiceClient);
+
+        public virtual void Dispose()
+        {
+            ServiceClient.Dispose();
+        }
+
+        private static IConfiguration GetConfiguration() =>
+            new ConfigurationBuilder()
+                .AddUserSecrets<CrmClientFixture>(optional: true)
+                .AddEnvironmentVariables("IntegrationTests_")
+                .Build();
+
+        private ServiceClient GetCrmServiceClient() =>
+            new(
+                new Uri(Configuration["CrmUrl"]),
+                Configuration["CrmClientId"],
+                Configuration["CrmClientSecret"],
+                useUniqueInstance: true);
+    }
+}
