7293304883f31250967287b0a85f34da6e68b5e9
James Gunn <james@gunn.io>
2021-12-29 09:55:21 +0000

    Deactivate records created in integration tests when done

--- tests/DqtApi.Tests/DataverseIntegration/CrmClientFixture.cs
+++ tests/DqtApi.Tests/DataverseIntegration/CrmClientFixture.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using DqtApi.DAL;
+using DqtApi.Models;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Xunit;
 
 namespace DqtApi.Tests.DataverseIntegration
 {
-    public class CrmClientFixture : IDisposable
+    public class CrmClientFixture : IDisposable, IAsyncLifetime
     {
+        private readonly List<(string EntityName, Guid EntityId)> _createdEntities;
+
         public CrmClientFixture()
         {
+            _createdEntities = new();
             Configuration = GetConfiguration();
             ServiceClient = GetCrmServiceClient();
         }
@@ -19,9 +29,38 @@ namespace DqtApi.Tests.DataverseIntegration
 
         public DataverseAdaptor CreateDataverseAdaptor() => new(ServiceClient);
 
+        public Task InitializeAsync() => Task.CompletedTask;
+
         public virtual void Dispose()
         {
             ServiceClient.Dispose();
+        }
+
+        public Task DisposeAsync() => Task.WhenAll(_createdEntities.Select(e =>
+        {
+            var deactivateRequest = new SetStateRequest()
+            {
+                EntityMoniker = new EntityReference(e.EntityName, e.EntityId),
+                State = new OptionSetValue((int)ContactState.Inactive),
+                Status = new OptionSetValue(2)
+            };
+
+            return ServiceClient.ExecuteAsync(deactivateRequest);
+        }));
+
+        public void RegisterForCleanup(Entity entity)
+        {
+            if (entity.Id == Guid.Empty)
+            {
+                throw new ArgumentException("Entity ID is not set.", nameof(entity));
+            }
+
+            _createdEntities.Add((entity.LogicalName, entity.Id));
+        }
+
+        public void RegisterForCleanup(string entityName, Guid entityId)
+        {
+            _createdEntities.Add((entityName, entityId));
         }
 
         private static IConfiguration GetConfiguration() =>
