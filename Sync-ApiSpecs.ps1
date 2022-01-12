ed0c4c58e581e39fa6cabb8d84554527a2a9ee21
James Gunn <james@gunn.io>
2022-01-12 14:33:54 +0000

    Add a Powershell script to download API specs to the docs folder

---
+++ Sync-ApiSpecs.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+param(
+    [Parameter(Position = 1)]
+    [String]$BaseUrl = "http://localhost:30473"
+)
+
+$ErrorActionPreference = "Stop"
+
+$versions = @("v1", "v2")
+$output = Join-Path $PSScriptRoot "docs" "api-specs"
+
+New-Item $output -ItemType Directory -ErrorAction SilentlyContinue
+
+foreach ($version in $versions) {
+    Invoke-WebRequest -Uri "${BaseUrl}/swagger/${version}/swagger.json" -OutFile (Join-Path $output "${version}.json")
+}
