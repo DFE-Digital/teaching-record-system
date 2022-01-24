315316442b5a176333b3411bc52ef1b02c4842d9
James Gunn <james@gunn.io>
2022-01-24 11:24:14 +0000

    Add Today property to IClock abstraction

--- tests/DqtApi.Tests/TestableClock.cs
+++ tests/DqtApi.Tests/TestableClock.cs
@@ -7,6 +7,7 @@ namespace DqtApi.Tests
         public static DateTime Initial => new(2021, 1, 4);  // Arbitary start date
 
         public DateTime UtcNow { get; set; } = Initial;
+        public DateOnly Today => DateOnly.FromDateTime(UtcNow);
 
         public void Reset() => UtcNow = Initial;
     }
