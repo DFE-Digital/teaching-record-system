c807c53ec7226c7697deee388574c9c8b6a04e1d
James Gunn <james@gunn.io>
2022-02-08 12:33:42 +0000

    Update date model binder to support DateOnly
    Also fix the error message when the value cannot be parsed.

---
+++ src/DqtApi/ModelBinding/DateModelBinder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace DqtApi.ModelBinding
+{
+    public class DateModelBinder : IModelBinder
+    {       
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var modelName = bindingContext.ModelName;
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            var modelState = bindingContext.ModelState;
+            modelState.SetModelValue(modelName, valueProviderResult);
+
+            var metadata = bindingContext.ModelMetadata;
+            var type = metadata.UnderlyingOrModelType;
+
+            try
+            {
+                var value = valueProviderResult.FirstValue;
+
+                object model;
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    model = null;
+                }
+                else if (type == typeof(DateTime))
+                {
+                    model = DateTime.ParseExact(value, Constants.DateFormat, provider: null);
+                }
+                else if (type == typeof(DateOnly))
+                {
+                    model = DateOnly.ParseExact(value, Constants.DateFormat);
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+
+                if (model == null && !metadata.IsReferenceOrNullableType)
+                {
+                    modelState.TryAddModelError(
+                        modelName,
+                        metadata.ModelBindingMessageProvider.ValueMustNotBeNullAccessor(
+                            valueProviderResult.ToString()));
+                }
+                else
+                {
+                    bindingContext.Result = ModelBindingResult.Success(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.TryAddModelError(modelName, ex, metadata);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
