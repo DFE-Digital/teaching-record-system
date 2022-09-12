db458148344a8e6ec9ca31301f4d5aa270dfa2a4
James Gunn <james@gunn.io>
2022-09-12 11:07:04 +0100

    Return whether a teacher was actually unlocked
    Also changed behavior to leave the loginfailedcounter as-is unless it's
    over the locked threshold.

--- src/DqtApi/V2/Handlers/UnlockTeacherHandler.cs
+++ src/DqtApi/V2/Handlers/UnlockTeacherHandler.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using DqtApi.DataStore.Crm;
 using DqtApi.DataStore.Crm.Models;
 using DqtApi.V2.Requests;
+using DqtApi.V2.Responses;
 using DqtApi.Validation;
 using MediatR;
 
 namespace DqtApi.V2.Handlers
 {
-    public class UnlockTeacherHandler : IRequestHandler<UnlockTeacherRequest>
+    public class UnlockTeacherHandler : IRequestHandler<UnlockTeacherRequest, UnlockTeacherResponse>
     {
         private readonly IDataverseAdapter _dataverseAdapter;
 
@@ -18,25 +18,39 @@ namespace DqtApi.V2.Handlers
             _dataverseAdapter = dataverseAdapter;
         }
 
-        public async Task<Unit> Handle(UnlockTeacherRequest request, CancellationToken cancellationToken)
+        public async Task<UnlockTeacherResponse> Handle(UnlockTeacherRequest request, CancellationToken cancellationToken)
         {
             var contact = await _dataverseAdapter.GetTeacher(request.TeacherId, columnNames: new[]
             {
                 Contact.Fields.dfeta_ActiveSanctions,
-                Contact.Fields.dfeta_TRN
+                Contact.Fields.dfeta_TRN,
+                Contact.Fields.dfeta_loginfailedcounter
             });
-            if (contact?.dfeta_ActiveSanctions == true)
-            {
-                throw new ErrorException(ErrorRegistry.TeacherHasActiveSanctions());
-            }
 
-            var found = await _dataverseAdapter.UnlockTeacherRecord(request.TeacherId);
-            if (!found)
+            if (contact is null)
             {
                 throw new NotFoundException(resourceName: Contact.EntityLogicalName, request.TeacherId);
             }
 
-            return Unit.Value;
+            if (contact.dfeta_ActiveSanctions == true)
+            {
+                throw new ErrorException(ErrorRegistry.TeacherHasActiveSanctions());
+            }
+
+            if (contact.dfeta_loginfailedcounter is null || contact.dfeta_loginfailedcounter < 3)
+            {
+                return new UnlockTeacherResponse()
+                {
+                    HasBeenUnlocked = false
+                };
+            }
+
+            await _dataverseAdapter.UnlockTeacherRecord(request.TeacherId);
+
+            return new UnlockTeacherResponse()
+            {
+                HasBeenUnlocked = true
+            };
         }
     }
 }
