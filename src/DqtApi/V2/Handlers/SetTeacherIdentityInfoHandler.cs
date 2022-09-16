4578700491a08d77215d1eed53585ff9dba1ed85
James Gunn <james@gunn.io>
2022-09-16 10:50:16 +0100

    Remove the duplicate checks around setting TsPersonId
    Identity is now the owner of this association so it will perform the
    duplicate checks.

--- src/DqtApi/V2/Handlers/SetTeacherIdentityInfoHandler.cs
+++ src/DqtApi/V2/Handlers/SetTeacherIdentityInfoHandler.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using DqtApi.DataStore.Crm;
 using DqtApi.DataStore.Crm.Models;
-using DqtApi.DataStore.Sql;
 using DqtApi.V2.Requests;
 using DqtApi.V2.Responses;
 using DqtApi.Validation;
@@ -14,23 +13,14 @@ namespace DqtApi.V2.Handlers
     public class SetTeacherIdentityInfoHandler : IRequestHandler<SetTeacherIdentityInfoRequest, TeacherIdentityInfo>
     {
         private readonly IDataverseAdapter _dataverseAdapter;
-        private readonly DqtContext _dqtContext;
 
-        public SetTeacherIdentityInfoHandler(IDataverseAdapter dataverseAdapter, DqtContext dqtContext)
+        public SetTeacherIdentityInfoHandler(IDataverseAdapter dataverseAdapter)
         {
             _dataverseAdapter = dataverseAdapter;
-            _dqtContext = dqtContext;
         }
 
         public async Task<TeacherIdentityInfo> Handle(SetTeacherIdentityInfoRequest request, CancellationToken cancellationToken)
         {
-            using var transaction = await _dqtContext.Database.BeginTransactionAsync();
-
-            // Acquire an advisory lock, scoped to the TsPersonId.
-            // This prevents multiple requests trying to assign the given TsPersonId at the same time;
-            // we need to ensure at most one record in DQT has a given TsPersonId.
-            await transaction.AcquireAdvisoryLock("SetTsPersonId", request.TsPersonId);
-
             var teachersWithTrn = await _dataverseAdapter.GetTeachersByTrn(request.Trn, columnNames: new[] { Contact.Fields.dfeta_TSPersonID });
 
             if (teachersWithTrn.Length == 0)
@@ -44,19 +34,8 @@ namespace DqtApi.V2.Handlers
 
             var teacher = teachersWithTrn.Single();
 
-            if (!string.IsNullOrEmpty(teacher.dfeta_TSPersonID) && teacher.dfeta_TSPersonID != request.TsPersonId)
-            {
-                throw new ErrorException(ErrorRegistry.TeacherAlreadyHasTsPersonIdDefined());
-            }
-
             if (teacher.dfeta_TSPersonID != request.TsPersonId)
             {
-                var existingTeacherWithTsPersonId = await _dataverseAdapter.GetTeacherByTsPersonId(request.TsPersonId);
-                if (existingTeacherWithTsPersonId != null)
-                {
-                    throw new ErrorException(ErrorRegistry.AnotherTeacherHasTheSpecifiedTsPersonIdAssigned());
-                }
-
                 await _dataverseAdapter.SetTsPersonId(teacher.Id, request.TsPersonId);
             }
 
