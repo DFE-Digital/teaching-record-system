972c433017fc9de6727ac93450dd4319637b9446
WILPOW <william.powell@softwire.com>
2023-03-24 15:17:41 +0000

    Add middle name to the V2 and V3 GET teachers endpoint

--- QualifiedTeachersApi/src/QualifiedTeachersApi/V2/Responses/GetTeacherResponse.cs
+++ QualifiedTeachersApi/src/QualifiedTeachersApi/V2/Responses/GetTeacherResponse.cs
@@ -9,6 +9,7 @@ public class GetTeacherResponse
     public string Trn { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string MiddleName { get; set; }
     public DateOnly? DateOfBirth { get; set; }
     public string NationalInsuranceNumber { get; set; }
     public bool HasActiveSanctions { get; set; }
--- QualifiedTeachersApi/tests/QualifiedTeachersApi.Tests/V2/Operations/GetTeacherTests.cs
+++ QualifiedTeachersApi/tests/QualifiedTeachersApi.Tests/V2/Operations/GetTeacherTests.cs
@@ -218,6 +218,7 @@ public class GetTeacherTests : ApiTestBase
         var birthDate = "1990-04-01";
         var firstName = Faker.Name.First();
         var lastName = Faker.Name.Last();
+        var middleName = Faker.Name.Middle();
         var nino = Faker.Identification.UkNationalInsuranceNumber();
         var qtsDate = (DateOnly?)null;
         var eytsDate = (DateOnly?)new DateOnly(2022, 7, 7);
@@ -239,6 +240,7 @@ public class GetTeacherTests : ApiTestBase
             BirthDate = DateTime.Parse(birthDate),
             FirstName = firstName,
             LastName = lastName,
+            MiddleName = middleName,
             dfeta_TRN = trn,
             dfeta_NINumber = nino,
             StateCode = ContactState.Active,
@@ -308,6 +310,7 @@ public class GetTeacherTests : ApiTestBase
                 trn = trn,
                 firstName = firstName,
                 lastName = lastName,
+                middleName = middleName,
                 dateOfBirth = birthDate,
                 nationalInsuranceNumber = nino,
                 hasActiveSanctions = false,
