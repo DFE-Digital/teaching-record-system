0bd2ea5b504a0b83e9187466189effbd4a81f4a0
WILPOW <william.powell@softwire.com>
2023-03-30 11:41:32 +0100

    Move requiest validation to validator class

--- QualifiedTeachersApi/src/QualifiedTeachersApi/V3/Requests/GetTeacherRequest.cs
+++ QualifiedTeachersApi/src/QualifiedTeachersApi/V3/Requests/GetTeacherRequest.cs
@@ -6,6 +6,5 @@ namespace QualifiedTeachersApi.V3.Requests;
 
 public record GetTeacherRequest : IRequest<GetTeacherResponse>
 {
-    [RegularExpression(@"^\d{7}$", ErrorMessage = "TRN must be 7 digits")]
     public required string Trn { get; init; }
 }
