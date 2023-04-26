aedd5cbcc7dc2492443c4b412002c43ed9e4f1cc
James Gunn <james@gunn.io>
2023-04-26 14:51:53 +0100

    Add tooling to upgrade the reporting DB schema (#493)

---
+++ QualifiedTeachersApi/src/QtCli/Commands.ResetReportingDbJournal.cs
@@ -0,0 +1,35 @@
+﻿using QualifiedTeachersApi.Services.DqtReporting;
+
+namespace QtCli;
+
+public static partial class Commands
+{
+    public static Command CreateResetReportingDbJournalCommand(IConfiguration configuration)
+    {
+        var connectionStringOption = new Option<string>("--connection-string")
+        {
+            IsRequired = true
+        };
+
+        var configuredConnectionString = configuration["DqtReporting:ReportingDbConnectionString"];
+        if (configuredConnectionString is not null)
+        {
+            connectionStringOption.SetDefaultValue(configuredConnectionString);
+        }
+
+        var migrateDbCommand = new Command("reset-reporting-db-journal", "Reset the migrations journal table to Initial.sql.")
+        {
+            connectionStringOption
+        };
+
+        migrateDbCommand.SetHandler(
+            (string connectionString) =>
+            {
+                var migrator = new Migrator(connectionString);
+                migrator.ResetJournal("Initial.sql");
+            },
+            connectionStringOption);
+
+        return migrateDbCommand;
+    }
+}
