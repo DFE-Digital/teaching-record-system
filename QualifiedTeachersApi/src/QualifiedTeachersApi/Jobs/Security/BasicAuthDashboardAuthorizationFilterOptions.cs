296c19429deae8ed617677c9be01dda97536eb17
Andrew Horth <andrew@horth.co.uk>
2023-05-16 20:53:43 +0100

    Fixed build error

--- QualifiedTeachersApi/src/QualifiedTeachersApi/Jobs/Security/BasicAuthDashboardAuthorizationFilterOptions.cs
+++ QualifiedTeachersApi/src/QualifiedTeachersApi/Jobs/Security/BasicAuthDashboardAuthorizationFilterOptions.cs
@@ -4,7 +4,7 @@ public class BasicAuthDashboardAuthorizationFilterOptions
 {
     public bool LoginCaseSensitive { get; set; }
 
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
-    public string Password { get; set; }
+    public required string Password { get; set; }
 }
