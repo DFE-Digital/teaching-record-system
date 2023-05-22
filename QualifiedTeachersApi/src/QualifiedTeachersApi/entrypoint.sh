4bc7394291271a2b62c877c561a92e5bc5c61c86
James Gunn <james@gunn.io>
2023-05-22 17:26:37 +0100

    Fix entry point script for missing config and multiple nodes

--- QualifiedTeachersApi/src/QualifiedTeachersApi/entrypoint.sh
+++ QualifiedTeachersApi/src/QualifiedTeachersApi/entrypoint.sh
@@ -4,25 +4,27 @@ set -e
 
 alias qtcli="dotnet /QtCli/QtCli.dll"
 
-if [ "$CF_INSTANCE_INDEX" == "0" ]; then
-  echo "Applying database migrations..."
-  dotnet /QtCli/QtCli.dll migrate-db --connection-string "$ConnectionStrings__DefaultConnection"
-  echo "Done applying database migrations"
-
-  REPORTING_DB_CONNSTR="$DqtReporting__ReportingDbConnectionString"
-
-  if [ -z "$REPORTING_DB_CONNSTR" ]; then
-    REPORTING_DB_CONNSTR=$(echo "$AppConfig" | jq -r '.DqtReporting.ReportingDbConnectionString')
-  fi
-
-  if [ ! -z "$REPORTING_DB_CONNSTR" ]; then
-   echo "Applying reporting database migrations..."
-   dotnet /QtCli/QtCli.dll migrate-reporting-db --connection-string "$REPORTING_DB_CONNSTR"
-   echo "Done applying reporting database migrations"
- fi
+REPORTING_DB_CONNSTR="$DqtReporting__ReportingDbConnectionString"
+if [ -z "$REPORTING_DB_CONNSTR" ]; then
+  REPORTING_DB_CONNSTR=$(echo "$AppConfig" | jq -r '.DqtReporting.ReportingDbConnectionString // empty')
 fi
 
 if [ -z "$REPORTING_DB_CONNSTR" ]; then
+  echo "Disabling DqtReportingService"
+  export DqtReporting__RunService="false"
+fi
+
+if [ "$CF_INSTANCE_INDEX" == "0" ]; then
+  echo "Applying database migrations..."
+  dotnet /QtCli/QtCli.dll migrate-db --connection-string "$ConnectionStrings__DefaultConnection"
+  echo "Done applying database migrations" 
+
+  if [ ! -z "$REPORTING_DB_CONNSTR" ]; then
+    echo "Applying reporting database migrations..."
+    dotnet /QtCli/QtCli.dll migrate-reporting-db --connection-string "$REPORTING_DB_CONNSTR"
+    echo "Done applying reporting database migrations"
+  fi
+else
   echo "Disabling DqtReportingService"
   export DqtReporting__RunService="false"
 fi
