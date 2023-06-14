f0213c9f4dfb77556105a035fbefd2fa7de925d4
Andrew Horth <andrew@horth.co.uk>
2023-06-14 14:39:59 +0100

    Allow for CORS authenticating in swagger against local identity

---
+++ QualifiedTeachersApi/src/QualifiedTeachersApi/wwwroot/docs/swagger-custom.js
@@ -0,0 +1,9 @@
+﻿window.fetch = function (fetch) {
+  return function () {
+    var req = arguments[1];
+    if (req.headers["X-Requested-With"]) {
+      delete req.headers["X-Requested-With"];
+    }
+    return fetch.apply(window, arguments);
+  };
+}(window.fetch);
