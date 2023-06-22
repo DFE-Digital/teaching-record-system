edc65589c09e58faa526b318c078ef91c494a114
aje <33416927+aje54@users.noreply.github.com>
2023-06-22 14:18:59 +0100

    spiked AD login and logout (#619)
    * spiked AD login and logout
    
    * rem sln file amend just file

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Program.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +26,17 @@ if (builder.Environment.IsProduction())
 
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+       .AddMicrosoftIdentityWebApp(builder.Configuration, "AzureAd");
+
+builder.Services.AddRazorPages().AddMvcOptions(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                  .RequireAuthenticatedUser()
+                  .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -35,6 +51,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/health", async context =>
