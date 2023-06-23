6e1141a096332a6fa3819af64b4b2a34809c808e
James Gunn <james@gunn.io>
2023-06-23 16:19:48 +0100

    Handle a removed TRN in ID webhook (#627)
    * Prevent VS trying to load tests from TestCommon project
    
    * Handle a removed TRN in ID webhook

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/Services/GetAnIdentity/WebHooks/UserUpdatedMessage.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/Services/GetAnIdentity/WebHooks/UserUpdatedMessage.cs
@@ -1,8 +1,16 @@
-﻿namespace TeachingRecordSystem.Api.Services.GetAnIdentity.WebHooks;
+﻿using Optional;
+
+namespace TeachingRecordSystem.Api.Services.GetAnIdentity.WebHooks;
 
 public record UserUpdatedMessage : INotificationMessage
 {
     public const string MessageTypeName = "UserUpdated";
 
     public required User User { get; init; }
+    public required UserUpdatedMessageChanges Changes { get; init; }
+}
+
+public record UserUpdatedMessageChanges
+{
+    public required Option<string?> Trn { get; init; }
 }
