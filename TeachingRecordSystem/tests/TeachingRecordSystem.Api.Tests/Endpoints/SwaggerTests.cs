754700284f6c3c4ce53a33a3c4c7bcbdd46f09cd
James Gunn <james@gunn.io>
2023-07-12 13:09:27 +0100

    Fix v3 swagger endpoint

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.Api.Tests/Endpoints/SwaggerTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TeachingRecordSystem.Api.Tests.Endpoints;
+
+public class SwaggerTests : ApiTestBase
+{
+    public SwaggerTests(ApiFixture apiFixture) : base(apiFixture)
+    {
+    }
+
+    [Theory]
+    [InlineData("v1")]
+    [InlineData("v2")]
+    [InlineData("v3")]
+    public async Task Get_SwaggerEndpoint_ReturnsOk(string version)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Get, $"swagger/{version}.json");
+        var httpClient = ApiFixture.CreateClient();
+
+        // Act
+        var response = await httpClient.SendAsync(request);
+
+        // Assert
+        Assert.Equal(200, (int)response.StatusCode);
+    }
+}
