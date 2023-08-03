82ce5a1832a93a88a5caa4c50af4d52f761ef544
James Gunn <james@gunn.io>
2023-08-03 15:58:22 +0100

    Add case insensitive collation

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20230803150603_CaseInsensitiveCollation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class CaseInsensitiveCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("create collation case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop collation case_insensitive;");
+        }
+    }
+}
