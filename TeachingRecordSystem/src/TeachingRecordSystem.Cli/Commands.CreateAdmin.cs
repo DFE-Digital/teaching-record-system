8199e49c3b79445baf835397f37470218f9796db
James Gunn <james@gunn.io>
2023-08-03 15:08:39 +0100

    Add CLI command for adding an admin

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Cli/Commands.CreateAdmin.cs
@@ -0,0 +1,61 @@
+﻿using TeachingRecordSystem.Core;
+using TeachingRecordSystem.Core.DataStore.Postgres;
+using TeachingRecordSystem.Core.DataStore.Postgres.Models;
+
+namespace TeachingRecordSystem.Cli;
+
+public static partial class Commands
+{
+    public static Command CreateCreateAdminCommand(IConfiguration configuration)
+    {
+        var emailOption = new Option<string>("--email") { IsRequired = true };
+        var nameOption = new Option<string>("--name") { IsRequired = true };
+        var connectionStringOption = new Option<string>("--connection-string") { IsRequired = true };
+
+        var configuredConnectionString = configuration.GetConnectionString("DefaultConnection");
+        if (configuredConnectionString is not null)
+        {
+            connectionStringOption.SetDefaultValue(configuredConnectionString);
+        }
+
+        var command = new Command("create-admin", $"Creates a new user with the {UserRoles.Administrator} role.")
+        {
+            emailOption,
+            nameOption,
+            connectionStringOption
+        };
+
+        command.AddValidator(commandResult =>
+        {
+            var email = commandResult.GetValueForOption(emailOption);
+
+            if (email?.EndsWith("@education.gov.uk", StringComparison.OrdinalIgnoreCase) != true)
+            {
+                commandResult.ErrorMessage = "Email address must be an @education.gov.uk address.";
+            }
+        });
+
+        command.SetHandler(
+            async (string email, string name, string connectionString) =>
+            {
+                using var dbContext = TrsDbContext.Create(connectionString);
+
+                dbContext.Users.Add(new()
+                {
+                    Active = true,
+                    Email = email,
+                    Name = name,
+                    Roles = new[] { UserRoles.Administrator },
+                    UserId = Guid.NewGuid(),
+                    UserType = UserType.Person
+                });
+
+                await dbContext.SaveChangesAsync();
+            },
+            emailOption,
+            nameOption,
+            connectionStringOption);
+
+        return command;
+    }
+}
