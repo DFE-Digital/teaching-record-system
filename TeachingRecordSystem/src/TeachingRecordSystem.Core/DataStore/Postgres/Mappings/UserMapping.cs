3b2c8950cf2ac622349cb7d4f9a9f66037f4c731
ihsanahmed-tpx <113981476+ihsanahmed-tpx@users.noreply.github.com>
2023-08-03 09:50:44 +0100

    added migration for default system user record (#691)
    * added migration for default system user record
    
    * added migration for default system user record
    
    * renamed method
    
    * renamed method
    
    * removed last .DS_Store

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/UserMapping.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/UserMapping.cs
@@ -15,5 +15,19 @@ public class UserMapping : IEntityTypeConfiguration<User>
         builder.Property(e => e.Email).HasMaxLength(200);
         builder.Property(e => e.AzureAdSubject).HasMaxLength(100);
         builder.Property(e => e.Roles).HasColumnType("varchar[]");
+
+        builder.HasData(GetDefaultUsers());
+    }
+
+    private static IEnumerable<User> GetDefaultUsers()
+    {
+        return new List<User>{new()
+        {
+            UserId = Guid.Parse("a81394d1-a498-46d8-af3e-e077596ab303"),
+            UserType = UserType.Application,
+            Name = "System",
+            Active = true,
+            Roles = Array.Empty<string>()
+        }};
     }
 }
