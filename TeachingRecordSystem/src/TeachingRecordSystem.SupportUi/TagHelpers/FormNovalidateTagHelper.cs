871ad629308f5b17a1f94e7fac957b6893727068
James Gunn <james@gunn.io>
2023-08-14 14:05:46 +0100

    Add Tag Helper to add novalidate attribute to forms

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/FormNovalidateTagHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TeachingRecordSystem.SupportUi.TagHelpers;
+
+[HtmlTargetElement("form")]
+public class FormNovalidateTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.Add("novalidate", string.Empty);
+    }
+}
