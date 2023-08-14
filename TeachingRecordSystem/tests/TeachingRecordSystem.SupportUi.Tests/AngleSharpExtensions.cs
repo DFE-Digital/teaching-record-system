8fc2fc62a6cd972f813d01563a52f32dcb49e1b2
James Gunn <james@gunn.io>
2023-08-14 14:54:50 +0100

    Port HTML test helpers from ID

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/AngleSharpExtensions.cs
@@ -0,0 +1,101 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace TeachingRecordSystem.SupportUi.Tests;
+
+public static class AngleSharpExtensions
+{
+    public static T? As<T>(this IElement element)
+        where T : class, IElement
+    {
+        return element as T;
+    }
+
+    public static IReadOnlyList<IElement> GetAllElementsByTestId(this IElement element, string testId) =>
+        element.QuerySelectorAll($"*[data-testid='{testId}']").ToList();
+
+    public static IElement? GetElementByTestId(this IElement element, string testId) =>
+        element.GetAllElementsByTestId(testId).SingleOrDefault();
+
+    public static IReadOnlyList<IElement> GetAllElementsByTestId(this IHtmlDocument doc, string testId) =>
+        doc.Body!.GetAllElementsByTestId(testId);
+
+    public static IElement? GetElementByLabel(this IHtmlDocument doc, string label)
+    {
+        var allLabels = doc.QuerySelectorAll("label");
+
+        foreach (var l in allLabels)
+        {
+            if (l.TextContent.Trim() == label)
+            {
+                var @for = l.GetAttribute("for");
+                return @for is not null ? doc.GetElementById(@for) : null;
+            }
+        }
+
+        return null;
+    }
+
+    public static IElement? GetElementByTestId(this IHtmlDocument doc, string testId) =>
+        doc.Body!.GetElementByTestId(testId);
+
+    public static IReadOnlyList<IElement> GetSummaryListActionsForKey(this IHtmlDocument doc, string key)
+    {
+        var row = doc.GetSummaryListRowForKey(key);
+        return row?.QuerySelectorAll(".govuk-summary-list__actions>*").ToArray() ?? Array.Empty<IElement>();
+    }
+
+    public static int GetSummaryListRowCountForKey(this IHtmlDocument doc, string key)
+    {
+        var count = 0;
+        var allRows = doc.QuerySelectorAll(".govuk-summary-list__row");
+
+        foreach (var row in allRows)
+        {
+            var rowKey = row.QuerySelector(".govuk-summary-list__key");
+
+            if (rowKey.GetCleansedTextContent() == key)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static IElement? GetSummaryListRowForKey(this IDocument doc, string key) =>
+        doc.Body?.GetSummaryListRowForKey(key);
+
+    public static IElement? GetSummaryListRowForKey(this IElement element, string key)
+    {
+        var allRows = element.QuerySelectorAll(".govuk-summary-list__row");
+
+        foreach (var row in allRows)
+        {
+            var rowKey = row.QuerySelector(".govuk-summary-list__key");
+
+            if (rowKey.GetCleansedTextContent() == key)
+            {
+                return row;
+            }
+        }
+
+        return null;
+    }
+
+    public static string? GetSummaryListValueForKey(this IDocument doc, string key) =>
+        doc.Body?.GetSummaryListValueForKey(key);
+
+    public static string? GetSummaryListValueForKey(this IElement element, string key)
+    {
+        var row = element.GetSummaryListRowForKey(key);
+        var rowValue = row?.QuerySelector(".govuk-summary-list__value");
+        return rowValue.GetCleansedTextContent();
+    }
+
+    /// <summary>
+    /// Trims whitespace from an <see cref="INode"/>'s <see cref="INode.TextContent"/> and removes any
+    /// U+00AD (&amp;shy;) characters.
+    /// </summary>
+    private static string? GetCleansedTextContent(this INode? node) => node?.TextContent?.Trim()?.Replace("\u00ad", "");
+}
---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/HttpResponseMessageExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System.Resources;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Xunit.Sdk;
+
+namespace TeachingRecordSystem.SupportUi.Tests;
+
+public static class HttpResponseMessageExtensions
+{
+    private static readonly string[] _testDataStrings = GetTestDataStrings();
+
+    public static async Task<IHtmlDocument> GetDocument(this HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+
+        var browsingContext = BrowsingContext.New(Configuration.Default);
+        var doc = (IHtmlDocument)await browsingContext.OpenAsync(req => req.Content(content));
+
+        AssertSmartQuotesUsed();
+
+        return doc;
+
+        void AssertSmartQuotesUsed()
+        {
+            VisitDocumentNodes(
+                doc,
+                node =>
+                {
+                    if (node.NodeType != NodeType.Text)
+                    {
+                        return;
+                    }
+
+                    if (node.ParentElement is IHtmlScriptElement)
+                    {
+                        return;
+                    }
+
+                    using var reader = new StringReader(node.Text());
+                    string? line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Some of our test data has non-smart quotes in it; ignore errors for that data
+                        if (_testDataStrings.Any(d => line.Contains(d)))
+                        {
+                            continue;
+                        }
+
+                        var nonSmartQuoteIndex = line.IndexOf('\'');
+                        if (nonSmartQuoteIndex != -1)
+                        {
+                            var indicatorLine = new string(' ', nonSmartQuoteIndex) + "^";
+                            var message = $"Missing smart quote:\n{line}\n{indicatorLine}";
+                            throw new XunitException(message);
+                        }
+                    }
+                });
+        }
+
+        void VisitDocumentNodes(IHtmlDocument document, Action<INode> visit)
+        {
+            VisitNode(document.DocumentElement);
+
+            void VisitNode(INode node)
+            {
+                visit(node);
+
+                foreach (var child in node.GetDescendants())
+                {
+                    visit(child);
+                }
+            }
+        }
+    }
+
+    public static async Task<HttpResponseMessage> FollowRedirect(this HttpResponseMessage response, HttpClient httpClient)
+    {
+        var statusCode = (int)response.StatusCode;
+
+        if (statusCode < 300 || statusCode > 399)
+        {
+            throw new InvalidOperationException($"Response status code is not a redirect status: {statusCode}.");
+        }
+
+        if (statusCode != StatusCodes.Status302Found)
+        {
+            throw new NotSupportedException();
+        }
+
+        var location = response.Headers.Location?.OriginalString;
+
+        if (location is null)
+        {
+            throw new InvalidOperationException("Response does not contain a Location header.");
+        }
+
+        return await httpClient.GetAsync(location);
+    }
+
+    private static string[] GetTestDataStrings()
+    {
+        var resourceManager = new ResourceManager("Faker.Resources.Name", typeof(Faker.Name).Assembly);
+        var allNames = resourceManager.GetString("First")!.Split(';', StringSplitOptions.TrimEntries)
+            .Concat(resourceManager.GetString("Last")!.Split(';', StringSplitOptions.TrimEntries));
+        return allNames.ToArray();
+    }
+}
