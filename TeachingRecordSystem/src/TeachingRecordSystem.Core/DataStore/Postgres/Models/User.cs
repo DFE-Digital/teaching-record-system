f75eaf1512b04e8b32976088e5c812d988afc601
James Gunn <james@gunn.io>
2023-08-14 12:26:14 +0100

    Use the same user ID in sign in as Graph API returns

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/User.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/User.cs
@@ -7,6 +7,6 @@ public class User
     public required UserType UserType { get; init; }
     public required string Name { get; set; }
     public string? Email { get; set; }
-    public string? AzureAdSubject { get; set; }
+    public string? AzureAdUserId { get; set; }
     public required string[] Roles { get; set; }
 }
