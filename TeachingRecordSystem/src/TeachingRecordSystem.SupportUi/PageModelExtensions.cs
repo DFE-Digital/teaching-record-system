4bef824392340701076a88a27dada4215dee1368
James Gunn <james@gunn.io>
2023-08-14 13:42:47 +0100

    Add extension method for returning a Page with a 400 response code (#724)

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/PageModelExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeachingRecordSystem.SupportUi;
+
+public static class PageModelExtensions
+{
+    public static PageResult PageWithErrors(this PageModel pageModel) => new PageResult() { StatusCode = StatusCodes.Status400BadRequest };
+}
