413bb1d41792e058bf5bdcee94057ec2cf157e79
James Gunn <james@gunn.io>
2023-08-30 18:03:36 +0100

    Add sanction codes to ReferenceDataCache

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/QueryHandlers/GetAllSanctionCodesHandler.cs
@@ -0,0 +1,29 @@
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using TeachingRecordSystem.Core.Dqt.Queries;
+
+namespace TeachingRecordSystem.Core.Dqt.QueryHandlers;
+
+public class GetAllSanctionCodesHandler : ICrmQueryHandler<GetAllSanctionCodesQuery, dfeta_sanctioncode[]>
+{
+    public async Task<dfeta_sanctioncode[]> Execute(GetAllSanctionCodesQuery query, IOrganizationServiceAsync organizationService)
+    {
+        var queryExpression = new QueryExpression()
+        {
+            EntityName = dfeta_sanctioncode.EntityLogicalName,
+            ColumnSet = new ColumnSet(
+                dfeta_sanctioncode.Fields.dfeta_sanctioncodeId,
+                dfeta_sanctioncode.Fields.dfeta_Value)
+        };
+
+        var request = new RetrieveMultipleRequest()
+        {
+            Query = queryExpression
+        };
+
+        var response = await organizationService.RetrieveMultipleAsync(queryExpression);
+
+        return response.Entities.Select(e => e.ToEntity<dfeta_sanctioncode>()).ToArray();
+    }
+}
