200b7a9265fb1170e89e61e716d7913fdd1e0e3e
Andrew Horth <andrew@horth.co.uk>
2023-09-28 17:33:32 +0100

    Add tag helper to render empty content as a hyphen

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/UseEmptyFallbackTagHelper.cs
@@ -0,0 +1,23 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TeachingRecordSystem.SupportUi.TagHelpers;
+
+[HtmlTargetElement("*", Attributes = "use-empty-fallback")]
+public class UseEmptyFallbackTagHelper : TagHelper
+{
+    public override int Order => -1;
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        var content = await output.GetChildContentAsync();
+        output.Attributes.RemoveAll("use-empty-fallback");
+
+        if (content.IsEmptyOrWhiteSpace)
+        {
+            output.AddClass("trs-subtle-emphasis", HtmlEncoder.Default);
+            output.Content.SetContent("-");
+        }
+    }
+}
