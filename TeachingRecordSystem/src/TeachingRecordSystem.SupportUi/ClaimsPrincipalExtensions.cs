a7a0cf966cabff3994ae04e9dd20d5ade62d8118
James Gunn <james@gunn.io>
2023-11-02 15:49:15 +0000

    Hide nav items for users who don't have any roles (#894)

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/ClaimsPrincipalExtensions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/ClaimsPrincipalExtensions.cs
@@ -10,4 +10,7 @@ public static class ClaimsPrincipalExtensions
 
     public static Guid GetUserId(this ClaimsPrincipal principal) =>
         Guid.Parse(principal.FindFirstValue(CustomClaims.UserId) ?? throw new InvalidOperationException("UserId claim was not found."));
+
+    public static bool IsActiveTrsUser(this ClaimsPrincipal principal) =>
+        principal.Claims.Any(c => c.Type == CustomClaims.UserId) && principal.Claims.Any(c => c.Type == ClaimTypes.Role);
 }
