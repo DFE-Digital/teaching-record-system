2930565d6ea5306b23a7ccaa59877c448728cfcf
James Gunn <james@gunn.io>
2023-11-20 15:07:39 +0000

    Add Terraform outputs for backup container/account name

---
+++ terraform/aks/outputs.tf
@@ -0,0 +1,7 @@
+output "postgres_azure_backup_storage_account_name" {
+  value = module.postgres.azure_backup_storage_account_name
+}
+
+output "postgres_azure_backup_storage_container_name" {
+  value = module.postgres.azure_backup_storage_container_name
+}
