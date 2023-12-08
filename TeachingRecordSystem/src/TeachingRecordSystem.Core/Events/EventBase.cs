dd652388418b3d9bd1f3a2e5fbd1f6bae4c5ac41
James Gunn <james@gunn.io>
2023-12-08 10:48:12 +0000

    Move event serializer options onto EventBase

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/EventBase.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/EventBase.cs
@@ -1,7 +1,11 @@
+using System.Text.Json;
+
 namespace TeachingRecordSystem.Core.Events;
 
 public abstract record EventBase
 {
+    public static JsonSerializerOptions JsonSerializerOptions { get; } = new();
+
     public required DateTime CreatedUtc { get; init; }
     public required Guid SourceUserId { get; init; }
 }
