0a50cc82a84f13951ef64f2aa0e8e51ab3e9aa3b
Andrew Horth <andrew@horth.co.uk>
2024-01-08 17:39:48 +0000

    Show timestamps in local time on Change log

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/DateTimeExtensions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/DateTimeExtensions.cs
@@ -19,6 +19,15 @@ public static class DateTimeExtensions
     public static DateOnly? ToDateOnlyWithDqtBstFix(this DateTime? dateTime, bool isLocalTime) =>
         dateTime.HasValue ? ToDateOnlyWithDqtBstFix(dateTime.Value, isLocalTime) : null;
 
-    public static DateTime? WithDqtBstFix(this DateTime? dateTime, bool isLocalTime) =>
-        dateTime.HasValue ? (isLocalTime ? TimeZoneInfo.ConvertTimeFromUtc(dateTime.Value, _gmt) : dateTime.Value) : null;
+    public static DateTime? ToUtc(this DateTime dateTime) =>
+        TimeZoneInfo.ConvertTimeToUtc(DateTime.SpecifyKind(dateTime, DateTimeKind.Unspecified), _gmt);
+
+    public static DateTime? ToUtc(this DateTime? dateTime) =>
+        dateTime.HasValue ? ToUtc(dateTime.Value) : null;
+
+    public static DateTime ToLocal(this DateTime dateTime) =>
+        TimeZoneInfo.ConvertTimeFromUtc(dateTime, _gmt);
+
+    public static DateTime? ToLocal(this DateTime? dateTime) =>
+        dateTime.HasValue ? ToLocal(dateTime.Value) : null;
 }
