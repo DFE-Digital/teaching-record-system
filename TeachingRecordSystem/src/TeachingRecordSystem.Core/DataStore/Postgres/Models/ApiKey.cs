a71e2a38da88f0a9a5a0170e6f7b1023cc9c9a25
James Gunn <james@gunn.io>
2024-01-17 16:06:33 +0000

    Shorten min length requirement for an API key

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/ApiKey.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/ApiKey.cs
@@ -5,7 +5,7 @@ public class ApiKey
     public const string KeyUniqueIndexName = "ix_api_keys_key";
 
     public const int KeyMaxLength = 100;
-    public const int KeyMinLength = 20;
+    public const int KeyMinLength = 16;
 
     public required Guid ApiKeyId { get; init; }
     public required DateTime CreatedOn { get; init; }
