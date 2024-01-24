76fad7024345a08536b797df75a98138d606c62f
James Gunn <james@gunn.io>
2024-01-24 15:02:51 +0000

    Initial setup for Authorize access project

--- TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Program.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Program.cs
@@ -1,23 +1,59 @@
 using GovUk.Frontend.AspNetCore;
+using Joonasw.AspNetCore.SecurityHeaders;
+using TeachingRecordSystem.AuthorizeAccessToATeacherRecord.Infrastructure.Logging;
+using TeachingRecordSystem.Core;
+using TeachingRecordSystem.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddGovUkFrontend();
+builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.AddServiceDefaults(dataProtectionBlobName: "AuthorizeAccess");
+
+builder.ConfigureLogging();
+
+builder.Services.AddGovUkFrontend();
+builder.Services.AddCsp(nonceByteAmount: 32);
+
+builder.Services
+    .AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.MapDefaultEndpoints();
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else if (!app.Environment.IsUnitTests())
+{
+    app.UseExceptionHandler("/error");
+    app.UseStatusCodePagesWithReExecute("/error", "?code={0}");
 }
 
-app.UseHttpsRedirection();
+app.UseCsp(csp =>
+{
+    var pageTemplateHelper = app.Services.GetRequiredService<PageTemplateHelper>();
+
+    csp.ByDefaultAllow
+        .FromSelf();
+
+    csp.AllowScripts
+        .FromSelf()
+        .From(pageTemplateHelper.GetCspScriptHashes())
+        .AddNonce();
+
+    // Ensure ASP.NET Core's auto refresh works
+    // See https://github.com/dotnet/aspnetcore/issues/33068
+    if (builder.Environment.IsDevelopment())
+    {
+        csp.AllowConnections
+            .ToAnywhere();
+    }
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -25,5 +61,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
