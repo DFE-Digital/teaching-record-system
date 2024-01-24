c016416ce2b0e3074d587c0e9698689b3ea4027e
James Gunn <james@gunn.io>
2024-01-24 10:56:22 +0000

    Make Person.Trn nullable

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/Person.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/Person.cs
@@ -6,7 +6,7 @@ public class Person
     public required DateTime? CreatedOn { get; init; }
     public required DateTime? UpdatedOn { get; set; }
     public DateTime? DeletedOn { get; set; }
-    public required string Trn { get; set; }
+    public required string? Trn { get; set; }
     public required string FirstName { get; set; }
     public required string MiddleName { get; set; }
     public required string LastName { get; set; }
