1f9c7651ed609306e916c8642365caf56e377977
James Gunn <james@gunn.io>
2024-01-25 16:09:08 +0000

    Add basic One Login integration setup

--- TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Program.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Program.cs
@@ -1,5 +1,10 @@
+using System.Security.Cryptography;
 using GovUk.Frontend.AspNetCore;
+using GovUk.OneLogin.AspNetCore;
 using Joonasw.AspNetCore.SecurityHeaders;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Tokens;
+using TeachingRecordSystem;
 using TeachingRecordSystem.AuthorizeAccessToATeacherRecord.Infrastructure.Logging;
 using TeachingRecordSystem.Core;
 using TeachingRecordSystem.ServiceDefaults;
@@ -14,6 +19,35 @@ builder.ConfigureLogging();
 
 builder.Services.AddGovUkFrontend();
 builder.Services.AddCsp(nonceByteAmount: 32);
+
+builder.Services.AddAuthentication(defaultScheme: OneLoginDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOneLogin(options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+        var rsa = RSA.Create();
+        var privateKeyPem = builder.Configuration.GetRequiredValue("OneLogin:PrivateKeyPem");
+        rsa.ImportPkcs8PrivateKey(Convert.FromBase64String(privateKeyPem), out _);
+        options.ClientAuthenticationCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+
+        var coreIdentityIssuer = ECDsa.Create();
+        var coreIdentityIssuerPem = builder.Configuration.GetRequiredValue("OneLogin:CoreIdentityIssuerPem");
+        coreIdentityIssuer.ImportSubjectPublicKeyInfo(Convert.FromBase64String(coreIdentityIssuerPem), out _);
+        options.CoreIdentityClaimIssuerSigningKey = new ECDsaSecurityKey(coreIdentityIssuer);
+        options.CoreIdentityClaimIssuer = "https://identity.integration.account.gov.uk/";
+
+        options.VectorsOfTrust = @"[""Cl.Cm.P2""]";
+
+        options.Claims.Add(OneLoginClaimTypes.CoreIdentity);
+
+        options.MetadataAddress = "https://oidc.integration.account.gov.uk/.well-known/openid-configuration";
+        options.ClientAssertionJwtAudience = "https://oidc.integration.account.gov.uk/token";
+
+        options.ClientId = builder.Configuration.GetRequiredValue("OneLogin:ClientId");
+        options.CallbackPath = "/_onelogin/aytq/callback";
+        options.SignedOutCallbackPath = "/_onelogin/aytq/logout-callback";
+    });
 
 builder.Services
     .AddRazorPages();
