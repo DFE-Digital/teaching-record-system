37d6cf6fb14f3c44ae2426dfcfeab4e5952827d3
James Gunn <james@gunn.io>
2024-01-25 17:04:49 +0000

    Add basic One Login integration setup (#1113)

--- TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Pages/Index.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccessToATeacherRecord/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TeachingRecordSystem.AuthorizeAccessToATeacherRecord.Pages;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     public void OnGet()
