caa19cd5de913712117140e9ee87794c104cad51
James Gunn <james@gunn.io>
2024-02-07 15:37:40 +0000

    Add test infrastructure for Authorize access (#1148)

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.AuthorizeAccess.Tests/JourneyInstanceExtensions.cs
@@ -0,0 +1,11 @@
+using TeachingRecordSystem.FormFlow;
+
+namespace TeachingRecordSystem.AuthorizeAccess.Tests;
+
+public static class JourneyInstanceExtensions
+{
+    public static string GetUniqueIdQueryParameter(this JourneyInstance journeyInstance) =>
+        journeyInstance.InstanceId.UniqueKey is string uniqueKey ?
+            $"{Constants.UniqueKeyQueryParameterName}={Uri.EscapeDataString(uniqueKey)}" :
+            string.Empty;
+}
