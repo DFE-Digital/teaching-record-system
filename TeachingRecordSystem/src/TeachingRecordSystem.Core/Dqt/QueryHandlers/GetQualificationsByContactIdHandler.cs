5740b088af186f827a5b20d1c9dd047d1319e397
James Gunn <james@gunn.io>
2024-02-13 16:55:48 +0000

    Update sync-person CLI command to sync person's MQs (#1166)

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/QueryHandlers/GetQualificationsByContactIdHandler.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/QueryHandlers/GetQualificationsByContactIdHandler.cs
@@ -2,6 +2,7 @@ using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Query;
 using TeachingRecordSystem.Core.Dqt.Queries;
+using TeachingRecordSystem.Core.Services.TrsDataSync;
 
 namespace TeachingRecordSystem.Core.Dqt.QueryHandlers;
 
@@ -13,18 +14,25 @@ public class GetQualificationsByContactIdHandler : ICrmQueryHandler<GetQualifica
         filter.AddCondition(dfeta_qualification.Fields.dfeta_PersonId, ConditionOperator.Equal, query.ContactId);
         filter.AddCondition(dfeta_qualification.Fields.StateCode, ConditionOperator.Equal, (int)dfeta_qualificationState.Active);
 
+        var columns = new List<string>()
+        {
+            dfeta_qualification.PrimaryIdAttribute,
+            dfeta_qualification.Fields.dfeta_Type,
+            dfeta_qualification.Fields.dfeta_CompletionorAwardDate,
+            dfeta_qualification.Fields.dfeta_MQ_MQEstablishmentId,
+            dfeta_qualification.Fields.dfeta_MQStartDate,
+            dfeta_qualification.Fields.dfeta_MQ_SpecialismId,
+            dfeta_qualification.Fields.dfeta_MQ_Date,
+            dfeta_qualification.Fields.dfeta_MQ_Status,
+            dfeta_qualification.Fields.StateCode
+        };
+
+        // Ensure we have enough to satisfy the sync
+        columns.AddRange(TrsDataSyncHelper.GetEntityInfoForModelType(TrsDataSyncHelper.ModelTypes.MandatoryQualification).AttributeNames);
+
         var queryExpression = new QueryExpression(dfeta_qualification.EntityLogicalName)
         {
-            ColumnSet = new ColumnSet(
-                dfeta_qualification.PrimaryIdAttribute,
-                dfeta_qualification.Fields.dfeta_Type,
-                dfeta_qualification.Fields.dfeta_CompletionorAwardDate,
-                dfeta_qualification.Fields.dfeta_MQ_MQEstablishmentId,
-                dfeta_qualification.Fields.dfeta_MQStartDate,
-                dfeta_qualification.Fields.dfeta_MQ_SpecialismId,
-                dfeta_qualification.Fields.dfeta_MQ_Date,
-                dfeta_qualification.Fields.dfeta_MQ_Status,
-                dfeta_qualification.Fields.StateCode),
+            ColumnSet = new ColumnSet(columns.Distinct().ToArray()),
             Criteria = filter
         };
 
