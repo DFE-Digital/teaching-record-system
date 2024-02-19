432d925c3b7920b6a0d65a539d0e1898b3a25b22
Kevin Joy <mrkevinjoy@outlook.com>
2024-02-19 11:42:50 +0000

    Extra existing nino normalization into seperate method

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/NationalInsuranceNumberHelper.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/NationalInsuranceNumberHelper.cs
@@ -11,11 +11,21 @@ public static partial class NationalInsuranceNumberHelper
             return false;
         }
 
-        var normalized = new string(value.Where(c => !Char.IsWhiteSpace(c) && c != '-').ToArray());
+        var normalized = NormalizeNationalInsuranceNumber(value)!;
 
         return ValidNinoPattern().IsMatch(normalized);
     }
 
     [GeneratedRegex("^[A-CEGHJ-PR-TW-Za-ceghj-pr-tw-z]{1}[A-CEGHJ-NPR-TW-Za-ceghj-npr-tw-z]{1}[0-9]{6}[A-DFMa-dfm]{0,1}$")]
     private static partial Regex ValidNinoPattern();
+
+    public static string? NormalizeNationalInsuranceNumber(string? value)
+    {
+        if (value is null)
+        {
+            return null;
+        }
+
+        return new string(value.Where(c => !Char.IsWhiteSpace(c) && c != '-').ToArray());
+    }
 }
