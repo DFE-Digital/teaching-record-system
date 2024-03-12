fe895db66540a6e3cab44e7476d2e2b2a371f56a
Andrew Horth <andrew@horth.co.uk>
2024-03-12 14:47:25 +0000

    Uncommented out temporarily commented out code - doh

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ImportTpsCsvExtractFileJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ImportTpsCsvExtractFileJob.cs
@@ -18,11 +18,11 @@ public class ImportTpsCsvExtractFileJob(
         // If we ever need to process more than one file then we can always manually trigger this job again or add a loop here
         var tpsCsvExtractId = Guid.NewGuid();
         var importJobId = await backgroundJobScheduler.Enqueue<TpsCsvExtractFileImporter>(j => j.ImportFile(tpsCsvExtractId, pendingImportFileNames[0], cancellationToken));
-        //var archiveJobId = await backgroundJobScheduler.ContinueJobWith<ITpsExtractStorageService>(importJobId, j => j.ArchiveFile(pendingImportFileNames[0], cancellationToken));
-        var copyJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractFileImporter>(importJobId, j => j.CopyValidFormatDataToStaging(tpsCsvExtractId, cancellationToken));
-        //var processInvalidTrnsJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(copyJobId, j => j.ProcessNonMatchingTrns(tpsCsvExtractId, cancellationToken));
-        //var processInvalidEstablishmentsJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processInvalidTrnsJobId, j => j.ProcessNonMatchingEstablishments(tpsCsvExtractId, cancellationToken));
-        //var processNewEmploymentHistoryJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processInvalidEstablishmentsJobId, j => j.ProcessNewEmploymentHistory(tpsCsvExtractId, cancellationToken));
-        //var processUpdatedEmploymentHistoryJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processNewEmploymentHistoryJobId, j => j.ProcessUpdatedEmploymentHistory(tpsCsvExtractId, cancellationToken));
+        var archiveJobId = await backgroundJobScheduler.ContinueJobWith<ITpsExtractStorageService>(importJobId, j => j.ArchiveFile(pendingImportFileNames[0], cancellationToken));
+        var copyJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractFileImporter>(archiveJobId, j => j.CopyValidFormatDataToStaging(tpsCsvExtractId, cancellationToken));
+        var processInvalidTrnsJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(copyJobId, j => j.ProcessNonMatchingTrns(tpsCsvExtractId, cancellationToken));
+        var processInvalidEstablishmentsJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processInvalidTrnsJobId, j => j.ProcessNonMatchingEstablishments(tpsCsvExtractId, cancellationToken));
+        var processNewEmploymentHistoryJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processInvalidEstablishmentsJobId, j => j.ProcessNewEmploymentHistory(tpsCsvExtractId, cancellationToken));
+        var processUpdatedEmploymentHistoryJobId = await backgroundJobScheduler.ContinueJobWith<TpsCsvExtractProcessor>(processNewEmploymentHistoryJobId, j => j.ProcessUpdatedEmploymentHistory(tpsCsvExtractId, cancellationToken));
     }
 }
