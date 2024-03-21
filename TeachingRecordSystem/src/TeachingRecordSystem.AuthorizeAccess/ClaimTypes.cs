e7b893ef66d1cc0d7398c8f19124a888fb0de9e6
James Gunn <james@gunn.io>
2024-03-21 17:54:02 +0000

    Add OIDC sign out support (#1252)

--- TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccess/ClaimTypes.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccess/ClaimTypes.cs
@@ -5,4 +5,5 @@ public static class ClaimTypes
     public const string Email = "email";
     public const string Subject = "sub";
     public const string Trn = "trn";
+    public const string OneLoginIdToken = "onelogin_id";
 }
