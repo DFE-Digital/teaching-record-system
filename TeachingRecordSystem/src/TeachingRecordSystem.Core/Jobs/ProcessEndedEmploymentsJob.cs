e4fafed5887d9959698aed63442be813af131546
Andrew Horth <andrew@horth.co.uk>
2024-05-07 16:35:07 +0100

    Fixed PR issues

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ProcessEndedEmploymentsJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ProcessEndedEmploymentsJob.cs
@@ -1,12 +1,11 @@
-using TeachingRecordSystem.Core.Jobs.Scheduling;
 using TeachingRecordSystem.Core.Services.WorkforceData;
 
 namespace TeachingRecordSystem.Core.Jobs;
 
-public class ProcessEndedEmploymentsJob(IBackgroundJobScheduler backgroundJobScheduler)
+public class ProcessEndedEmploymentsJob(TpsCsvExtractProcessor tpsCsvExtractProcessor)
 {
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        await backgroundJobScheduler.Enqueue<TpsCsvExtractProcessor>(j => j.ProcessEndedEmployments(cancellationToken));
+        await tpsCsvExtractProcessor.ProcessEndedEmployments(cancellationToken);
     }
 }
