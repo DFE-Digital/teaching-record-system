a6773eac7d6275d757c1157481fdb723b7e878b9
James Gunn <james@gunn.io>
2024-05-14 10:24:20 +0100

    Handle no suggestions for connecting One Login user (#1320)

--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/SupportTaskTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/SupportTaskTests.cs
@@ -3,7 +3,7 @@ namespace TeachingRecordSystem.SupportUi.EndToEndTests;
 public class SupportTaskTests(HostFixture hostFixture) : TestBase(hostFixture)
 {
     [Fact]
-    public async Task ConnectOneLoginUser()
+    public async Task ConnectOneLoginUser_WithSuggestion()
     {
         var person = await TestData.CreatePerson();
         var oneLoginUser = await TestData.CreateOneLoginUser(personId: null, verifiedInfo: ([person.FirstName, person.LastName], person.DateOfBirth));
@@ -17,6 +17,29 @@ public class SupportTaskTests(HostFixture hostFixture) : TestBase(hostFixture)
 
         await page.WaitForUrlPathAsync($"/support-tasks/connect-one-login-user/{supportTask.SupportTaskReference}");
         await page.ClickAsync($"label:text-is('{person.FirstName} {person.MiddleName} {person.LastName}')");
+        await page.ClickContinueButton();
+
+        await page.WaitForUrlPathAsync($"/support-tasks/connect-one-login-user/{supportTask.SupportTaskReference}/connect");
+        await page.ClickButton("Connect record");
+
+        await page.WaitForUrlPathAsync("/support-tasks");
+    }
+
+    [Fact]
+    public async Task ConnectOneLoginUser_WithoutSuggestions()
+    {
+        var person = await TestData.CreatePerson();
+        var oneLoginUser = await TestData.CreateOneLoginUser(personId: null, verifiedInfo: ([TestData.GenerateFirstName(), TestData.GenerateLastName()], TestData.GenerateDateOfBirth()));
+        var supportTask = await TestData.CreateConnectOneLoginUserSupportTask(oneLoginUser.Subject);
+
+        await using var context = await HostFixture.CreateBrowserContext();
+        var page = await context.NewPageAsync();
+
+        await page.GotoAsync("/support-tasks");
+        await page.ClickAsync($"a:text-is('{supportTask.SupportTaskReference}')");
+
+        await page.WaitForUrlPathAsync($"/support-tasks/connect-one-login-user/{supportTask.SupportTaskReference}");
+        await page.FillAsync($"label:text-is('Connect to TRN (optional)')", person.Trn!);
         await page.ClickContinueButton();
 
         await page.WaitForUrlPathAsync($"/support-tasks/connect-one-login-user/{supportTask.SupportTaskReference}/connect");
