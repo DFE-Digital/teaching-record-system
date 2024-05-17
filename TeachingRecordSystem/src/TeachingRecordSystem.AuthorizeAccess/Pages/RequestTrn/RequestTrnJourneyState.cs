8274a40aad6300a1b690c82c52f3fc2c687950c9
Andrew Horth <andrew@horth.co.uk>
2024-05-17 11:27:25 +0100

    Fixed issues from PR comments

--- TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccess/Pages/RequestTrn/RequestTrnJourneyState.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.AuthorizeAccess/Pages/RequestTrn/RequestTrnJourneyState.cs
@@ -7,5 +7,5 @@ public class RequestTrnJourneyState
     public const string JourneyName = "RequestTrnJourney";
 
     public static JourneyDescriptor JourneyDescriptor { get; } =
-    new JourneyDescriptor(JourneyName, typeof(RequestTrnJourneyState), requestDataKeys: [], appendUniqueKey: true);
+        new JourneyDescriptor(JourneyName, typeof(RequestTrnJourneyState), requestDataKeys: [], appendUniqueKey: true);
 }
