3b82fe4e3d9305919dab21dd22a7f5de29e4775f
James Gunn <james@gunn.io>
2024-06-07 13:36:46 +0100

    Simplify referencing previous version DTOs

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/V20240416/Controllers/TeachersController.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/V20240416/Controllers/TeachersController.cs
@@ -4,8 +4,7 @@ using Swashbuckle.AspNetCore.Annotations;
 using TeachingRecordSystem.Api.Infrastructure.ModelBinding;
 using TeachingRecordSystem.Api.Infrastructure.Security;
 using TeachingRecordSystem.Api.V3.Core.Operations;
-using TeachingRecordSystem.Api.V3.V20240416.Requests;
-using TeachingRecordSystem.Api.V3.V20240416.Responses;
+using GetTeacherDtoVersion = TeachingRecordSystem.Api.V3.V20240101;
 
 namespace TeachingRecordSystem.Api.V3.V20240416.Controllers;
 
@@ -17,13 +16,13 @@ public class TeachersController(IMapper mapper) : ControllerBase
         OperationId = "GetTeacherByTrn",
         Summary = "Get teacher details by TRN",
         Description = "Gets the details of the teacher corresponding to the given TRN.")]
-    [ProducesResponseType(typeof(GetTeacherResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(GetTeacherDtoVersion.Responses.GetTeacherResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     [Authorize(Policy = AuthorizationPolicies.GetPerson)]
     public async Task<IActionResult> Get(
         [FromRoute] string trn,
-        [FromQuery, ModelBinder(typeof(FlagsEnumStringListModelBinder)), SwaggerParameter("The additional properties to include in the response.")] GetTeacherRequestIncludes? include,
+        [FromQuery, ModelBinder(typeof(FlagsEnumStringListModelBinder)), SwaggerParameter("The additional properties to include in the response.")] GetTeacherDtoVersion.Requests.GetTeacherRequestIncludes? include,
         [FromQuery, SwaggerParameter("Adds an additional check that the record has the specified dateOfBirth, if provided.")] DateOnly? dateOfBirth,
         [FromServices] GetPersonHandler handler)
     {
@@ -39,7 +38,7 @@ public class TeachersController(IMapper mapper) : ControllerBase
             return NotFound();
         }
 
-        var response = mapper.Map<GetTeacherResponse>(result);
+        var response = mapper.Map<GetTeacherDtoVersion.Responses.GetTeacherResponse>(result);
         return Ok(response);
     }
 }
