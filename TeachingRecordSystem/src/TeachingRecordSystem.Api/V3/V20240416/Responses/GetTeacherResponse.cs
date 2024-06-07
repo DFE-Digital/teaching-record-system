a24365d2c8ff4da4b4c4a70ac61e1843d2d95436
James Gunn <james@gunn.io>
2024-06-07 13:55:52 +0100

    Simplify referencing previous version DTOs (#1358)

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/V20240416/Responses/GetTeacherResponse.cs
+++
@@ -1,6 +0,0 @@
-using TeachingRecordSystem.Api.V3.Core.Operations;
-
-namespace TeachingRecordSystem.Api.V3.V20240416.Responses;
-
-[AutoMap(typeof(GetPersonResult))]
-public record GetTeacherResponse : V20240101.Responses.GetTeacherResponse;
