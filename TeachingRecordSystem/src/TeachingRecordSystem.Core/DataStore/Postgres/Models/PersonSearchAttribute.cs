3b053e3f00bb1e2c5430454322a2d573945f1015
Andrew Horth <andrew@horth.co.uk>
2024-06-17 15:46:56 +0100

    Increase length of attribute_value field in person_search_attributes table to 1000 characters

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/PersonSearchAttribute.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/PersonSearchAttribute.cs
@@ -3,7 +3,7 @@ namespace TeachingRecordSystem.Core.DataStore.Postgres.Models;
 public class PersonSearchAttribute
 {
     public const int AttributeTypeMaxLength = 50;
-    public const int AttributeValueMaxLength = 100;
+    public const int AttributeValueMaxLength = 1000;
     public const int AttributeKeyMaxLength = 50;
     public const string PersonIdIndexName = "ix_person_search_attributes_person_id";
     public const string AttributeTypeAndValueIndexName = "ix_person_search_attributes_attribute_type_and_value";
