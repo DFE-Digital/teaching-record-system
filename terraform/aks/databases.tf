82670d2e4f27d5def9052641f15ec2124566cb83
Venkata Challa <venkata.challa80@gmail.com>
2024-06-24 15:48:08 +0100

    Review app config added.

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -2,7 +2,7 @@ module "redis" {
   source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/redis?ref=testing"
 
   namespace             = var.namespace
-  environment           = var.environment_name
+  environment           = local.app_name_suffix
   azure_resource_prefix = var.azure_resource_prefix
   service_name          = var.service_name
   service_short         = var.service_short_name
@@ -19,10 +19,10 @@ module "redis" {
 }
 
 module "postgres" {
-  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=testing"
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=1813-standardise-logging-fields"
 
   namespace             = var.namespace
-  environment           = var.environment_name
+  environment           = local.app_name_suffix
   azure_resource_prefix = var.azure_resource_prefix
   service_name          = var.service_name
   service_short         = var.service_short_name
