ffc35b050b63bfd44cfbb469a1de735bba694d9e
Andrew Horth <andrew@horth.co.uk>
2024-08-14 14:32:34 +0100

    Remove current alerts functionality backed by DQT (#1463)

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/Queries/GetSanctionDetailsByContactIdQuery.cs
+++
@@ -1,5 +0,0 @@
-namespace TeachingRecordSystem.Core.Dqt.Queries;
-
-public record GetSanctionDetailsByContactIdQuery(Guid ContactId) : ICrmQuery<SanctionDetailResult[]>;
-
-public record SanctionDetailResult(dfeta_sanction Sanction, string Description);
