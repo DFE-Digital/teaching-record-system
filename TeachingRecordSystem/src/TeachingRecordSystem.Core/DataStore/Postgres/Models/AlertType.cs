501ec25ba66821e6894a35034dbc4815fc974439
Andrew Horth <andrew@horth.co.uk>
2024-09-04 21:25:24 +0100

    Added data seeding for alert categories and types

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/AlertType.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/AlertType.cs
@@ -3,10 +3,13 @@ namespace TeachingRecordSystem.Core.DataStore.Postgres.Models;
 public class AlertType
 {
     public const int NameMaxLength = 200;
+    public const int DqtSanctionCodeMaxLength = 5;
     public const string AlertCategoryIdIndexName = "ix_alert_types_alert_category_id";
     public const string AlertCategoryForeignKeyName = "fk_alert_types_alert_category";
 
     public required Guid AlertTypeId { get; init; }
     public required Guid AlertCategoryId { get; init; }
     public required string Name { get; init; }
+    public required string? DqtSanctionCode { get; init; }
+    public required ProhibitionLevel ProhibitionLevel { get; init; }
 }
---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Models/ProhibitionLevel.cs
@@ -0,0 +1,10 @@
+namespace TeachingRecordSystem.Core.Models;
+
+public enum ProhibitionLevel
+{
+    None = 0,
+    Teaching = 1,
+    Restrict = 2,
+    Notify = 3,
+    LeadershipPositions = 4
+}
