536564916b868f9a68a419fdaba1965e08ee6a84
James Gunn <james@gunn.io>
2024-09-17 12:06:07 +0100

    Add alerts API spec and stub endpoints (#1517)

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/VNext/Requests/CreateAlertRequestBody.cs
@@ -0,0 +1,12 @@
+using Optional;
+
+namespace TeachingRecordSystem.Api.V3.VNext.Requests;
+
+public record CreateAlertRequestBody
+{
+    public required string Trn { get; init; }
+    public required Guid AlertTypeId { get; init; }
+    public required DateOnly StartDate { get; init; }
+    public Option<DateOnly?> EndDate { get; init; }
+    public Option<string?> Details { get; init; }
+}
