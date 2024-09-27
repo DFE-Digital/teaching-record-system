fe3fe66b7b3b5a8242244483821a18e0580b5032
James Gunn <james@gunn.io>
2024-09-27 16:06:46 +0100

    Remove command timeout from job

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ClearAlertsJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/ClearAlertsJob.cs
@@ -9,6 +9,7 @@ public class ClearAlertsJob(TrsDbContext dbContext)
     public async Task Execute()
     {
         await using var txn = await dbContext.Database.BeginTransactionAsync();
+        dbContext.Database.SetCommandTimeout(0);
         await dbContext.Database.ExecuteSqlAsync($"delete from events where event_name like 'Alert%';");
         await dbContext.Database.ExecuteSqlAsync($"delete from alerts;");
         await txn.CommitAsync();
