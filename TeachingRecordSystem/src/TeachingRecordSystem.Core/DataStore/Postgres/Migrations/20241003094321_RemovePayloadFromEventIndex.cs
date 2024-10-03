fb6f388a3019f672f5e92dcffe74960b0a4d67dc
James Gunn <james@gunn.io>
2024-10-03 11:04:16 +0100

    Remove the payload column from event index (#1548)

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20241003094321_RemovePayloadFromEventIndex.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePayloadFromEventIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_events_person_id_event_name",
+                table: "events");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_events_person_id_event_name",
+                table: "events",
+                columns: new[] { "person_id", "event_name" },
+                filter: "person_id is not null");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_events_person_id_event_name",
+                table: "events");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_events_person_id_event_name",
+                table: "events",
+                columns: new[] { "person_id", "event_name" },
+                filter: "person_id is not null")
+                .Annotation("Npgsql:IndexInclude", new[] { "payload" });
+        }
+    }
+}
