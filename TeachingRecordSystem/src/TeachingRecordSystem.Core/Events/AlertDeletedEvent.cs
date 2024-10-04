53c9f7aeb9d7ce88bc599e482e233b3992b8ae1d
Andrew Horth <andrew@horth.co.uk>
2024-10-04 11:45:30 +0100

    Removed unnecessary property

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/AlertDeletedEvent.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/AlertDeletedEvent.cs
@@ -3,9 +3,8 @@ using File = TeachingRecordSystem.Core.Events.Models.File;
 
 namespace TeachingRecordSystem.Core.Events;
 
-public record class AlertDeletedEvent : EventBase, IEventWithPersonId, IEventWithAlert, IEventWithKey
+public record class AlertDeletedEvent : EventBase, IEventWithPersonId, IEventWithAlert
 {
-    public string? Key { get; init; }
     public required Guid PersonId { get; init; }
     public required Alert Alert { get; init; }
     public required string? Reason { get; init; }
