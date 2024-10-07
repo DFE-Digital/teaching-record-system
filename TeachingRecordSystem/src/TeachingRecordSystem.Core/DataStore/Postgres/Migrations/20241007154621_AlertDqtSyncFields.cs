370cff83c0f22c27fe17aaa5d5ec7f52e0b0b016
James Gunn <james@gunn.io>
2024-10-07 16:42:54 +0100

    Add Alerts to sync service

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20241007154621_AlertDqtSyncFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlertDqtSyncFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dqt_first_sync",
+                table: "alerts",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "dqt_last_sync",
+                table: "alerts",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dqt_first_sync",
+                table: "alerts");
+
+            migrationBuilder.DropColumn(
+                name: "dqt_last_sync",
+                table: "alerts");
+        }
+    }
+}
---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtReporting/Migrations/0031_TrsAlertsSyncMetadata.sql
@@ -0,0 +1,2 @@
+alter table trs_alerts add dqt_first_sync datetime
+alter table trs_alerts add dqt_last_sync datetime
