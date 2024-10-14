e44b08265eff6f13888aba4839dc9a6f3be4f10d
James Gunn <james@gunn.io>
2024-10-14 15:57:51 +0100

    Move conventions into the folders they apply to (#1575)

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Alerts/AddAlert/Conventions.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeachingRecordSystem.SupportUi.Infrastructure.Filters;
+
+namespace TeachingRecordSystem.SupportUi.Pages.Alerts.AddAlert;
+
+public class Conventions : IConfigureFolderConventions
+{
+    public void Configure(RazorPagesOptions options)
+    {
+        options.Conventions.AddFolderApplicationModelConvention(
+            this.GetFolderPathFromNamespace(),
+            model =>
+            {
+                model.Filters.Add(new CheckPersonExistsFilterFactory());
+            });
+    }
+}
---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Alerts/ReopenAlert/Conventions.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeachingRecordSystem.SupportUi.Infrastructure.Filters;
+
+namespace TeachingRecordSystem.SupportUi.Pages.Alerts.ReopenAlert;
+
+public class Conventions : IConfigureFolderConventions
+{
+    public void Configure(RazorPagesOptions options)
+    {
+        options.Conventions.AddFolderApplicationModelConvention(
+            this.GetFolderPathFromNamespace(),
+            model =>
+            {
+                model.Filters.Add(new ServiceFilterAttribute<CheckAlertExistsFilter>());
+            });
+    }
+}
