78f96fa0733030bbd81ec33d1ac89a79fc917442
Kevin Joy <mrkevinjoy@outlook.com>
2024-10-23 09:48:10 +0100

    Add missing programme types to reporting db

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtReporting/Migrations/0032_AddMissingProgrammeTypes.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[GlobalOptionSetMetadata](OptionSetName,[Option],IsUserLocalizedLabel,LocalizedLabelLanguageCode,LocalizedLabel) values('dfeta_programmetype',389040025,0,1033,'Primary and secondary undergraduate fee funded');
+
+INSERT INTO [dbo].[GlobalOptionSetMetadata](OptionSetName,[Option],IsUserLocalizedLabel,LocalizedLabelLanguageCode,LocalizedLabel) values('dfeta_programmetype',389040026,0,1033,'Primary and secondary postgraduate fee funded');
