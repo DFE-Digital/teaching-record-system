d7d56dd902fe14109f5f6b5153f1974a34005c61
James Gunn <james@gunn.io>
2024-10-24 15:14:31 +0100

    Apply authorization checks to Person Alerts page (#1607)

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/PersonDetail/Alerts.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/PersonDetail/Alerts.cshtml.cs
@@ -1,18 +1,22 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TeachingRecordSystem.Core.DataStore.Postgres;
 using TeachingRecordSystem.Core.DataStore.Postgres.Models;
+using TeachingRecordSystem.SupportUi.Infrastructure.Security;
 
 namespace TeachingRecordSystem.SupportUi.Pages.Persons.PersonDetail;
 
-public class AlertsModel(TrsDbContext dbContext) : PageModel
+public class AlertsModel(TrsDbContext dbContext, ReferenceDataCache referenceDataCache, IAuthorizationService authorizationService) : PageModel
 {
     [FromRoute]
     public Guid PersonId { get; set; }
 
-    public Alert[]? OpenAlerts { get; set; }
+    public AlertWithPermissions[]? OpenAlerts { get; set; }
 
-    public Alert[]? ClosedAlerts { get; set; }
+    public AlertWithPermissions[]? ClosedAlerts { get; set; }
+
+    public bool CanAddAlert { get; set; }
 
     public async Task OnGet()
     {
@@ -22,7 +26,27 @@ public class AlertsModel(TrsDbContext dbContext) : PageModel
             .Where(a => a.PersonId == PersonId)
             .ToArrayAsync();
 
-        OpenAlerts = alerts.Where(a => a.IsOpen).OrderBy(a => a.StartDate).ThenBy(a => a.AlertType.Name).ToArray();
-        ClosedAlerts = alerts.Where(a => !a.IsOpen).OrderBy(a => a.StartDate).ThenBy(a => a.EndDate).ThenBy(a => a.AlertType.Name).ToArray();
+        var alertTypePermissions = await alerts
+            .Select(a => a.AlertTypeId)
+            .Distinct()
+            .ToAsyncEnumerable()
+            .SelectAwait(async id => (
+                AlertTypeId: id,
+                CanRead: await authorizationService.AuthorizeForAlertTypeAsync(User, id, Permissions.Alerts.Read) is { Succeeded: true },
+                CanWrite: await authorizationService.AuthorizeForAlertTypeAsync(User, id, Permissions.Alerts.Write) is { Succeeded: true }))
+            .ToDictionaryAsync(t => t.AlertTypeId);
+
+        var authorizedAlerts = alerts
+            .Where(a => alertTypePermissions[a.AlertTypeId].CanRead)
+            .Select(a => new AlertWithPermissions(a, alertTypePermissions[a.AlertTypeId].CanWrite));
+
+        OpenAlerts = authorizedAlerts.Where(a => a.Alert.IsOpen).OrderBy(a => a.Alert.StartDate).ThenBy(a => a.Alert.AlertType.Name).ToArray();
+        ClosedAlerts = authorizedAlerts.Where(a => !a.Alert.IsOpen).OrderBy(a => a.Alert.StartDate).ThenBy(a => a.Alert.EndDate).ThenBy(a => a.Alert.AlertType.Name).ToArray();
+
+        CanAddAlert = await (await referenceDataCache.GetAlertTypes(activeOnly: true))
+            .ToAsyncEnumerable()
+            .AnyAwaitAsync(async at => (await authorizationService.AuthorizeForAlertTypeAsync(User, at.AlertTypeId, Permissions.Alerts.Write)) is { Succeeded: true });
     }
+
+    public record AlertWithPermissions(Alert Alert, bool CanWrite);
 }
