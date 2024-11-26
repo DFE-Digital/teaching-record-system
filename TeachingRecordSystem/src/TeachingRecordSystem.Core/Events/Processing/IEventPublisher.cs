aa1998bdbfa78e48aa28a49f0261f6b713f10b9c
James Gunn <james@gunn.io>
2024-11-26 14:19:05 +0000

    Remove event publishing services

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/Processing/IEventPublisher.cs
+++
@@ -1,6 +0,0 @@
-namespace TeachingRecordSystem.Core.Events.Processing;
-
-public interface IEventPublisher
-{
-    Task PublishEventAsync(EventBase @event);
-}
--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/Processing/NoopEventPublisher.cs
+++
@@ -1,6 +0,0 @@
-namespace TeachingRecordSystem.Core.Events.Processing;
-
-public class NoopEventPublisher : IEventPublisher
-{
-    public Task PublishEventAsync(EventBase @event) => Task.CompletedTask;
-}
