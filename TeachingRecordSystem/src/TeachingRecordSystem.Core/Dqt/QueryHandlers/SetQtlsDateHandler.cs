fe92b50a67c2b464ca66bca753f5a39fdbb4c78a
Kevin Joy <mrkevinjoy@outlook.com>
2024-12-04 13:20:27 +0000

    Revert "Fix QTLS date handling for BST (#1705)"
    This reverts commit 5ef67ff0170c31a99058929a66cb739269b2bc7c.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/QueryHandlers/SetQtlsDateHandler.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Dqt/QueryHandlers/SetQtlsDateHandler.cs
@@ -29,7 +29,7 @@ public class SetQtlsDateHandler : ICrmQueryHandler<SetQtlsDateQuery, bool>
             Target = new Contact()
             {
                 Id = query.ContactId,
-                dfeta_qtlsdate = query.QtlsDate.ToDateTimeWithDqtBstFix(isLocalTime: true)
+                dfeta_qtlsdate = query.QtlsDate.ToDateTimeWithDqtBstFix(isLocalTime: false)
             }
         });
         await requestBuilder.ExecuteAsync();
