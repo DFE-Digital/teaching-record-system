3291fec5656344ed5bc526e1820b71df6e968a66
Kevin Joy <mrkevinjoy@outlook.com>
2024-12-05 13:43:02 +0000

    Added NPQAccessToken to gettrn journey

--- TeachingRecordSystem/tests/TeachingRecordSystem.AuthorizeAccess.Tests/PageTests/RequestTrn/IndexTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.AuthorizeAccess.Tests/PageTests/RequestTrn/IndexTests.cs
@@ -6,15 +6,32 @@ public class IndexTests(HostFixture hostFixture) : TestBase(hostFixture)
     public async Task Get_ValidRequest_RendersExpectedContent()
     {
         // Arrange
+        var accessToken = HostFixture.Services.GetRequiredService<IConfiguration>().GetValue<string>("RequestTrnAccessToken");
         var state = CreateNewState();
         var journeyInstance = await CreateJourneyInstance(state);
 
-        var request = new HttpRequestMessage(HttpMethod.Get, $"/request-trn?{journeyInstance.GetUniqueIdQueryParameter()}");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/request-trn?{journeyInstance.GetUniqueIdQueryParameter()}&AccessToken={accessToken}");
 
         // Act
         var response = await HttpClient.SendAsync(request);
 
         // Assert
         await AssertEx.HtmlResponseAsync(response);
+    }
+
+    [Fact]
+    public async Task Get_MissingAccessToken_ReturnsBadRequest()
+    {
+        // Arrange
+        var state = CreateNewState();
+        var journeyInstance = await CreateJourneyInstance(state);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/request-trn?{journeyInstance.GetUniqueIdQueryParameter()}&AccessToken=");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status400BadRequest, (int)response.StatusCode);
     }
 }
