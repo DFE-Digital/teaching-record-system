375426bb0fe7f4117ce80faeb5c336a605aa0a91
Andrew Horth <andrew@horth.co.uk>
2024-12-09 18:26:42 +0000

    Extended live sync to sync induction details
    Tweak to ensure required CRM entity attributes are retrieved for sync
    
    Amended to clear down data between tests which was causing indeterminate test results

--- TeachingRecordSystem/tests/TeachingRecordSystem.Core.Tests/Services/TrsDataSync/TrsDataSyncServiceTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.Core.Tests/Services/TrsDataSync/TrsDataSyncServiceTests.cs
@@ -1,9 +1,14 @@
+
 namespace TeachingRecordSystem.Core.Tests.Services.TrsDataSync;
 
 [Collection(nameof(TrsDataSyncTestCollection))]
-public partial class TrsDataSyncServiceTests(TrsDataSyncServiceFixture fixture) : IClassFixture<TrsDataSyncServiceFixture>
+public partial class TrsDataSyncServiceTests(TrsDataSyncServiceFixture fixture) : IClassFixture<TrsDataSyncServiceFixture>, IAsyncLifetime
 {
     private TestableClock Clock => fixture.Clock;
 
     private TestData TestData => fixture.TestData;
+
+    Task IAsyncLifetime.DisposeAsync() => Task.CompletedTask;
+
+    Task IAsyncLifetime.InitializeAsync() => fixture.DbFixture.DbHelper.ClearDataAsync();
 }
