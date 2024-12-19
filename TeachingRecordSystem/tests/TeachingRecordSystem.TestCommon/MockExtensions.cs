19bc1242b3dc60f3ea6002ad00e88facd8fe3c2f
James Gunn <james@gunn.io>
2024-12-19 13:34:40 +0000

    Simplify helper method (#1740)

--- TeachingRecordSystem/tests/TeachingRecordSystem.TestCommon/MockExtensions.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.TestCommon/MockExtensions.cs
@@ -9,16 +9,6 @@ public static class MockExtensions
             params TResult[] items)
         where T : class
     {
-        return setup.Returns(GetAsyncEnumerableAsync(items));
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        static async IAsyncEnumerable<R> GetAsyncEnumerableAsync<R>(IEnumerable<R> enumerable)
-        {
-            foreach (var item in enumerable)
-            {
-                yield return item;
-            }
-        }
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        return setup.Returns(items.ToAsyncEnumerable());
     }
 }
