d6be8859f22b66031ea6a0126769e81f01339d0f
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-12-27 18:14:49 +0000

    Basic stem of the longest journey through the pages

--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/EditInductionPageExtensions.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/EditInductionPageExtensions.cs
@@ -13,4 +13,35 @@ public static class EditInductionPageExtensions
     {
         return page.GetByTestId($"change-induction-status").ClickAsync();
     }
+
+    public static Task AssertOnEditInductionStatusPageAsync(this IPage page, Guid personId)
+    {
+        return page.WaitForUrlPathAsync($"/persons/{personId}/edit-induction/status");
+    }
+
+    public static Task AssertOnEditInductionStartDatePageAsync(this IPage page, Guid personId)
+    {
+        return page.WaitForUrlPathAsync($"/persons/{personId}/edit-induction/start-date");
+    }
+
+    public static Task AssertOnEditInductionCompletedDatePageAsync(this IPage page, Guid personId)
+    {
+        return page.WaitForUrlPathAsync($"/persons/{personId}/edit-induction/date-completed");
+    }
+
+    public static Task AssertOnEditInductionChangeReasonPageAsync(this IPage page, Guid personId)
+    {
+        return page.WaitForUrlPathAsync($"/persons/{personId}/edit-induction/change-reason");
+    }
+
+    public static Task AssertOnEditInductionCheckYourAnswersPageAsync(this IPage page, Guid personId)
+    {
+        return page.WaitForUrlPathAsync($"/persons/{personId}/edit-induction/check-answers");
+    }
+
+    public static Task SelectStatusAsync(this IPage page, InductionStatus status)
+    {
+        var radioButton = page.Locator($"input[type='radio'][value='{status.ToString()}']");
+        return radioButton.Locator("xpath=following-sibling::label").ClickAsync();
+    }
 }
--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/JourneyTests/InductionTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/JourneyTests/InductionTests.cs
@@ -1,5 +1,45 @@
-﻿namespace TeachingRecordSystem.SupportUi.EndToEndTests.JourneyTests;
+namespace TeachingRecordSystem.SupportUi.EndToEndTests.JourneyTests;
 
-public class InductionTests
+public class InductionTests : TestBase 
 {
+    public InductionTests(HostFixture hostFixture)
+        : base(hostFixture)
+    {
+    }
+
+    [Fact]
+    public async Task EditInductionStatus()
+    {
+        var setStartDate = new DateOnly(2021, 1, 1);
+        var person = await TestData.CreatePersonAsync(
+                personBuilder => personBuilder
+                .WithQts()
+                .WithInductionStatus(inductionBuilder => inductionBuilder
+                    .WithStatus(InductionStatus.InProgress)
+                    .WithStartDate(setStartDate)
+                ));
+        var personId = person.ContactId;
+
+        await using var context = await HostFixture.CreateBrowserContext();
+        var page = await context.NewPageAsync();
+
+        await page.GoToPersonInductionPageAsync(personId);
+        await page.ClickEditInductionStatusPageAsync();
+
+        await page.AssertOnEditInductionStatusPageAsync(person.PersonId);
+
+        await page.SelectStatusAsync(InductionStatus.Failed);
+        await page.ClickContinueButtonAsync();
+
+        await page.AssertOnEditInductionStartDatePageAsync(person.PersonId);
+        await page.ClickContinueButtonAsync();
+
+        await page.AssertOnEditInductionCompletedDatePageAsync(person.PersonId);
+        await page.ClickContinueButtonAsync();
+
+        await page.AssertOnEditInductionChangeReasonPageAsync(person.PersonId);
+        await page.ClickContinueButtonAsync();
+
+        await page.AssertOnEditInductionCheckYourAnswersPageAsync(person.PersonId);
+    }
 }
