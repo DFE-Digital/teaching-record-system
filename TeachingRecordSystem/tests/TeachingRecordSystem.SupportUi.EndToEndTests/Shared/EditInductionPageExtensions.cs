e42361253f66ee711440f410296c8c3160ce78f9
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-12-27 12:19:52 +0000

    add new file for induction page extensions

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.EndToEndTests/Shared/EditInductionPageExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Playwright;
+
+namespace TeachingRecordSystem.SupportUi.EndToEndTests.Shared;
+
+public static class EditInductionPageExtensions
+{
+    public static Task GoToPersonInductionPageAsync(this IPage page, Guid personId)
+    {
+        return page.GotoAsync($"/persons/{personId}/induction");
+    }
+
+    public static Task ClickEditInductionStatusPageAsync(this IPage page)
+    {
+        return page.GetByTestId($"change-induction-status").ClickAsync();
+    }
+}
