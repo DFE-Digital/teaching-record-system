0bd8e44b486a31a14cf49f4aceff23722ebe130c
James Gunn <james@gunn.io>
2025-01-16 15:36:37 +0000

    Add UK to Country reference data set

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250116153528_UkCountry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class UkCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "countries",
+                columns: new[] { "country_id", "name" },
+                values: new object[] { "UK", "United Kingdom" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "countries",
+                keyColumn: "country_id",
+                keyValue: "UK");
+        }
+    }
+}
