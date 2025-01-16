c7f7fa4488ba0bc4a03ab807ebe47d622d5d8183
CLAWLOR <catherine.lawlor@education.gov.uk>
2025-01-16 21:16:50 +0000

    basic page

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/PersonDetail/EditInduction/CheckYourAnswers.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/PersonDetail/EditInduction/CheckYourAnswers.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TeachingRecordSystem.Core.DataStore.Postgres;
 
 namespace TeachingRecordSystem.SupportUi.Pages.Persons.PersonDetail.EditInduction;
 
 [Journey(JourneyNames.EditInduction), RequireJourneyInstance]
 public class CheckYourAnswersModel : CommonJourneyPage
 {
-    public CheckYourAnswersModel(TrsLinkGenerator linkGenerator) : base(linkGenerator)
+    private readonly TrsDbContext _dbContext;
+    public string? PersonName { get; set; }
+    public InductionStatus InductionStatus { get; set; }
+    public DateOnly? StartDate { get; set; }
+    public DateOnly? CompletedDate { get; set; }
+    public string? ExemptionReasonsDisplayString { get; set; }
+    public InductionChangeReasonOption? ChangeReason { get; set; }
+    public string? ChangeReasonDetail { get; set; }
+    public string? UploadedEvidenceFileUrl { get; set; }
+    public string? EvidenceFileName { get; set; }
+    public bool ShowStartDate =>
+        InductionStatus == InductionStatus.InProgress ||
+        InductionStatus == InductionStatus.Passed ||
+        InductionStatus == InductionStatus.Failed ||
+        InductionStatus == InductionStatus.FailedInWales;
+    public bool ShowCompletedDate =>
+        InductionStatus == InductionStatus.Failed ||
+        InductionStatus == InductionStatus.FailedInWales ||
+        InductionStatus == InductionStatus.Passed;
+    public bool ShowExemptionReasons =>
+        InductionStatus == InductionStatus.Exempt;
+
+    public CheckYourAnswersModel(TrsLinkGenerator linkGenerator, TrsDbContext dbContext) : base(linkGenerator)
     {
+        _dbContext = dbContext;
     }
 
     public string BackLink => PageLink(InductionJourneyPage.ChangeReasons);
@@ -25,5 +50,25 @@ public class CheckYourAnswersModel : CommonJourneyPage
     public Func<Guid, JourneyInstanceId, string> NextPage()
     {
         return (Id, journeyInstanceId) => LinkGenerator.PersonInduction(Id);
+    }
+
+    public override async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+    {
+        await JourneyInstance!.State.EnsureInitializedAsync(_dbContext, PersonId, InductionJourneyPage.Status);
+
+        var personInfo = context.HttpContext.GetCurrentPersonFeature();
+        PersonId = personInfo.PersonId;
+        PersonName = personInfo.Name;
+        //EvidenceFileId = JourneyInstance!.State.EvidenceFileId;
+        EvidenceFileName = JourneyInstance!.State.EvidenceFileName;
+        //UploadedEvidenceFileUrl = JourneyInstance!.State.UploadedEvidenceFileUrl;
+        ChangeReason = JourneyInstance!.State.ChangeReason;
+        ChangeReasonDetail = JourneyInstance!.State.ChangeReasonDetail;
+        InductionStatus = JourneyInstance!.State.InductionStatus;
+        StartDate = JourneyInstance!.State.StartDate;
+        CompletedDate = JourneyInstance!.State.CompletedDate;
+
+        //EvidenceFileSizeDescription = JourneyInstance!.State.EvidenceFileSizeDescription;
+        await next();
     }
 }
