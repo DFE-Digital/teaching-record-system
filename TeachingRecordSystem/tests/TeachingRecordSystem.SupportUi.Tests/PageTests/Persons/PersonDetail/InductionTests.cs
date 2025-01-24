7a1e63ce7661d58127050c7f3334c5640368e0bd
CLAWLOR <catherine.lawlor@education.gov.uk>
2025-01-24 20:59:05 +0000

    managed by cpd test

--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/Persons/PersonDetail/InductionTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/Persons/PersonDetail/InductionTests.cs
@@ -216,7 +216,7 @@ public class InductionTests(HostFixture hostFixture) : TestBase(hostFixture)
     {
         //Arrange
         var expectedWarning = "To change this teacher’s induction status ";
-        var overSevenYearsAgo = Clock.Today.AddYears(-7).AddDays(-1);
+        var lessThanSevenYearsAgo = Clock.Today.AddYears(-1);
 
         var person = await TestData.CreatePersonAsync();
         await WithDbContext(async dbContext =>
@@ -224,8 +224,8 @@ public class InductionTests(HostFixture hostFixture) : TestBase(hostFixture)
             dbContext.Attach(person.Person);
             person.Person.SetCpdInductionStatus(
                 InductionStatus.Passed,
-                startDate: Clock.Today.AddYears(-7).AddMonths(-6),
-                completedDate: overSevenYearsAgo,
+                startDate: Clock.Today.AddYears(-2),
+                completedDate: lessThanSevenYearsAgo,
                 cpdModifiedOn: Clock.UtcNow,
                 updatedBy: SystemUser.SystemUserId,
                 now: Clock.UtcNow,
