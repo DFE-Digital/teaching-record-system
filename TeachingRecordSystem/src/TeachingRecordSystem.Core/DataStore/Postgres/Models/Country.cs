caf4e01205d619263a6f23806aecf7eb89de40e0
Andrew Horth <andrew@horth.co.uk>
2025-02-27 16:59:42 +0000

    Populated routes reference data

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/Country.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/Country.cs
@@ -4,4 +4,6 @@ public class Country
 {
     public required string CountryId { get; init; }
     public required string Name { get; set; }
+    public required string OfficialName { get; set; }
+    public required string CitizenNames { get; set; }
 }
