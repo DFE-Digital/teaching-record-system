c6c3b30ab6883bd6b455d7da1e9619d38a609251
James Gunn <james@gunn.io>
2025-02-27 14:35:55 +0000

    Add a test that there are no DbContext changes not covered by a migration

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.Core.Tests/DataStore/Postgres/DbContextTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeachingRecordSystem.Core.Tests.DataStore.Postgres;
+
+public class DbContextTests(DbFixture dbFixture)
+{
+    [Fact]
+    public Task NoOutstandingMigrations() => dbFixture.WithDbContextAsync(dbContext =>
+    {
+        var migrationsAssembly = dbContext.GetService<IMigrationsAssembly>();
+
+        if (migrationsAssembly.ModelSnapshot is not null)
+        {
+            var snapshotModel = migrationsAssembly.ModelSnapshot.Model;
+
+            if (snapshotModel is IMutableModel mutableModel)
+            {
+                snapshotModel = mutableModel.FinalizeModel();
+            }
+
+            snapshotModel = dbContext.GetService<IModelRuntimeInitializer>().Initialize(snapshotModel);
+            var hasDifferences = dbContext.GetService<IMigrationsModelDiffer>().HasDifferences(
+                snapshotModel.GetRelationalModel(),
+                dbContext.GetService<IDesignTimeModel>().Model.GetRelationalModel());
+
+            Assert.False(hasDifferences, "DbContext has pending changes not covered by a migration.");
+        }
+
+        return Task.CompletedTask;
+    });
+}
