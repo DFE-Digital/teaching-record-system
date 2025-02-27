cabd076358041516f43467096cd27d3946544464
Kevin Joy <mrkevinjoy@outlook.com>
2025-02-27 18:45:26 +0000

    Added migration script to populated required fields for route types

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/RouteToProfessionalStatus.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/RouteToProfessionalStatus.cs
@@ -6,4 +6,14 @@ public class RouteToProfessionalStatus
     public required string Name { get; init; }
     public required ProfessionalStatusType ProfessionalStatusType { get; init; }
     public required bool IsActive { get; set; }
+    public required FieldRequirement TrainingStartDateRequired { get; init; }
+    public required FieldRequirement TrainingEndDateRequired { get; init; }
+    public required FieldRequirement AwardDateRequired { get; init; }
+    public required FieldRequirement InductionExemptionRequired { get; init; }
+    public required FieldRequirement TrainingProviderRequired { get; init; }
+    public required FieldRequirement DegreeTypeRequired { get; init; }
+    public required FieldRequirement TrainingCountryRequired { get; init; }
+    public required FieldRequirement TrainingAgeSpecialismTypeRequired { get; init; }
+    public required FieldRequirement TrainingSubjectsRequired { get; init; }
+
 }
