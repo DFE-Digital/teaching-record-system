76b7e1d0c8d59ffc811bb96bbdfd658cbc57d0ba
James Gunn <james@gunn.io>
2025-04-09 13:17:00 +0100

    Add separate propertes for each name on TrnRequestMetadata (#1934)
    We need to return names a first, middle and last so we should store them
    as such rather than in an array. The back-fill job will pick up
    populating these then we can make them non-nullable.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/TrnRequestMetadata.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/TrnRequestMetadata.cs
@@ -8,6 +8,9 @@ public class TrnRequestMetadata
     public required bool? IdentityVerified { get; init; }
     public required string? EmailAddress { get; init; }
     public required string? OneLoginUserSubject { get; init; }
+    public required string? FirstName { get; init; }
+    public required string? MiddleName { get; init; }
+    public required string? LastName { get; init; }
     public required string[] Name { get; init; }
     public required DateOnly DateOfBirth { get; init; }
     public bool? PotentialDuplicate { get; init; }
