c4b4ffba1f15c1534ad936d15e0f6ccb971d21f9
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2025-04-24 19:20:38 +0100

    Create new add users page

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Users/AddUser/Confirm.cshtml.cs
@@ -0,0 +1,121 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeachingRecordSystem.Core.DataStore.Postgres;
+using TeachingRecordSystem.SupportUi.Infrastructure.Filters;
+using TeachingRecordSystem.SupportUi.Infrastructure.Security;
+using TeachingRecordSystem.SupportUi.Services.AzureActiveDirectory;
+
+namespace TeachingRecordSystem.SupportUi.Pages.Users.AddUser;
+
+[Authorize(Policy = AuthorizationPolicies.UserManagement)]
+[RequireFeatureEnabledFilterFactory(FeatureNames.NewUserRoles)]
+public class ConfirmModel(
+    TrsDbContext dbContext,
+    IAadUserService userService,
+    IClock clock,
+    TrsLinkGenerator linkGenerator) : PageModel
+{
+    private Services.AzureActiveDirectory.User? _user;
+
+    [BindProperty(SupportsGet = true)]
+    public string? UserId { get; set; }
+
+    [Display(Name = "Email address")]
+    public string? Email { get; set; }
+
+    [BindProperty]
+    [Display(Name = "Name")]
+    [Required(ErrorMessage = "Enter a name")]
+    [MaxLength(Core.DataStore.Postgres.Models.User.NameMaxLength, ErrorMessage = "Name must be 200 characters or less")]
+    public string? Name { get; set; }
+
+    [BindProperty]
+    [Display(Name = "Role")]
+    [Required(ErrorMessage = "Select a role")]
+    public string? Role { get; set; }
+
+    public string? AzureAdUserId { get; set; }
+
+    public IEnumerable<UserRoleViewModel> RoleOptions { get; set; } = [];
+
+    public IActionResult OnGet()
+    {
+        Name = _user!.Name;
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        // Ensure submitted roles are valid
+        if (!string.IsNullOrWhiteSpace(Role) && !UserRoles.All.Contains(Role))
+        {
+            return BadRequest();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return this.PageWithErrors();
+        }
+
+        var newUser = new Core.DataStore.Postgres.Models.User()
+        {
+            Active = true,
+            AzureAdUserId = AzureAdUserId,
+            Email = _user!.Email,
+            Name = Name!,
+            Role = Role,
+            UserId = Guid.NewGuid()
+        };
+
+        dbContext.Users.Add(newUser);
+
+        await dbContext.AddEventAndBroadcastAsync(new UserAddedEvent()
+        {
+            EventId = Guid.NewGuid(),
+            User = Core.Events.Models.User.FromModel(newUser),
+            RaisedBy = User.GetUserId(),
+            CreatedUtc = clock.UtcNow
+        });
+
+        await dbContext.SaveChangesAsync();
+
+        var roleName = UserRoles.GetDisplayNameForRole(Role!);
+        roleName = roleName.Substring(0, 1).ToLowerInvariant() + roleName.Substring(1);
+        var article = new[] { 'a', 'e', 'i', 'o', 'u' }.Any(vowel => roleName.StartsWith(vowel)) ? "an" : "a";
+
+        TempData.SetFlashSuccess(message: $"{Name} has been added as {article} {roleName}.");
+        return Redirect(linkGenerator.Users());
+    }
+
+    public override async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+    {
+        _user = await userService.GetUserByIdAsync(UserId!);
+
+        if (_user is null)
+        {
+            context.Result = NotFound();
+            return;
+        }
+
+        Email = _user.Email;
+        AzureAdUserId = _user.UserId;
+
+        RoleOptions = UserRoles.All.Select(r => new UserRoleViewModel
+        {
+            Name = r,
+            DisplayName = UserRoles.GetDisplayNameForRole(r),
+            Permissions = UserRoles.GetPermissionsForRole(r).Select(p => new UserPermissionViewModel
+            {
+                Type = UserPermissionTypes.GetDisplayNameForPermissionType(p.Type),
+                Level = p.Level
+            })
+
+        });
+
+        await next();
+    }
+}
