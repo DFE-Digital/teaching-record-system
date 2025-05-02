377e608926413bace202406b024fb7d7c2433029
Kevin Joy <mrkevinjoy@outlook.com>
2025-05-02 13:14:05 +0100

    .

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/HostApplicationBuilderExtensions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/HostApplicationBuilderExtensions.cs
@@ -54,6 +54,7 @@ public static class HostApplicationBuilderExtensions
                 builder.Services.AddHttpClient<PopulateNameSynonymsJob>();
                 builder.Services.AddTransient<InductionStatusUpdatedSupportJob>();
                 builder.Services.AddTransient<CpdInductionImporterJob>();
+                builder.Services.AddTransient<BackfillDqtNotesJob>();
 
                 builder.Services.AddStartupTask(sp =>
                 {
