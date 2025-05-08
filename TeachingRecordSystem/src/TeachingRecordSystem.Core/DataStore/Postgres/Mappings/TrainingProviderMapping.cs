743c415bebd9b982111ad00dc83f61faf4990fcd
CLAWLOR <catherine.lawlor@education.gov.uk>
2025-05-08 12:42:34 +0100

    migration to allow nullable ukprn

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/TrainingProviderMapping.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/TrainingProviderMapping.cs
@@ -11,7 +11,7 @@ public class TrainingProviderMapping : IEntityTypeConfiguration<TrainingProvider
         builder.HasKey(x => x.TrainingProviderId);
         builder.HasIndex(x => x.Ukprn).HasDatabaseName(TrainingProvider.UkprnIndexName).IsUnique();
         builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
-        builder.Property(x => x.Ukprn).IsRequired().HasMaxLength(8).IsFixedLength();
+        builder.Property(x => x.Ukprn).HasMaxLength(8).IsFixedLength();
         builder.Property(x => x.IsActive).IsRequired();
     }
 }
