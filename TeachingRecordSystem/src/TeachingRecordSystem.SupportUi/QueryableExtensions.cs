4ef4949de311f2c1e290fe15658f8a5156f3fc01
James Gunn <james@gunn.io>
2025-05-08 18:12:25 +0100

    Add pagination to list of API TRN requests

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/QueryableExtensions.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace TeachingRecordSystem.SupportUi;
+
+public static class QueryableExtensions
+{
+    public static IOrderedQueryable<TSource> OrderBy<TSource, TKey>(
+        this IQueryable<TSource> source,
+        SortDirection direction,
+        Expression<Func<TSource, TKey>> keySelector)
+    {
+        return direction == SortDirection.Ascending ? source.OrderBy(keySelector) : source.OrderByDescending(keySelector);
+    }
+
+    public static IOrderedQueryable<TSource> ThenBy<TSource, TKey>(
+        this IOrderedQueryable<TSource> source,
+        SortDirection direction,
+        Expression<Func<TSource, TKey>> keySelector)
+    {
+        return direction == SortDirection.Ascending ? source.ThenBy(keySelector) : source.ThenByDescending(keySelector);
+    }
+
+    public static async Task<ResultPage<T>> GetPageAsync<T>(
+        this IQueryable<T> source,
+        int? currentPage,
+        int itemsPerPage)
+    {
+        var page = Math.Max(currentPage ?? 1, 1);
+
+        var total = await source.CountAsync();
+
+        var lastPage = ResultPage.GetLastPage(total, itemsPerPage);
+
+        // Is the page requested out of bounds?
+        if (page > lastPage)
+        {
+            page = lastPage;
+        }
+
+        var items = await source
+            .Skip((page - 1) * itemsPerPage)
+            .Take(itemsPerPage)
+            .ToArrayAsync();
+
+        return new ResultPage<T>(items.AsReadOnly(), page, total, itemsPerPage);
+    }
+}
+
+public class ResultPage<T>(IReadOnlyCollection<T> items, int currentPage, int totalItems, int itemsPerPage) : IReadOnlyCollection<T>
+{
+    public int LastPage => ResultPage.GetLastPage(totalItems, itemsPerPage);
+
+    public int CurrentPage => currentPage;
+
+    public int TotalItems => totalItems;
+
+    public int ItemsPerPage => itemsPerPage;
+
+    public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public int Count => items.Count;
+}
+
+public static class ResultPage
+{
+    public static int GetLastPage(int totalItems, int itemsPerPage) =>
+        Math.Max(1, (int)Math.Ceiling(totalItems / (decimal)itemsPerPage));
+}
