b1d97e3bfa1f2edda64d1bd3f7641b134c40be55
Kevin Joy <mrkevinjoy@outlook.com>
2025-05-14 15:01:16 +0100

    .

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtOutbox/Messages/AddWelshRMessage.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtOutbox/Messages/AddWelshRMessage.cs
@@ -10,13 +10,5 @@ public class AddWelshRMessage
 {
     public required Guid PersonId { get; set; }
     public required DateOnly? AwardedDate { get; set; }
-    public required List<Guid> Subjects { get; set; }
-    public required Guid? TrainingProviderId { get; set; }
-    public required DateOnly? TrainingStartDate { get; set; }
-    public required DateOnly? TrainingEndDate { get; set; }
-    public required int? TrainingAgeSpecialismRangeFrom { get; set; }
-    public required int? TrainingAgeSpecialismRangeTo { get; set; }
-    public required string? TrainingCountryId { get; set; }
-    public required TrainingAgeSpecialismType? TrainingAgeSpecialismType { get; set; }
 }
 
