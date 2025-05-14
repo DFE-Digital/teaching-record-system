b2089c40a26ab3f75c49673d2f6d8a175de44a8c
James Gunn <james@gunn.io>
2025-05-14 11:34:13 +0100

    Add a WaitForJobToComplete method to IBackgroundJobScheduler

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/Scheduling/HangfireBackgroundJobScheduler.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/Scheduling/HangfireBackgroundJobScheduler.cs
@@ -1,26 +1,50 @@
 using System.Linq.Expressions;
 using Hangfire;
+using Hangfire.States;
+using Npgsql;
 
 namespace TeachingRecordSystem.Core.Jobs.Scheduling;
 
-public class HangfireBackgroundJobScheduler : IBackgroundJobScheduler
+public class HangfireBackgroundJobScheduler(IBackgroundJobClient jobClient, NpgsqlDataSource dataSource) : IBackgroundJobScheduler
 {
-    private readonly IBackgroundJobClient _jobClient;
-
-    public HangfireBackgroundJobScheduler(IBackgroundJobClient jobClient)
-    {
-        _jobClient = jobClient;
-    }
-
     public Task<string> EnqueueAsync<T>(Expression<Func<T, Task>> expression) where T : notnull
     {
-        var jobId = _jobClient.Enqueue(expression);
+        var jobId = jobClient.Enqueue(expression);
         return Task.FromResult(jobId);
     }
 
     public Task<string> ContinueJobWithAsync<T>(string parentId, Expression<Func<T, Task>> expression) where T : notnull
     {
-        var jobId = _jobClient.ContinueJobWith(parentId, expression);
+        var jobId = jobClient.ContinueJobWith(parentId, expression);
         return Task.FromResult(jobId);
+    }
+
+    public async Task WaitForJobToCompleteAsync(string jobId, CancellationToken cancellationToken)
+    {
+        await using var conn = dataSource.CreateConnection();
+        await conn.OpenAsync(cancellationToken);
+
+        await using var cmd = dataSource.CreateCommand();
+        cmd.CommandText = "select statename from hangfire.job where id = @id";
+        cmd.Parameters.AddWithValue("@id", long.Parse(jobId));
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await using var reader = await cmd.ExecuteReaderAsync(cancellationToken);
+
+            if (!await reader.ReadAsync(cancellationToken))
+            {
+                throw new ArgumentException("Job does not exist.");
+            }
+
+            var stateName = reader.GetString(0);
+
+            if (stateName == FailedState.StateName || stateName == SucceededState.StateName)
+            {
+                return;
+            }
+
+            await Task.Delay(100, cancellationToken);
+        }
     }
 }
