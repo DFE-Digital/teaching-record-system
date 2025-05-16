269baf131fd395331af6cc6187612ba2f83b4b40
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2025-05-16 11:10:37 +0100

    Rename columns and table dqt_notes to notes (#2039)
    rename dqt_notes table and rename some columns to accommodate for notes
    that originate from dqt or trs.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/DqtNoteMapping.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/DqtNoteMapping.cs
@@ -3,16 +3,16 @@ using TeachingRecordSystem.Core.DataStore.Postgres.Models;
 
 namespace TeachingRecordSystem.Core.DataStore.Postgres.Mappings;
 
-public class DqtNoteMapping : IEntityTypeConfiguration<DqtNote>
+public class DqtNoteMapping : IEntityTypeConfiguration<Note>
 {
-    public void Configure(EntityTypeBuilder<DqtNote> builder)
+    public void Configure(EntityTypeBuilder<Note> builder)
     {
-        builder.ToTable("dqt_notes");
-        builder.HasKey(x => x.Id);
+        builder.ToTable("notes");
+        builder.HasKey(x => x.NoteId);
         builder.Property(x => x.PersonId).IsRequired();
-        builder.Property(x => x.NoteText).IsRequired();
-        builder.Property(x => x.CreatedByDqtUserId).IsRequired();
-        builder.Property(x => x.CreatedByDqtUserName).IsRequired();
+        builder.Property(x => x.ContentHtml).IsRequired();
+        builder.Property(x => x.CreatedByDqtUserId).IsRequired(false);
+        builder.Property(x => x.CreatedByDqtUserName).IsRequired(false);
         builder.Property(x => x.CreatedOn).IsRequired();
         builder.HasOne<Person>().WithMany().HasForeignKey(q => q.PersonId);
     }
