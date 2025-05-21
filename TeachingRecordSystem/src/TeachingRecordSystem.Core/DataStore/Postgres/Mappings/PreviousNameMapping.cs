3e478cbafdde5997ddfa58ec8a6bac3026b84cda
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2025-05-21 15:50:08 +0100

    wip

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Mappings/PreviousNameMapping.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeachingRecordSystem.Core.DataStore.Postgres.Models;
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Mappings;
+
+public class PersonMapping : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("persons");
+        builder.HasKey(p => p.PersonId);
+        builder.HasIndex(p => p.DqtContactId).HasFilter("dqt_contact_id is not null").IsUnique();
+        builder.HasIndex(p => p.Trn).HasFilter("trn is not null").IsUnique();
+        builder.Property(p => p.Trn).HasMaxLength(7).IsFixedLength();
+        builder.Property(p => p.FirstName).HasMaxLength(Person.FirstNameMaxLength).UseCollation("case_insensitive");
+        builder.Property(p => p.MiddleName).HasMaxLength(Person.MiddleNameMaxLength).UseCollation("case_insensitive");
+        builder.Property(p => p.LastName).HasMaxLength(Person.LastNameMaxLength).UseCollation("case_insensitive");
+        builder.Property(p => p.EmailAddress).HasMaxLength(Person.EmailAddressMaxLength).UseCollation("case_insensitive");
+        builder.Property(p => p.MobileNumber).HasMaxLength(Person.MobileNumberMaxLength);
+        builder.Property(p => p.NationalInsuranceNumber).HasMaxLength(Person.NationalInsuranceNumberMaxLength).IsFixedLength();
+        builder.Property(p => p.DqtFirstName).HasMaxLength(100).UseCollation("case_insensitive");
+        builder.Property(p => p.DqtMiddleName).HasMaxLength(100).UseCollation("case_insensitive");
+        builder.Property(p => p.DqtLastName).HasMaxLength(100).UseCollation("case_insensitive");
+        builder.Property(p => p.InductionStatus).IsRequired().HasDefaultValue(InductionStatus.None);
+        builder.Property(p => p.InductionExemptionReasonIds).IsRequired();
+        builder.Property(p => p.InductionStatusWithoutExemption).IsRequired();
+    }
+}
---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/PreviousName.cs
@@ -0,0 +1,33 @@
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Models;
+
+public class PreviousName
+{
+    public required Guid PreviousNameId { get; init; }
+    public required DateTime? CreatedOn { get; init; }
+    public required DateTime? UpdatedOn { get; set; }
+    public DateTime? DeletedOn { get; set; }
+    public required string FirstName { get; set; }
+    public required string? MiddleName { get; set; }
+    public required string LastName { get; set; }
+
+    public Guid? DqtFirstNamePreviousNameId { get; init; }
+    public DateTime? DqtFirstNameFirstSync { get; set; }
+    public DateTime? DqtFirstNameLastSync { get; set; }
+    public int? DqtFirstNameState { get; set; }
+    public DateTime? DqtFirstNameCreatedOn { get; set; }
+    public DateTime? DqtFirstNameModifiedOn { get; set; }
+
+    public Guid? DqtMiddleNamePreviousNameId { get; init; }
+    public DateTime? DqtMiddleNameFirstSync { get; set; }
+    public DateTime? DqtMiddleNameLastSync { get; set; }
+    public int? DqtMiddleNameState { get; set; }
+    public DateTime? DqtMiddleNameCreatedOn { get; set; }
+    public DateTime? DqtMiddleNameModifiedOn { get; set; }
+
+    public Guid? DqtLastNamePreviousNameId { get; init; }
+    public DateTime? DqtLastNameFirstSync { get; set; }
+    public DateTime? DqtLastNameLastSync { get; set; }
+    public int? DqtLastNameState { get; set; }
+    public DateTime? DqtLastNameCreatedOn { get; set; }
+    public DateTime? DqtLastNameModifiedOn { get; set; }
+}
