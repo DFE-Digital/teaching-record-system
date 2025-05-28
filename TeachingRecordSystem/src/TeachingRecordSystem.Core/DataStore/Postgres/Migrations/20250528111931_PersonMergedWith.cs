b45aed0a9d4feef93d632435e7f8ce9402694c25
James Gunn <james@gunn.io>
2025-05-28 12:01:05 +0100

    Add MergedWithPersonId to Person & sync it from DQT

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250528111931_PersonMergedWith.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class PersonMergedWith : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "merged_with_person_id",
+                table: "persons",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_persons_persons_merged_with_person_id",
+                table: "persons",
+                column: "merged_with_person_id",
+                principalTable: "persons",
+                principalColumn: "person_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_persons_persons_merged_with_person_id",
+                table: "persons");
+
+            migrationBuilder.DropColumn(
+                name: "merged_with_person_id",
+                table: "persons");
+        }
+    }
+}
