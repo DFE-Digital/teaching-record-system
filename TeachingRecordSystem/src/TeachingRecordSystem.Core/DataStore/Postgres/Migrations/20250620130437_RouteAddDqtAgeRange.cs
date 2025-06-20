61236051b9f65161c9288276cc70c2b7e8c8b311
Andrew Horth <andrew@horth.co.uk>
2025-06-20 14:11:43 +0100

    added new fields to capture the DQT age ranges used to derive the route age ranges in TRS

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250620130437_RouteAddDqtAgeRange.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class RouteAddDqtAgeRange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "dqt_age_range_from",
+                table: "qualifications",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "dqt_age_range_to",
+                table: "qualifications",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dqt_age_range_from",
+                table: "qualifications");
+
+            migrationBuilder.DropColumn(
+                name: "dqt_age_range_to",
+                table: "qualifications");
+        }
+    }
+}
---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtReporting/Migrations/0072_TrsQualificationsDqtAgeRange.sql
@@ -0,0 +1,2 @@
+alter table trs_qualifications add dqt_age_range_from varchar(16)
+alter table trs_qualifications add dqt_age_range_to varchar(16)
