efab491a0395a953bb373f5b0b5e0c14468aad1c
James Gunn <james@gunn.io>
2025-06-27 12:37:34 +0100

    Remove old feature flags (#2161)

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/FeatureNames.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/FeatureNames.cs
@@ -2,8 +2,6 @@ namespace TeachingRecordSystem.SupportUi;
 
 public static class FeatureNames
 {
-    public const string Alerts = "Alerts";
-    public const string Induction = "Induction";
     public const string SwitchRoles = "SwitchRoles";
     public const string RoutesToProfessionalStatus = "RoutesToProfessionalStatus";
     public const string NewUserRoles = "NewUserRoles";
