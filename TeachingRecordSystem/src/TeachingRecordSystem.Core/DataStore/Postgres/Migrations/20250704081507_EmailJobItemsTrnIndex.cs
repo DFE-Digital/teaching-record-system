7a1f9514beb878b84e6c188ab57945f77d73a91c
James Gunn <james@gunn.io>
2025-07-04 09:15:28 +0100

    Add indexes to trn column on email job item tables

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250704081507_EmailJobItemsTrnIndex.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailJobItemsTrnIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_qts_awarded_emails_job_items_trn",
+                table: "qts_awarded_emails_job_items",
+                column: "trn");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_international_qts_awarded_emails_job_items_trn",
+                table: "international_qts_awarded_emails_job_items",
+                column: "trn");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_induction_completed_emails_job_items_trn",
+                table: "induction_completed_emails_job_items",
+                column: "trn");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_eyts_awarded_emails_job_items_trn",
+                table: "eyts_awarded_emails_job_items",
+                column: "trn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_qts_awarded_emails_job_items_trn",
+                table: "qts_awarded_emails_job_items");
+
+            migrationBuilder.DropIndex(
+                name: "ix_international_qts_awarded_emails_job_items_trn",
+                table: "international_qts_awarded_emails_job_items");
+
+            migrationBuilder.DropIndex(
+                name: "ix_induction_completed_emails_job_items_trn",
+                table: "induction_completed_emails_job_items");
+
+            migrationBuilder.DropIndex(
+                name: "ix_eyts_awarded_emails_job_items_trn",
+                table: "eyts_awarded_emails_job_items");
+        }
+    }
+}
