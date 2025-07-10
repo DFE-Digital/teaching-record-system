4f46ef4cbc657d6f758a17d70f6ed573152f812e
James Gunn <james@gunn.io>
2025-07-10 11:05:38 +0100

    Fix MQ DTO mapping (#2210)

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/V20240606/Responses/GetPersonResponse.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/V3/V20240606/Responses/GetPersonResponse.cs
@@ -133,6 +133,7 @@ public record GetPersonResponseNpqQualificationType
 [AutoMap(typeof(GetPersonResultMandatoryQualification))]
 public record GetPersonResponseMandatoryQualification
 {
+    [SourceMember("EndDate")]
     public required DateOnly Awarded { get; init; }
     public required string Specialism { get; init; }
 }
