1f3f64d12cfccc86e6093ddb0bb7f97fafabede9
Andrew Horth <andrew@horth.co.uk>
2025-07-24 15:50:05 +0100

    Adde job to backfill DQT ITT & QTS related events in TRS

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/HostApplicationBuilderExtensions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/HostApplicationBuilderExtensions.cs
@@ -221,6 +221,16 @@ public static class HostApplicationBuilderExtensions
                     job => job.ExecuteAsync(CancellationToken.None),
                     Cron.Never);
 
+                recurringJobManager.AddOrUpdate<BackfillDqtIttQtsBusinessEventAuditsJob>(
+                    $"{nameof(BackfillDqtIttQtsBusinessEventAuditsJob)} (dry-run)",
+                    job => job.ExecuteAsync(/*dryRun: */true, CancellationToken.None),
+                    Cron.Never);
+
+                recurringJobManager.AddOrUpdate<BackfillDqtIttQtsBusinessEventAuditsJob>(
+                    nameof(BackfillDqtIttQtsBusinessEventAuditsJob),
+                    job => job.ExecuteAsync(/*dryRun: */false, CancellationToken.None),
+                    Cron.Never);
+
                 return Task.CompletedTask;
             });
         }
