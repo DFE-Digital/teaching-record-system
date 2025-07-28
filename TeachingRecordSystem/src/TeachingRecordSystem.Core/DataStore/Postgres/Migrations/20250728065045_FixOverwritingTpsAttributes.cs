bdac16db13728f7dba0dd85a9f6cf940c678448e
James Gunn <james@gunn.io>
2025-07-28 20:41:14 +0100

    Fix clearing TPS-sourced person_search_attributes when person is changed (#2279)
    The previous version had `DELETE FROM person_search_attributes WHERE
    person_id = ANY(p_person_ids);` which clears out
    `person_search_attributes` not (re-)populated by this procedure.
    
    This also changes to using the `merge` statement now we're on Postgres
    17.

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250728065045_FixOverwritingTpsAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixOverwritingTpsAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Procedure("p_refresh_person_search_attributes_v5.sql");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Procedure("p_refresh_person_search_attributes_v4.sql");
+        }
+    }
+}
