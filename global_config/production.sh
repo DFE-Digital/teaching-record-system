273d4d27663c007e7cb283e861e687faa94564a0
James Gunn <james@gunn.io>
2025-07-28 10:03:17 +0100

    Temporarily ignore postgres DB version changes in deploys

--- global_config/production.sh
+++ global_config/production.sh
@@ -2,4 +2,4 @@ CONFIG=production
 CONFIG_SHORT=pd
 AZURE_SUBSCRIPTION=s189-teacher-services-cloud-production
 AZURE_RESOURCE_PREFIX=s189p01
-TERRAFORM_MODULES_TAG=stable
+TERRAFORM_MODULES_TAG=skip-postgres-version-changes
