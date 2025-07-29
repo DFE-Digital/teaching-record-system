6ce4ec29f559f111ae8ac99fd849b133b3121530
James Gunn <james@gunn.io>
2025-07-29 16:26:12 +0100

    Sync previous names from DQT into TRS

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/PreviousName.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Models/PreviousName.cs
@@ -8,10 +8,12 @@ public class PreviousName
     public required Guid PreviousNameId { get; init; }
     public required Guid PersonId { get; init; }
     public Person? Person { get; }
-    public required DateTime? CreatedOn { get; init; }
-    public required DateTime? UpdatedOn { get; set; }
+    public required DateTime CreatedOn { get; init; }
+    public required DateTime UpdatedOn { get; set; }
     public DateTime? DeletedOn { get; set; }
     public required string FirstName { get; set; }
     public required string MiddleName { get; set; }
     public required string LastName { get; set; }
+    public Guid[]? DqtPreviousNameIds { get; init; }
+    public Guid? DqtAuditId { get; init; }
 }
