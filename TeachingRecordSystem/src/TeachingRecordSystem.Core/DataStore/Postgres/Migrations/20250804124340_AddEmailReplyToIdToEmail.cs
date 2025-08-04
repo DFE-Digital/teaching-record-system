0f4ce7bdf4e7160ba32a53e6e732906e62d788b5
Andrew Horth <andrew@horth.co.uk>
2025-08-04 13:48:17 +0100

    Re-added db migration to add email reply to id

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250804124340_AddEmailReplyToIdToEmail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailReplyToIdToEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email_reply_to_id",
+                table: "emails",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_reply_to_id",
+                table: "emails");
+        }
+    }
+}
