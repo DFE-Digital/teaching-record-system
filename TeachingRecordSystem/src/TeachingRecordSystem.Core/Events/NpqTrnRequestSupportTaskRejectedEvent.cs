0c9305a60631e37fb226f7f0dc70d5b04b27fcc7
CLAWLOR <catherine.lawlor@education.gov.uk>
2025-08-11 08:48:42 +0100

    PR comments

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/NpqTrnRequestSupportTaskRejectedEvent.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/NpqTrnRequestSupportTaskRejectedEvent.cs
@@ -3,6 +3,6 @@ namespace TeachingRecordSystem.Core.Events;
 public record NpqTrnRequestSupportTaskRejectedEvent : SupportTaskUpdatedEvent
 {
     public required EventModels.TrnRequestMetadata RequestData { get; init; }
-    public string? RejectionReason { get; init; }
+    public required string? RejectionReason { get; init; }
 }
 
--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/NpqTrnRequests/Reject/RejectNpqTrnRequestState.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/NpqTrnRequests/Reject/RejectNpqTrnRequestState.cs
@@ -2,8 +2,6 @@ namespace TeachingRecordSystem.SupportUi.Pages.SupportTasks.NpqTrnRequests.Rejec
 
 public class RejectNpqTrnRequestState : IRegisterJourney
 {
-    public static Guid CreateNewRecordPersonIdSentinel => Guid.Empty;
-
     public static JourneyDescriptor Journey { get; } = new(
         JourneyNames.RejectNpqTrnRequest,
         typeof(RejectNpqTrnRequestState),
