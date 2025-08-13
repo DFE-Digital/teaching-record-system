3ba4eae2b9c644218a9f4fe608cbbca6c3785a45
James Gunn <james@gunn.io>
2025-08-13 13:41:47 +0100

    Use ReferenceDataCache for training provider endpoint

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/V2/Handlers/GetIttProvidersHandler.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/V2/Handlers/GetIttProvidersHandler.cs
@@ -1,20 +1,19 @@
 using MediatR;
 using TeachingRecordSystem.Api.V2.Requests;
 using TeachingRecordSystem.Api.V2.Responses;
-using TeachingRecordSystem.Core.DataStore.Postgres;
 
 namespace TeachingRecordSystem.Api.V2.Handlers;
 
-public class GetIttProvidersHandler(TrsDbContext dbContext) : IRequestHandler<GetIttProvidersRequest, GetIttProvidersResponse>
+public class GetIttProvidersHandler(ReferenceDataCache referenceDataCache) : IRequestHandler<GetIttProvidersRequest, GetIttProvidersResponse>
 {
     public async Task<GetIttProvidersResponse> Handle(GetIttProvidersRequest request, CancellationToken cancellationToken)
     {
-        var ittProviders = await dbContext.TrainingProviders
+        var ittProviders = (await referenceDataCache.GetTrainingProvidersAsync(activeOnly: false))
             .Where(p => p.Ukprn != null)
             .OrderBy(p => p.Name)
             .ThenBy(p => p.Ukprn)
             .Select(p => new IttProviderInfo() { ProviderName = p.Name, Ukprn = p.Ukprn! })
-            .ToArrayAsync();
+            .ToArray();
 
         return new GetIttProvidersResponse()
         {
