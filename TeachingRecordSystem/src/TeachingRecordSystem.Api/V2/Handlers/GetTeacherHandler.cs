d733b518862c1b78645d95fe3bfa70f36a6def16
James Gunn <james@gunn.io>
2025-08-15 12:15:08 +0100

    Rewire V2 Get Teacher endpoint onto TRS (#2369)

--- TeachingRecordSystem/src/TeachingRecordSystem.Api/V2/Handlers/GetTeacherHandler.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Api/V2/Handlers/GetTeacherHandler.cs
@@ -3,57 +3,35 @@ using MediatR;
 using TeachingRecordSystem.Api.V2.Requests;
 using TeachingRecordSystem.Api.V2.Responses;
 using TeachingRecordSystem.Core.DataStore.Postgres;
-using TeachingRecordSystem.Core.Dqt;
-using TeachingRecordSystem.Core.Dqt.Models;
 
 namespace TeachingRecordSystem.Api.V2.Handlers;
 
-public class GetTeacherHandler(IDataverseAdapter dataverseAdapter, TrsDbContext dbContext) :
+public class GetTeacherHandler(TrsDbContext dbContext) :
     IRequestHandler<GetTeacherRequest, GetTeacherResponse>
 {
     public async Task<GetTeacherResponse> Handle(GetTeacherRequest request, CancellationToken cancellationToken)
     {
-        var teacher = await dataverseAdapter.GetTeacherByTrnAsync(
-            request.Trn,
-            columnNames: new[]
-            {
-                Contact.Fields.FirstName,
-                Contact.Fields.LastName,
-                Contact.Fields.MiddleName,
-                Contact.Fields.BirthDate,
-                Contact.Fields.dfeta_EYTSDate,
-                Contact.Fields.dfeta_QTSDate,
-                Contact.Fields.dfeta_NINumber,
-                Contact.Fields.dfeta_TRN,
-                Contact.Fields.dfeta_HUSID,
-                Contact.Fields.dfeta_AllowPiiUpdatesFromRegister
-            },
-            activeOnly: true);
+        var person = await dbContext.Persons
+            .Include(p => p.Alerts).AsSplitQuery()
+            .SingleOrDefaultAsync(p => p.Trn == request.Trn);
 
-        if (teacher is null)
-        {
-            return null;
-        }
-
-        var person = await dbContext.Persons.SingleAsync(p => p.PersonId == teacher.Id);
-
-        var hasActiveAlert = await dbContext.Alerts.Where(a => a.PersonId == teacher.Id && a.IsOpen).AnyAsync();
+        var hasActiveAlert = person.Alerts!.Any(a => a.IsOpen);
 
         return new GetTeacherResponse()
         {
-            DateOfBirth = teacher.BirthDate.ToDateOnlyWithDqtBstFix(isLocalTime: false),
-            FirstName = teacher.FirstName,
+            DateOfBirth = person.DateOfBirth,
+            FirstName = person.FirstName,
             HasActiveSanctions = hasActiveAlert,
-            LastName = teacher.LastName,
-            MiddleName = teacher.MiddleName,
-            NationalInsuranceNumber = teacher.dfeta_NINumber,
-            Trn = teacher.dfeta_TRN,
+            LastName = person.LastName,
+            MiddleName = person.MiddleName,
+            NationalInsuranceNumber = person.NationalInsuranceNumber,
+            Trn = person.Trn,
             QtsDate = person.QtsDate,
             EytsDate = person.EytsDate,
-            HusId = teacher.dfeta_HUSID,
+            HusId = null,
             EarlyYearsStatus = null,
             InitialTeacherTraining = [],
-            AllowPIIUpdates = teacher.dfeta_AllowPiiUpdatesFromRegister ?? false
+            AllowPIIUpdates = person.AllowDetailsUpdatesFromSourceApplication
         };
     }
 }
