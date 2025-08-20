01abd5cb5367da4e74a5d55daae4dfac2122f637
James Gunn <james@gunn.io>
2025-08-20 11:55:40 +0100

    Fix CreatePersonMigratedEventsJob (#2391)
    Fixes the 'Trn' property in the created events and join should speed up
    the job too.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/CreatePersonMigratedEventsJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/CreatePersonMigratedEventsJob.cs
@@ -16,27 +16,26 @@ public class CreatePersonMigratedEventsJob(NpgsqlDataSource dataSource)
                     'PersonMigratedEvent' as event_name,
                     (now() at time zone 'utc') as created,
                     json_build_object(
-                        'PersonId', person_id,
-                        'Trn', 'trn',
+                        'PersonId', p.person_id,
+                        'Trn', p.trn,
                         'PersonAttributes', json_build_object(
-                            'FirstName', first_name,
-                            'MiddleName', middle_name,
-                            'LastName', last_name,
-                            'DateOfBirth', date_of_birth,
-                            'EmailAddress', email_address,
-                            'NationalInsuranceNumber', national_insurance_number,
-                            'Gender', gender
+                            'FirstName', p.first_name,
+                            'MiddleName', p.middle_name,
+                            'LastName', p.last_name,
+                            'DateOfBirth', p.date_of_birth,
+                            'EmailAddress', p.email_address,
+                            'NationalInsuranceNumber', p.national_insurance_number,
+                            'Gender', p.gender
                         )
                     ) as payload,
                     gen_random_uuid() as event_id,
                     (now() at time zone 'utc') as inserted,
-                    person_id,
-                    ARRAY[person_id] as person_ids,
+                    p.person_id,
+                    ARRAY[p.person_id] as person_ids,
                     false as published
-                from persons
-                where person_id not in (
-                    select person_id from events where event_name = 'PersonMigratedEvent'
-                )
+                from persons p
+                left join events e on p.person_id = e.person_id and e.event_name = 'PersonMigratedEvent'
+                where e.person_id is null
                 limit 5000
                 """);
             cmd.CommandTimeout = 0;
