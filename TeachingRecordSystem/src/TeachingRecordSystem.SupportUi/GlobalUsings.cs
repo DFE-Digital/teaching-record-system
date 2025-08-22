91405a98752f283497937822dc5ff654d45b0374
James Gunn <james@gunn.io>
2025-08-22 11:08:15 +0100

    Move xUnit-dependent helper methods into separate projects (#2407)
    So we can transition to TUnit we need to separate out our
    xUnit-dependent helper methods. This adds `TestCommon.XUnit` and
    `UiTestCommon.XUnit`.

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/GlobalUsings.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/GlobalUsings.cs
@@ -2,5 +2,4 @@ global using GovUk.Frontend.AspNetCore;
 global using TeachingRecordSystem.Core.Dqt;
 global using TeachingRecordSystem.WebCommon.FormFlow;
 global using ColumnSet = Microsoft.Xrm.Sdk.Query.ColumnSet;
-global using CrmTask = TeachingRecordSystem.Core.Dqt.Models.Task;
 global using UiDefaults = TeachingRecordSystem.SupportUi.Pages.Common.UiDefaults;
