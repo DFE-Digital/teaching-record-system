531c1f42ea4db30f80d230916c0839b1b33bb626
James Gunn <james@gunn.io>
2025-08-29 10:20:22 +0100

    Fix lint error

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/GlobalUsings.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/GlobalUsings.cs
@@ -1,5 +1,4 @@
 global using GovUk.Frontend.AspNetCore;
 global using TeachingRecordSystem.Core.Dqt;
 global using TeachingRecordSystem.WebCommon.FormFlow;
-global using ColumnSet = Microsoft.Xrm.Sdk.Query.ColumnSet;
 global using UiDefaults = TeachingRecordSystem.SupportUi.Pages.Common.UiDefaults;
