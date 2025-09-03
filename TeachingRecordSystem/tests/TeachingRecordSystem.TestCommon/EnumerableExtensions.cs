7df8557444eaa8e855abd49f55b6629ea17512a4
James Gunn <james@gunn.io>
2025-09-03 12:43:02 +0100

    Remove some un-used methods (#2477)
    Also simplifies a method to remove a nullability annotation warning.

--- TeachingRecordSystem/tests/TeachingRecordSystem.TestCommon/EnumerableExtensions.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.TestCommon/EnumerableExtensions.cs
@@ -8,15 +8,6 @@ public static class EnumerableExtensions
     public static T RandomOne<T>(this IEnumerable<T> items) =>
         items.OrderBy(_ => Guid.NewGuid()).First();
 
-    public static T RandomOneExcept<T>(this IEnumerable<T> values, Predicate<T> exclude)
-    {
-        var options = values;
-
-        if (exclude is not null)
-        {
-            options = options.Where(o => !exclude(o));
-        }
-
-        return options.RandomOne();
-    }
+    public static T RandomOneExcept<T>(this IEnumerable<T> values, Predicate<T> exclude) =>
+        values.Where(o => !exclude(o)).RandomOne();
 }
