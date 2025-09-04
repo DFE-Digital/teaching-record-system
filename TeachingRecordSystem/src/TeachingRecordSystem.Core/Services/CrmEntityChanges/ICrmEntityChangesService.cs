b9bc5db490cd401d3155755a675111f8a999e28f
James Gunn <james@gunn.io>
2025-09-04 19:13:21 +0100

    Resurrect DQT sync to reporting DB (#2468)
    DQT is still being used for a few reporting things so this needs to come
    back for a while.

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/CrmEntityChanges/ICrmEntityChangesService.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace TeachingRecordSystem.Core.Services.CrmEntityChanges;
+
+public interface ICrmEntityChangesService
+{
+    IAsyncEnumerable<IChangedItem[]> GetEntityChangesAsync(
+        string changesKey,
+        string entityLogicalName,
+        ColumnSet columns,
+        DateTime? modifiedSince,
+        int pageSize = 1000,
+        bool rollUpChanges = true,
+        CancellationToken cancellationToken = default);
+}
