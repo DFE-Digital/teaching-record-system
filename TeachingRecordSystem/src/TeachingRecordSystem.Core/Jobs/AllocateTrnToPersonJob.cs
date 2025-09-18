46645e2cc04cfd5bef93acb139be50c83bf5b247
Andrew Horth <andrew@horth.co.uk>
2025-09-18 11:04:47 +0100

    Added job to allocate a TRN to a specific person (#2545)
    ### Context
    
    We need to assign a TRN to this record to enable support to proceed with
    their work.
    The simplest way to do this is a one-off job.

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/AllocateTrnToPersonJob.cs
@@ -0,0 +1,37 @@
+using TeachingRecordSystem.Core.DataStore.Postgres;
+using TeachingRecordSystem.Core.Services.TrnGeneration;
+
+namespace TeachingRecordSystem.Core.Jobs;
+
+public class AllocateTrnToPersonJob(
+    TrsDbContext dbContext,
+    ITrnGenerator trnGenerator,
+    IClock clock)
+{
+    public async Task ExecuteAsync(bool dryRun, CancellationToken cancellationToken)
+    {
+        await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+        var person = await dbContext.Persons
+            .Where(p => p.PersonId == Guid.Parse("5057496c-849f-4433-915f-b1b46e1855e8"))
+            .SingleAsync(cancellationToken);
+
+        var newTrn = await trnGenerator.GenerateTrnAsync();
+        person.Trn = newTrn;
+        dbContext.AddEventWithoutBroadcast(new TrnAllocatedEvent()
+        {
+            EventId = Guid.NewGuid(),
+            CreatedUtc = clock.UtcNow,
+            PersonId = person.PersonId,
+            RaisedBy = DataStore.Postgres.Models.SystemUser.SystemUserId,
+            Trn = newTrn
+        });
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        if (!dryRun)
+        {
+            await transaction.CommitAsync(cancellationToken);
+        }
+    }
+}
