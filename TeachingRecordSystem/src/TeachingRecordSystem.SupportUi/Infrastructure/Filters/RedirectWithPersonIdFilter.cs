74c1841d76254e3b7ea5a13d1efe78645cc440f1
James Gunn <james@gunn.io>
2025-09-24 17:47:32 +0100

    Support using TRN in URL instead of Person ID

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Infrastructure/Filters/RedirectWithPersonIdFilter.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TeachingRecordSystem.Core.DataStore.Postgres;
+
+namespace TeachingRecordSystem.SupportUi.Infrastructure.Filters;
+
+public partial class RedirectWithPersonIdFilter(TrsDbContext dbContext) : IAsyncResourceFilter
+{
+    [GeneratedRegex(@"^\/persons\/([0-9]{7})($|\/)")]
+    private static partial Regex _personWithTrnPathRegex();
+
+    public static int Order => -1000;
+
+    public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+    {
+        var matched = _personWithTrnPathRegex().Match(context.HttpContext.Request.Path);
+
+        if (matched.Success)
+        {
+            var trn = matched.Groups[1].Value;
+
+            var person = await dbContext.Persons
+                .Where(p => p.Trn == trn)
+                .Select(p => new { p.PersonId })
+                .SingleOrDefaultAsync();
+
+            if (person is not null)
+            {
+                var newUrl = context.HttpContext.Request.Path.ToString().Replace($"/persons/{trn}", $"/persons/{person.PersonId}");
+                context.Result = new RedirectResult(newUrl, permanent: false, preserveMethod: true);
+                return;
+            }
+        }
+
+        await next();
+    }
+}
