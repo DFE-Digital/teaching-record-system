008ffe3620aaf2366e43cd0e070300abd1cd4629
James Gunn <james@gunn.io>
2025-09-26 09:53:59 +0100

    Add missing property to support task updated events (#2586)

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/DataStore/Postgres/Migrations/20250926082733_BackfillSupportTaskTypeInData2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeachingRecordSystem.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillSupportTaskTypeInData2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                update events set
+                    payload = jsonb_set(
+                        payload,
+                        '{SupportTask,Data,\$support-task-type}',
+                         (payload->'SupportTask'->>'SupportTaskType')::jsonb)
+                where event_name in (
+                    'ApiTrnRequestSupportTaskUpdatedEvent',
+                    'ChangeDateOfBirthRequestSupportTaskApprovedEvent',
+                    'ChangeDateOfBirthRequestSupportTaskCancelledEvent',
+                    'ChangeDateOfBirthRequestSupportTaskRejectedEvent',
+                    'ChangeNameRequestSupportTaskApprovedEvent',
+                    'ChangeNameRequestSupportTaskCancelledEvent',
+                    'ChangeNameRequestSupportTaskRejectedEvent',
+                    'NpqTrnRequestSupportTaskRejectedEvent',
+                    'NpqTrnRequestSupportTaskResolvedEvent',
+                    'TeacherPensionsPotentialDuplicateSupportTaskResolvedEvent'
+                );
+
+                update events set
+                    payload = jsonb_set(
+                        payload,
+                        '{OldSupportTask,Data,\$support-task-type}',
+                         (payload->'OldSupportTask'->>'SupportTaskType')::jsonb)
+                where event_name in (
+                    'ApiTrnRequestSupportTaskUpdatedEvent',
+                    'ChangeDateOfBirthRequestSupportTaskApprovedEvent',
+                    'ChangeDateOfBirthRequestSupportTaskCancelledEvent',
+                    'ChangeDateOfBirthRequestSupportTaskRejectedEvent',
+                    'ChangeNameRequestSupportTaskApprovedEvent',
+                    'ChangeNameRequestSupportTaskCancelledEvent',
+                    'ChangeNameRequestSupportTaskRejectedEvent',
+                    'NpqTrnRequestSupportTaskRejectedEvent',
+                    'NpqTrnRequestSupportTaskResolvedEvent',
+                    'TeacherPensionsPotentialDuplicateSupportTaskResolvedEvent'
+                );
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
