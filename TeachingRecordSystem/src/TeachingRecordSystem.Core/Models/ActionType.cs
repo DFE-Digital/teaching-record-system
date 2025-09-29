2c26fe1c7ae031edf961944fab1e2fbd5caf1c7d
James Gunn <james@gunn.io>
2025-09-29 10:50:04 +0100

    Add new event models

---
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Models/ActionType.cs
@@ -0,0 +1,10 @@
+namespace TeachingRecordSystem.Core.Models;
+
+public enum ActionType
+{
+    RouteToProfessionalStatusCreated = 1,
+    RouteToProfessionalStatusUpdated = 2,
+    RouteToProfessionalStatusDeleted = 3,
+    ApiTrnRequestSupportTaskResolved = 4,
+    //etc..
+}
