2e8930b19d138f358788dd16a4d9f63437ff64ea
James Gunn <james@gunn.io>
2025-09-29 09:22:57 +0100

    Remove command timeout from job

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/DeleteStaleJourneyStatesJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/DeleteStaleJourneyStatesJob.cs
@@ -5,15 +5,15 @@ namespace TeachingRecordSystem.Core.Jobs;
 public class DeleteStaleJourneyStatesJob(TrsDbContext dbContext, IClock clock)
 {
     public const string JobSchedule = "0 3 * * *";  //3AM every day
-    public const string LastRunDateKey = "LastRunDate";
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        var cutoffDate = clock.Today.AddDays(-1).ToDateTime();
-        var staleJourneyStates = await dbContext.JourneyStates
-           .Where(e => e.Created < cutoffDate)
-           .ExecuteDeleteAsync();
+        dbContext.Database.SetCommandTimeout(0);
 
-        await dbContext.SaveChangesAsync();
+        var cutoffDate = clock.Today.AddDays(-1).ToDateTime();
+
+        await dbContext.JourneyStates
+           .Where(e => e.Created < cutoffDate)
+           .ExecuteDeleteAsync(cancellationToken);
     }
 }
