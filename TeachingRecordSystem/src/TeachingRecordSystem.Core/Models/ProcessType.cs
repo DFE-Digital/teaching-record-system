308c430ea6923542556845297612538ca36adff1
James Gunn <james@gunn.io>
2025-10-08 14:43:27 +0100

    Remove placeholder process types

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Models/ProcessType.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Models/ProcessType.cs
@@ -2,9 +2,4 @@ namespace TeachingRecordSystem.Core.Models;
 
 public enum ProcessType
 {
-    RouteToProfessionalStatusCreated = 1,
-    RouteToProfessionalStatusUpdated = 2,
-    RouteToProfessionalStatusDeleted = 3,
-    ApiTrnRequestSupportTaskResolved = 4,
-    //etc..
 }
