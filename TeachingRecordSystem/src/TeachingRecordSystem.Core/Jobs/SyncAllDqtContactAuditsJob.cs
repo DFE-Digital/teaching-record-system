0c386d26bc5470c9431dbd4ca1389f470fa4f579
James Gunn <james@gunn.io>
2025-10-24 06:37:46 +0100

    Increase automatic retries for audit sync job

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/SyncAllDqtContactAuditsJob.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Jobs/SyncAllDqtContactAuditsJob.cs
@@ -1,4 +1,5 @@
 using System.ServiceModel;
+using Hangfire;
 using Hangfire.Server;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,7 @@ using TeachingRecordSystem.Core.Services.TrsDataSync;
 
 namespace TeachingRecordSystem.Core.Jobs;
 
+[AutomaticRetry(Attempts = 100)]
 public class SyncAllDqtContactAuditsJob(
     [FromKeyedServices(TrsDataSyncHelper.CrmClientName)] IOrganizationServiceAsync2 organizationService,
     IDbContextFactory<TrsDbContext> dbContextFactory,
