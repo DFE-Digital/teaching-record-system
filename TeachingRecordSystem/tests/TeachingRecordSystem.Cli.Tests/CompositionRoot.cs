e9e87e54b2e9b843f07b3c35a153fb8c5c10cfd5
James Gunn <james@gunn.io>
2025-10-27 13:53:34 +0000

    Use xunit3 for Cli.Tests

---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.Cli.Tests/CompositionRoot.cs
@@ -0,0 +1,57 @@
+//using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using TeachingRecordSystem.Cli.Tests;
+using TeachingRecordSystem.TestCommon;
+//using Xunit.v3;
+
+[assembly: AssemblyFixture(typeof(CompositionRoot))]
+
+namespace TeachingRecordSystem.Cli.Tests;
+
+public class CompositionRoot : IAsyncLifetime
+{
+    // static CompositionRoot()
+    // {
+    //     Xunit.v3.TypeActivator.Current = new TypeActivator(Instance.Services);
+    // }
+
+    public static CompositionRoot Instance => new();
+
+    public IServiceProvider Services { get; } = CreateServiceProvider();
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<CompositionRoot>()
+            .AddEnvironmentVariables()
+            .Build();
+
+        DbHelper.ConfigureDbServices(services, configuration.GetPostgresConnectionString());
+
+        services
+            .AddSingleton<IConfiguration>(configuration)
+            .AddSingleton<DbFixture>();
+
+        return services.BuildServiceProvider();
+    }
+
+    async ValueTask IAsyncLifetime.InitializeAsync() => await Services.GetRequiredService<DbHelper>().InitializeAsync();
+
+    ValueTask IAsyncDisposable.DisposeAsync() => ValueTask.CompletedTask;
+
+    // private class TypeActivator(IServiceProvider serviceProvider) : ITypeActivator
+    // {
+    //     public object CreateInstance(
+    //         ConstructorInfo constructor,
+    //         object?[]? arguments,
+    //         Func<Type, IReadOnlyCollection<ParameterInfo>, string> missingArgumentMessageFormatter)
+    //     {
+    //         return ActivatorUtilities.CreateInstance(
+    //             serviceProvider,
+    //             constructor.DeclaringType!,
+    //             (arguments ?? []).Where(a => a is not null));
+    //     }
+    // }
+}
---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.TestCommon.XUnit3/AssertEx.Http.cs
@@ -0,0 +1,122 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+using Xunit.Sdk;
+
+namespace TeachingRecordSystem.TestCommon;
+
+public static partial class AssertEx
+{
+    public static async Task<JsonDocument> JsonResponseAsync(HttpResponseMessage response, int expectedStatusCode = 200)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+
+        if (expectedStatusCode != (int)response.StatusCode)
+        {
+            throw new XunitException(
+                $"""
+                Response status code does not match
+                Expected: {expectedStatusCode}
+                Actual:   {(int)response.StatusCode}
+
+                Response body:
+                {await response.Content.ReadAsStringAsync()}
+                """);
+        }
+
+        var contentType = response.Content.Headers.ContentType?.MediaType;
+        if (contentType is not "application/json" and not "application/problem+json")
+        {
+            throw new XunitException($"Content type '{contentType}' is not a JSON content type");
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<JsonDocument>(SerializerOptions);
+        Assert.NotNull(result);
+        return result;
+    }
+
+    public static async Task JsonResponseEqualsAsync(HttpResponseMessage response, object expected, int expectedStatusCode = 200)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+        ArgumentNullException.ThrowIfNull(expected);
+
+        var jsonDocument = await JsonResponseAsync(response, expectedStatusCode);
+
+        JsonObjectEquals(expected, jsonDocument);
+    }
+
+    public static async Task JsonResponseIsErrorAsync(HttpResponseMessage response, int expectedErrorCode, int expectedStatusCode)
+    {
+        var problemDetails = await ResponseIsProblemDetailsAsync(response, expectedStatusCode);
+
+        Assert.NotNull(problemDetails.Extensions);
+        Assert.Contains(problemDetails.Extensions, kvp => kvp.Key == "errorCode");
+        Assert.Equal(expectedErrorCode, problemDetails.Extensions?["errorCode"].GetInt32());
+    }
+
+    public static async Task JsonResponseHasValidationErrorForPropertyAsync(
+        HttpResponseMessage response,
+        string propertyName,
+        string expectedError,
+        int expectedStatusCode = 400)
+    {
+        var problemDetails = await ResponseIsProblemDetailsAsync(response, expectedStatusCode);
+
+        Assert.NotNull(problemDetails.Extensions);
+        Assert.Equal(expectedError, problemDetails.Errors?[propertyName].Single());
+    }
+
+    public static async Task JsonResponseHasValidationErrorsForPropertiesAsync(
+        HttpResponseMessage response,
+        IReadOnlyDictionary<string, string> expectedErrors,
+        int expectedStatusCode = 400)
+    {
+        var problemDetails = await ResponseIsProblemDetailsAsync(response, expectedStatusCode);
+
+        Assert.NotNull(problemDetails.Extensions);
+
+        foreach (var e in expectedErrors)
+        {
+            Assert.Equal(e.Value, problemDetails.Errors?[e.Key].Single());
+        }
+    }
+
+    private static async Task<ProblemDetails> ResponseIsProblemDetailsAsync(HttpResponseMessage response, int expectedStatusCode)
+    {
+        ArgumentNullException.ThrowIfNull(response);
+
+        Assert.Equal(expectedStatusCode, (int)response.StatusCode);
+        Assert.Equal("application/problem+json", response.Content.Headers.ContentType?.MediaType);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        Assert.NotNull(problemDetails);
+        Assert.Equal(expectedStatusCode, problemDetails!.Status);
+
+        return problemDetails;
+    }
+
+    private class ProblemDetails
+    {
+        public string? Title { get; set; }
+        public int Status { get; set; }
+        [JsonConverter(typeof(ProblemDetailsErrorJsonConverter))]
+        public IDictionary<string, string[]>? Errors { get; set; }
+        [JsonExtensionData]
+        public IDictionary<string, JsonElement>? Extensions { get; set; }
+    }
+
+    private class ProblemDetailsErrorJsonConverter : JsonConverter<IDictionary<string, string[]>>
+    {
+        public override IDictionary<string, string[]>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var dic = (Dictionary<string, string[]>)JsonSerializer.Deserialize(ref reader, typeToConvert, options)!;
+            return new Dictionary<string, string[]>(dic, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public override void Write(Utf8JsonWriter writer, IDictionary<string, string[]> value, JsonSerializerOptions options)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
