6f39a73b51dc879242eb07aba730c9744537d928
James Gunn <james@gunn.io>
2025-11-03 10:59:09 +0000

    Remove DQT sync from reporting service (#2726)
    Our auth token has expired and no new data is going into DQT any way.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtReporting/DqtReportingOptions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/DqtReporting/DqtReportingOptions.cs
@@ -7,19 +7,7 @@ public class DqtReportingOptions
     public const string DefaultTrsDbReplicationSlotName = "dqt_rep_sync_slot";
 
     [Required]
-    public required int PollIntervalSeconds { get; set; }
-
-    [Required]
-    public required string[] Entities { get; set; }
-
-    [Required]
-    public required string CrmConnectionString { get; set; }
-
-    [Required]
     public required string ReportingDbConnectionString { get; set; }
-
-    [Required]
-    public required bool ProcessAllEntityTypesConcurrently { get; set; }
 
     [Required]
     public required bool RunService { get; set; }
