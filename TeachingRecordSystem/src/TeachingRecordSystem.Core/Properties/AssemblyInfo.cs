daaa996c5b8ee1a3c018831e1f41e6ea2f180ff4
James Gunn <james@gunn.io>
2025-11-10 12:04:07 +0000

    Move Core.Tests into Core.LegacyTests

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("TeachingRecordSystem.Core.Tests")]
-[assembly: InternalsVisibleTo("TeachingRecordSystem.Core.Dqt.CrmIntegrationTests")]
