a59702e19da95d73e01d07be67c1a5fe432c05ef
Andrew Horth <andrew@horth.co.uk>
2025-11-11 18:30:25 +0000

    Added filtering to change requests index page

--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/SupportTasks/ChangeRequests/IndexTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/SupportTasks/ChangeRequests/IndexTests.cs
@@ -1,3 +1,5 @@
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using TeachingRecordSystem.Core.DataStore.Postgres.Models;
 
 namespace TeachingRecordSystem.SupportUi.Tests.PageTests.SupportTasks.ChangeRequests;
@@ -6,7 +8,7 @@ namespace TeachingRecordSystem.SupportUi.Tests.PageTests.SupportTasks.ChangeRequ
 public class IndexTests(HostFixture hostFixture) : TestBase(hostFixture)
 {
     [Fact]
-    public async Task Get_NoOpenTasks_ShowsNoTasksMessage()
+    public async Task Get_NoOpenChangeRequests_ShowsNoChangeRequestsMessage()
     {
         // Arrange
         var createPersonResult = await TestData.CreatePersonAsync();
@@ -28,7 +30,7 @@ public class IndexTests(HostFixture hostFixture) : TestBase(hostFixture)
     [Theory]
     [InlineData(SupportTaskType.ChangeNameRequest)]
     [InlineData(SupportTaskType.ChangeDateOfBirthRequest)]
-    public async Task Get_WithTask_ShowsExpectedDataInResultsTable(SupportTaskType supportTaskType)
+    public async Task Get_WithChangeRequest_ShowsExpectedDataInResultsTable(SupportTaskType supportTaskType)
     {
         // Arrange
         var createPersonResult = await TestData.CreatePersonAsync();
@@ -73,4 +75,96 @@ public class IndexTests(HostFixture hostFixture) : TestBase(hostFixture)
             Assert.Equal(expectedText, column.TrimmedText());
         }
     }
+
+    [Fact]
+    public async Task Get_NoChangeRequestTypesSpecifiedAndFiltersApplied_ReturnsNoResults()
+    {
+        // Arrange
+        var createPersonResult = await TestData.CreatePersonAsync();
+        var supportTask = await TestData.CreateChangeNameRequestSupportTaskAsync(
+            createPersonResult.PersonId,
+            b => b.WithLastName(TestData.GenerateChangedLastName(createPersonResult.LastName)));
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/change-requests?_f=true");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+        Assert.Empty(doc.GetElementsByTagName("table"));
+        Assert.NotNull(doc.GetElementByTestId("no-tasks-message"));
+    }
+
+    [Fact]
+    public async Task Get_NoChangeRequestTypesSpecifiedAndNoFiltersApplied_ReturnsAllChangeRequestTypes()
+    {
+        // Arrange
+        var createPersonResult1 = await TestData.CreatePersonAsync();
+        var nameChangeRequest = await TestData.CreateChangeNameRequestSupportTaskAsync(
+            createPersonResult1.PersonId,
+            b => b.WithLastName(TestData.GenerateChangedLastName(createPersonResult1.LastName)));
+        var createPersonResult2 = await TestData.CreatePersonAsync();
+        var dobChangeRequest = await TestData.CreateChangeDateOfBirthRequestSupportTaskAsync(
+                createPersonResult2.PersonId,
+                b => b.WithDateOfBirth(TestData.GenerateChangedDateOfBirth(createPersonResult2.DateOfBirth)));
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/change-requests");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+        var references = GetTaskReferences(doc);
+        Assert.Contains(nameChangeRequest.SupportTaskReference, references);
+        Assert.Contains(dobChangeRequest.SupportTaskReference, references);
+    }
+
+    [Theory]
+    [InlineData(new[] { SupportTaskType.ChangeNameRequest })]
+    [InlineData(new[] { SupportTaskType.ChangeDateOfBirthRequest })]
+    [InlineData(new[] { SupportTaskType.ChangeNameRequest, SupportTaskType.ChangeDateOfBirthRequest })]
+    public async Task Get_ChangeRequestTypesSpecifiedAndFiltersApplied_ReturnsResultsMatchingChangeRequestTypesOnly(SupportTaskType[] supportTaskTypes)
+    {
+        // Arrange
+        var createPersonResult1 = await TestData.CreatePersonAsync();
+        var nameChangeRequest = await TestData.CreateChangeNameRequestSupportTaskAsync(
+            createPersonResult1.PersonId,
+            b => b.WithLastName(TestData.GenerateChangedLastName(createPersonResult1.LastName)));
+        var createPersonResult2 = await TestData.CreatePersonAsync();
+        var dobChangeRequest = await TestData.CreateChangeDateOfBirthRequestSupportTaskAsync(
+                createPersonResult2.PersonId,
+                b => b.WithDateOfBirth(TestData.GenerateChangedDateOfBirth(createPersonResult2.DateOfBirth)));
+
+        var changeRequestTypesQueryParam = string.Join("&", supportTaskTypes.Select(t => $"changeRequestTypes={t}"));
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/change-requests?{changeRequestTypesQueryParam}");
+        // Act
+        var response = await HttpClient.SendAsync(request);
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+        var references = GetTaskReferences(doc);
+
+        if (supportTaskTypes.Contains(SupportTaskType.ChangeNameRequest))
+        {
+            Assert.Contains(nameChangeRequest.SupportTaskReference, references);
+        }
+        else
+        {
+            Assert.DoesNotContain(nameChangeRequest.SupportTaskReference, references);
+        }
+
+        if (supportTaskTypes.Contains(SupportTaskType.ChangeDateOfBirthRequest))
+        {
+            Assert.Contains(dobChangeRequest.SupportTaskReference, references);
+        }
+        else
+        {
+            Assert.DoesNotContain(dobChangeRequest.SupportTaskReference, references);
+        }
+    }
+
+    private static IEnumerable<string> GetTaskReferences(IHtmlDocument document) =>
+        document.GetElementByTestId("results")?.QuerySelectorAll("tbody>tr").Attr("data-reference") ?? [];
 }
