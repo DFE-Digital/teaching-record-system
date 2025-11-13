744b0c613cd2eaec42ef6a4de1bf3574dd3276ed
Andrew Horth <andrew@horth.co.uk>
2025-11-13 14:37:09 +0000

    Removed [Display] attributes and added labels to view

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/TeacherPensions/Resolve/Merge.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/TeacherPensions/Resolve/Merge.cshtml.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using TeachingRecordSystem.Core.DataStore.Postgres;
@@ -29,34 +28,23 @@ public class MergeModel(TrsDbContext dbContext, SupportUiLinkGenerator linkGener
     public PersonAttribute<Gender?>? Gender { get; set; }
 
     [BindProperty]
-    [Display(Name = "Date of birth")]
     public PersonAttributeSource? DateOfBirthSource { get; set; }
 
     [BindProperty]
-    [Display(Name = "Email")]
-    public PersonAttributeSource? EmailAddressSource { get; set; }
-
-    [BindProperty]
-    [Display(Name = "National Insurance number")]
     public PersonAttributeSource? NationalInsuranceNumberSource { get; set; }
 
     [BindProperty]
-    [Display(Name = "Gender")]
     public PersonAttributeSource? GenderSource { get; set; }
 
     [BindProperty]
-    [Display(Name = "First name")]
     public PersonAttributeSource? FirstNameSource { get; set; }
 
     [BindProperty]
-    [Display(Name = "Last name")]
     public PersonAttributeSource? LastNameSource { get; set; }
 
-    [Display(Name = "TRN")]
     public PersonAttributeSource? TRNSource { get; set; }
 
     [BindProperty]
-    [Display(Name = "Add comments (optional)")]
     public string? MergeComments { get; set; }
 
     [BindProperty]
