915ae58cca34ae8fae577259c3aa1b7607331efb
Stu Sherwin <stu.sherwin@gmail.com>
2025-11-25 12:25:18 +0000

    Fix screen reader behaviour for sortable table columns (#2858)
    ### Context
    
    An Accessibility Audit performed 15th Oct 2025 identified a number of
    non-compliances with Accessibility Standards to be addressed
    
    * When providing sortable lists, the buttons to sort by any given column
    cannot currently be interpreted by screen readers
    * At present, the screen reader simply describes the icon ( ‘black arrow
    pointing up’ )
    * It is recommended that Alt-Text, or a similar method, is added such
    that screenreaders can both identify that this is a sort option, in
    which direction the list will be sorted ( ascending or descending ) and
    which field(s) the button is sorting the list by
    
    
    https://trello.com/c/sdc1LfqP/1999-aa005-provide-alt-text-for-list-sorting-buttons-to-identify-fields-and-direction
    
    ### Changes proposed in this pull request
    
    Implemented fix based on this example: [Sortable Table
    Example](https://www.w3.org/WAI/ARIA/apg/patterns/table/examples/sortable-table/)
    * Moved unicode sorting indicators into `aria-hidden` span within the
    button
    * Added screen-reader-only text to the table caption - this required
    creating additional tag helpers to intercept the <table> and <caption>
    elements.
    * Screen reader output
      * before:
        ```
        table
    Name filled down pointing triangle column header sorted descending
        Name  filled down pointing triangle  button
    filled down pointing triangle Email filled up pointing triangle column
    header column 2
    filled down pointing triangle Email filled up pointing triangle button
        ```
      * after:
        ```
        (column headers with buttons are sortable)   table
        Name  column header  column 1
        Name  button
        Email  column header  sorted ascending  column 2
        Email  button
        ``` 
    
    ### Checklist
    
    -   [x] Attach to Trello card
    -   [x] Rebased master
    -   [x] Cleaned commit history
    -   [x] Tested by running locally

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/FormEnctypeTagHelper.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/FormEnctypeTagHelper.cs
@@ -16,7 +16,7 @@ public class FormEnctypeTagHelper(SupportUiFormContext formContext) : TagHelper
             output.Attributes.Add("enctype", "multipart/form-data");
         }
 
-        // Reset for the next form
+        // Reset context for the next form in the page
         formContext.HasFileUpload = false;
     }
 }
@@ -30,6 +30,11 @@ public class FileUploadTagHelper(SupportUiFormContext formContext) : TagHelper
     }
 }
 
+// Context class for form tag helpers to communicate with each other.
+// Unfortunately TagHelperContext.Items cannot be used for this purpose as any
+// partial view or view component within the hierarchy interrupts the TagHelperContext
+// and causes a new context to be created, making it unusable for this purpose
+// This must be injected in request scope using .AddScoped()
 public class SupportUiFormContext
 {
     public bool HasFileUpload { get; set; }
--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/SortableTableColumnTagHelper.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/TagHelpers/SortableTableColumnTagHelper.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.AspNetCore.WebUtilities;
@@ -6,8 +7,14 @@ using Microsoft.Extensions.Primitives;
 namespace TeachingRecordSystem.SupportUi.TagHelpers;
 
 [HtmlTargetElement("th", Attributes = "sort-direction")]
-public class SortableTableColumnTagHelper : TagHelper
+public class SortableTableColumnTagHelper(SupportUiSortableTableContext tableContext) : TagHelper
 {
+    // Text to be added to table caption for screen readers when sorting headers are present.
+    // Screen reader behaviour for sortable column headers is modelled on this example in the
+    // ARIA Authoring Practices Guide (APG):
+    // https://www.w3.org/WAI/ARIA/apg/patterns/table/examples/sortable-table/
+    public const string ScreenReaderCaptionText = " (column headers with buttons are sortable)";
+
     [HtmlAttributeName("sort-direction")]
     public SortDirection? Direction { get; set; }
 
@@ -16,6 +23,8 @@ public class SortableTableColumnTagHelper : TagHelper
 
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
+        tableContext.HasSortableColumn = true;
+
         var content = await output.GetChildContentAsync();
 
         var ariaSort = Direction switch
@@ -63,10 +72,114 @@ public class SortableTableColumnTagHelper : TagHelper
         button.Attributes.Add("type", "submit");
         button.InnerHtml.AppendHtml(content);
 
+        // Contains sort direction indicators ▼ and ▲
+        button.InnerHtml.AppendHtml("<span aria-hidden=\"true\"></span>");
+
         form.InnerHtml.AppendHtml(button);
 
         output.Attributes.Add("aria-sort", ariaSort);
 
         output.Content.SetHtmlContent(form);
     }
+}
+
+// If a table contains sorting headers we need to add text visible only to screen readers to the
+// table caption. However, we don't the table contains sorting headers until we've rendered all
+// the child elements of the table, and there may already be a caption defined on the table.
+// So we need to hijack any existing <caption> element, suppressing its output and saving its
+// content within the SupportUiSortableTableContext so the <table> element can render it instead.
+[HtmlTargetElement("caption")]
+public class SortableTableCaptionTagHelper(SupportUiSortableTableContext tableContext) : TagHelper
+{
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        // Make sure we're actually in a table and not another type of element with a caption.
+        if (tableContext.IsWithinTableContext)
+        {
+            tableContext.HasCaption = true;
+
+            var content = await output.GetChildContentAsync();
+            tableContext.CaptionAttributes = context.AllAttributes.ToDictionary(a => a.Name, a => a.Value.ToString());
+            tableContext.CaptionContent = new HtmlString(content.GetContent());
+            output.SuppressOutput();
+        }
+    }
+}
+
+// This tag helper soelely exists to add text to the table caption for screen readers
+[HtmlTargetElement("table")]
+public class SortableTableTagHelper(SupportUiSortableTableContext tableContext) : TagHelper
+{
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        // Start a new table context scope
+        tableContext.IsWithinTableContext = true;
+
+        // Render child elements and see if they update the context
+        var content = await output.GetChildContentAsync();
+
+        if (tableContext.HasCaption || tableContext.HasSortableColumn)
+        {
+            var caption = new TagBuilder("caption");
+
+            // If there was already a caption defined, now we render its content/attributes
+            if (tableContext.HasCaption)
+            {
+                if (tableContext.CaptionAttributes is Dictionary<string, string?> captionAttributes)
+                {
+                    foreach (var attribute in captionAttributes)
+                    {
+                        caption.Attributes.Add(attribute.Key, attribute.Value);
+                    }
+                }
+
+                if (tableContext.CaptionContent is HtmlString captionContent)
+                {
+                    caption.InnerHtml.AppendHtml(captionContent);
+                }
+            }
+
+            // If the table has sortable columns, add the screen reader text to the caption
+            if (tableContext.HasSortableColumn)
+            {
+                if (tableContext.HasCaption)
+                {
+                    // Append to already existing caption
+                    caption.InnerHtml.AppendHtml($"<span class=\"govuk-visually-hidden\">{SortableTableColumnTagHelper.ScreenReaderCaptionText}</span>");
+                }
+                else
+                {
+                    // If there wasn't already a caption defined, we can make the whole caption
+                    // visible only to screen readers
+                    caption.Attributes.Add("class", "govuk-visually-hidden");
+                    caption.InnerHtml.Append(SortableTableColumnTagHelper.ScreenReaderCaptionText);
+                }
+            }
+
+            output.Content.AppendHtml(caption);
+        }
+
+        output.Content.AppendHtml(content);
+
+        // Reset context for the next table in the page
+        tableContext.IsWithinTableContext = false;
+        tableContext.HasSortableColumn = false;
+        tableContext.HasCaption = false;
+        tableContext.CaptionAttributes = null;
+        tableContext.CaptionContent = null;
+    }
+}
+
+// Context class for SortableTable tag helpers to communicate with each other.
+// Unfortunately TagHelperContext.Items cannot be used for this purpose as any
+// partial view or view component within the hierarchy interrupts the TagHelperContext
+// and causes a new context to be created, making it unusable for this purpose
+// This must be injected in request scope using .AddScoped()
+public class SupportUiSortableTableContext
+{
+    public bool IsWithinTableContext { get; set; }
+    public bool HasSortableColumn { get; set; }
+    public bool HasCaption { get; set; }
+    public Dictionary<string, string?>? CaptionAttributes { get; set; }
+    public HtmlString? CaptionContent { get; set; }
 }
