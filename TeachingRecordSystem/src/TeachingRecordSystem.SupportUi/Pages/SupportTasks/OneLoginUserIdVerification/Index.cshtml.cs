cf2a9220a5d9de3c05061926d8c348c73442ebd4
Catherine Lawlor <55924986+CathLass@users.noreply.github.com>
2025-11-27 17:31:13 +0000

    One login support task list (#2875)
    ### Context
    Basic page to list support tasks for one login verification checks
    
    ### Changes proposed in this pull request
    
    https://trello.com/c/qZow4jHF/1997-onelogin-trs-console-support-task-list

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/OneLoginUserIdVerification/Index.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/SupportTasks/OneLoginUserIdVerification/Index.cshtml.cs
@@ -1,11 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeachingRecordSystem.Core.DataStore.Postgres;
+using TeachingRecordSystem.Core.Models.SupportTasks;
 
 namespace TeachingRecordSystem.SupportUi.Pages.SupportTasks.OneLoginUserIdVerification;
 
-public class Index : PageModel
+public class Index(TrsDbContext dbContext) : PageModel
 {
-    public void OnGet()
-    {
+    [BindProperty(SupportsGet = true)]
+    [FromQuery]
+    public string? Search { get; set; }
 
+    public IReadOnlyCollection<Result>? Results { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        var tasks = dbContext.SupportTasks
+            .Where(t => t.SupportTaskType == SupportTaskType.OneLoginUserIdVerification && t.Status == SupportTaskStatus.Open)
+            .OrderBy(t => t.CreatedOn);
+
+        Results = await tasks
+            .Select(t => new
+            {
+                t,
+                Data = t.Data as OneLoginUserIdVerificationData
+            })
+            .Where(item => item.Data != null)
+            .Join(
+                dbContext.OneLoginUsers,
+                y => y.Data!.OneLoginUserSubject,
+                user => user.Subject,
+                (item, user) => new Result(
+                    item.t.SupportTaskReference,
+                    item.Data!.StatedFirstName,
+                    item.Data!.StatedLastName,
+                    user.EmailAddress,
+                    item.t.CreatedOn))
+            .ToArrayAsync();
+    }
+
+    public record Result(
+        string SupportTaskReference,
+        string FirstName,
+        string LastName,
+        string? EmailAddress,
+        DateTime CreatedOn)
+    {
+        public string Name => $"{FirstName} {LastName}";
     }
 }
---
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/SupportTasks/OneLoginUserIdVerification/IndexTests.cs
@@ -0,0 +1,96 @@
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Optional;
+using TeachingRecordSystem.Core.Models.SupportTasks;
+
+namespace TeachingRecordSystem.SupportUi.Tests.PageTests.SupportTasks.OneLoginUserIdVerification;
+
+[ClearDbBeforeTest, Collection(nameof(DisableParallelization))]
+public class IndexTests(HostFixture hostFixture) : TestBase(hostFixture)
+{
+    [Fact]
+    public async Task Get_ShowsListOfOpenTasksWithOldestFirst()
+    {
+        // Arrange
+        var oneLoginUser1 = await TestData.CreateOneLoginUserAsync(personId: null, email: Option.Some<string?>(TestData.GenerateUniqueEmail()), verifiedInfo: null);
+        var oneLoginUser2 = await TestData.CreateOneLoginUserAsync(personId: null, email: Option.Some<string?>(TestData.GenerateUniqueEmail()), verifiedInfo: null);
+        var supportTask1 = await TestData.CreateOneLoginUserIdVerificationSupportTaskAsync(oneLoginUser1.Subject);
+        Clock.Advance(TimeSpan.FromDays(1));
+        var supportTask2 = await TestData.CreateOneLoginUserIdVerificationSupportTaskAsync(oneLoginUser2.Subject);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/one-login-user-id-verification");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+
+        var resultRows = doc.GetElementByTestId("results")
+            ?.GetElementsByTagName("tbody")
+            .FirstOrDefault()
+            ?.GetElementsByTagName("tr");
+
+        Assert.NotNull(resultRows);
+        var topRow = resultRows[0];
+        AssertRowHasContent(topRow, "name", $"{((OneLoginUserIdVerificationData)supportTask1.Data)!.StatedFirstName} {((OneLoginUserIdVerificationData)supportTask1.Data)!.StatedLastName}");
+        AssertRowHasContent(topRow, "email", oneLoginUser1.EmailAddress!);
+        AssertRowHasContent(topRow, "requested-on", supportTask1.CreatedOn.ToString(UiDefaults.DateOnlyDisplayFormat));
+        AssertRowHasContent(topRow, "requested-on", supportTask1.CreatedOn.ToString(UiDefaults.DateOnlyDisplayFormat));
+
+        var nextRow = resultRows[1];
+        AssertRowHasContent(nextRow, "name", $"{((OneLoginUserIdVerificationData)supportTask2.Data)!.StatedFirstName} {((OneLoginUserIdVerificationData)supportTask2.Data)!.StatedLastName}");
+        AssertRowHasContent(nextRow, "email", oneLoginUser2.EmailAddress!);
+        AssertRowHasContent(nextRow, "requested-on", supportTask2.CreatedOn.ToString(UiDefaults.DateOnlyDisplayFormat));
+
+        void AssertRowHasContent(IElement row, string testId, string expectedText)
+        {
+            var column = row.GetElementByTestId(testId);
+            Assert.NotNull(column);
+            Assert.Equal(expectedText, column.TrimmedText());
+        }
+    }
+
+    [Fact]
+    public async Task Get_TaskListItemLinksToSupportTask()
+    {
+        // Arrange
+        var oneLoginUser = await TestData.CreateOneLoginUserAsync(personId: null, email: Option.Some<string?>(TestData.GenerateUniqueEmail()), verifiedInfo: null);
+        var supportTask = await TestData.CreateOneLoginUserIdVerificationSupportTaskAsync(oneLoginUser.Subject);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/one-login-user-id-verification");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+
+        var resultRow = doc.GetElementByTestId("results")
+            ?.GetElementsByTagName("tbody")
+            .FirstOrDefault()
+            ?.GetElementsByTagName("tr")
+            .FirstOrDefault();
+
+        Assert.NotNull(resultRow);
+        var nameLink = resultRow.GetElementByTestId("name")!.GetElementsByTagName("a").FirstOrDefault() as IHtmlAnchorElement;
+        Assert.Contains($"/support-tasks/one-login-user-id-verification/{supportTask.SupportTaskReference}/resolve", nameLink!.Href);
+    }
+
+    [Fact]
+    public async Task Get_NoTasks_ShowsNoTasksMessage()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Get, $"/support-tasks/one-login-user-id-verification");
+
+        // Act
+        var response = await HttpClient.SendAsync(request);
+
+        // Assert
+        var doc = await AssertEx.HtmlResponseAsync(response);
+
+        var resultSection = doc.GetElementByTestId("results");
+        Assert.NotNull(resultSection);
+        Assert.NotNull(resultSection.GetElementByTestId("no-tasks-message"));
+    }
+}
