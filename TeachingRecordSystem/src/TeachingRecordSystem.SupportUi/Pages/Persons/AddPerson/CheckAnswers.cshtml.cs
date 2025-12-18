bf26443be86db76a6bfaeaf1fc12cae56b5fc1d3
Stu SHERWIN <Stu.SHERWIN@EDUCATION.GOV.UK>
2025-12-18 10:58:25 +0000

    Fix tests

--- TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/AddPerson/CheckAnswers.cshtml.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.SupportUi/Pages/Persons/AddPerson/CheckAnswers.cshtml.cs
@@ -14,7 +14,7 @@ public class CheckAnswersModel(
     : CommonJourneyPage(linkGenerator, evidenceUploadManager)
 {
     public string? FirstName { get; set; }
-    public string? MiddleName { get; set; }
+    public string? MiddleName { get; set; }#
     public string? LastName { get; set; }
     public DateOnly? DateOfBirth { get; set; }
     public EmailAddress? EmailAddress { get; set; }
