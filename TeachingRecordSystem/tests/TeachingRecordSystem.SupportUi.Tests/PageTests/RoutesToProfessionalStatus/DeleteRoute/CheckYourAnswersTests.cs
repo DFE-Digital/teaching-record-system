3be8efe1271f2ad0c0be7ebd0b423342a4b6d089
CLAWLOR <catherine.lawlor@education.gov.uk>
2025-12-18 12:46:49 +0000

    fix tests

--- TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/RoutesToProfessionalStatus/DeleteRoute/CheckYourAnswersTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.SupportUi.Tests/PageTests/RoutesToProfessionalStatus/DeleteRoute/CheckYourAnswersTests.cs
@@ -176,7 +176,7 @@ public class CheckYourAnswersTests(HostFixture hostFixture) : TestBase(hostFixtu
         var doc = await AssertEx.HtmlResponseAsync(response);
 
         doc.AssertSummaryListRowValueContentMatches("Reason", deleteRouteState.ChangeReason!.GetDisplayName()!);
-        doc.AssertSummaryListRowValueContentMatches("More details", deleteRouteState.ChangeReasonDetail!.ChangeReasonDetail!);
+        doc.AssertSummaryListRowValueContentMatches("Additional information", deleteRouteState.ChangeReasonDetail!.ChangeReasonDetail!);
         doc.AssertSummaryListRowValueContentMatches("Evidence", "Not provided");
     }
 
