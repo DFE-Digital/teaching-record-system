ff8dab7a746e30dd3177605df3b445b7be527265
James Gunn <james@gunn.io>
2025-12-22 13:40:01 +0000

    Attempt to fix migrations

--- TeachingRecordSystem/src/TeachingRecordSystem.Cli/Commands.MigrateDb.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Cli/Commands.MigrateDb.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Npgsql;
 using TeachingRecordSystem.Core.DataStore.Postgres;
 
 namespace TeachingRecordSystem.Cli;
@@ -28,47 +25,49 @@ public static partial class Commands
             async parseResult =>
             {
                 var connectionString = parseResult.GetRequiredValue(connectionStringOption);
-                var targetMigration = parseResult.GetValue(targetMigrationOption);
+                //var targetMigration = parseResult.GetValue(targetMigrationOption);
 
                 using var dbContext = TrsDbContext.Create(connectionString, commandTimeout: (int)TimeSpan.FromMinutes(10).TotalSeconds);
 
-                // Ensure we've got squashed migration recorded
-                var migrationsAssembly = dbContext.Database.GetService<IMigrationsAssembly>();
-                var initialMigration = migrationsAssembly.Migrations.OrderBy(k => k.Key).First();
-                var pendingMigrations = (await dbContext.Database.GetPendingMigrationsAsync()).ToArray();
-                var appliedMigrations = (await dbContext.Database.GetAppliedMigrationsAsync()).ToArray();
+                await dbContext.Database.MigrateAsync();
 
-                if (appliedMigrations.Length != 0 && pendingMigrations.Contains(initialMigration.Key))
-                {
-                    using var txn = await dbContext.Database.BeginTransactionAsync();
-                    var historyRepository = dbContext.GetService<IHistoryRepository>();
-                    await using var @lock = await historyRepository.AcquireDatabaseLockAsync();
-
-                    // Add the squashed migration to the history table
-                    var insertScript = historyRepository.GetInsertScript(new HistoryRow(initialMigration.Key, ProductInfo.GetVersion()));
-                    await dbContext.Database.ExecuteSqlRawAsync(insertScript);
-
-                    // Remove all migrations that don't exist any more (i.e. have been squashed)
-                    foreach (var appliedMigration in appliedMigrations)
-                    {
-                        if (!migrationsAssembly.Migrations.ContainsKey(appliedMigration))
-                        {
-                            var deleteScript = historyRepository.GetDeleteScript(appliedMigration);
-                            await dbContext.Database.ExecuteSqlRawAsync(deleteScript);
-                        }
-                    }
-
-                    await txn.CommitAsync();
-                }
-
-                var migrator = dbContext.GetService<IMigrator>();
-                await migrator.MigrateAsync(targetMigration);
-
-                // Ensure the user has the replication permission
-                var user = new NpgsqlConnectionStringBuilder(connectionString).Username;
-#pragma warning disable EF1002 // Risk of vulnerability to SQL injection.
-                await dbContext.Database.ExecuteSqlRawAsync($"alter user {user} with replication");
-#pragma warning restore EF1002 // Risk of vulnerability to SQL injection.
+//                 // Ensure we've got squashed migration recorded
+//                 var migrationsAssembly = dbContext.Database.GetService<IMigrationsAssembly>();
+//                 var initialMigration = migrationsAssembly.Migrations.OrderBy(k => k.Key).First();
+//                 var pendingMigrations = (await dbContext.Database.GetPendingMigrationsAsync()).ToArray();
+//                 var appliedMigrations = (await dbContext.Database.GetAppliedMigrationsAsync()).ToArray();
+//
+//                 if (appliedMigrations.Length != 0 && pendingMigrations.Contains(initialMigration.Key))
+//                 {
+//                     using var txn = await dbContext.Database.BeginTransactionAsync();
+//                     var historyRepository = dbContext.GetService<IHistoryRepository>();
+//                     await using var @lock = await historyRepository.AcquireDatabaseLockAsync();
+//
+//                     // Add the squashed migration to the history table
+//                     var insertScript = historyRepository.GetInsertScript(new HistoryRow(initialMigration.Key, ProductInfo.GetVersion()));
+//                     await dbContext.Database.ExecuteSqlRawAsync(insertScript);
+//
+//                     // Remove all migrations that don't exist any more (i.e. have been squashed)
+//                     foreach (var appliedMigration in appliedMigrations)
+//                     {
+//                         if (!migrationsAssembly.Migrations.ContainsKey(appliedMigration))
+//                         {
+//                             var deleteScript = historyRepository.GetDeleteScript(appliedMigration);
+//                             await dbContext.Database.ExecuteSqlRawAsync(deleteScript);
+//                         }
+//                     }
+//
+//                     await txn.CommitAsync();
+//                 }
+//
+//                 var migrator = dbContext.GetService<IMigrator>();
+//                 await migrator.MigrateAsync(targetMigration);
+//
+//                 // Ensure the user has the replication permission
+//                 var user = new NpgsqlConnectionStringBuilder(connectionString).Username;
+// #pragma warning disable EF1002 // Risk of vulnerability to SQL injection.
+//                 await dbContext.Database.ExecuteSqlRawAsync($"alter user {user} with replication");
+// #pragma warning restore EF1002 // Risk of vulnerability to SQL injection.
             });
 
         return migrateDbCommand;
