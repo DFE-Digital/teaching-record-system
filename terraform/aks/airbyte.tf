a8e860f8863d116fbad4e729f823c13faba30be9
RMcVelia <rick.mcvelia@gmail.com>
2025-12-23 12:05:17 +0000

    Add airbyte to dev env

---
+++ terraform/aks/airbyte.tf
@@ -0,0 +1,130 @@
+# data "azurerm_key_vault" "key_vault" {
+#   name                = var.key_vault_name
+#   resource_group_name = local.app_resource_group_name
+# }
+
+# data "azurerm_key_vault_secret" "statuscake_password" {
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "STATUSCAKE-PASSWORD"
+# }
+
+# data "azurerm_key_vault_secret" "airbyte_client_id" {
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "AIRBYTE-CLIENT-ID"
+# }
+
+# data "azurerm_key_vault_secret" "airbyte_client_secret" {
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "AIRBYTE-CLIENT-SECRET"
+# }
+
+# data "azurerm_key_vault_secret" "airbyte_workspace_id" {
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "AIRBYTE-WORKSPACE-ID"
+# }
+
+# # change this to a random password?
+# data "azurerm_key_vault_secret" "airbyte_replication_password" {
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "AIRBYTE-REPLICATION-PASSWORD"
+# }
+
+# data "azurerm_key_vault_secret" "airbyte_bq_sa" {
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+#   name         = "AIRBYTE-BQ-SA"
+# }
+
+module "airbyte" {
+  source = "./vendor/modules/aks//aks/airbyte"
+
+  count = var.airbyte_enabled ? 1 : 0
+
+  environment           = local.app_name_suffix
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short_name
+  service_name          = var.service_name
+  docker_image          = var.docker_image
+  postgres_version      = var.postgres_server_version
+  postgres_url          = module.postgres.url
+
+  host_name          = module.postgres.host
+  database_name      = module.postgres.name
+  # workspace_id       = data.azurerm_key_vault_secret.airbyte_workspace_id[0].value
+  # client_id          = data.azurerm_key_vault_secret.airbyte_client_id[0].value
+  # client_secret      = data.azurerm_key_vault_secret.airbyte_client_secret[0].value
+  # repl_password      = data.azurerm_key_vault_secret.airbyte_replication_password[0].value
+  workspace_id       = module.infrastructure_secrets.map.AIRBYTE-WORKSPACE-ID
+  client_id          = module.infrastructure_secrets.map.AIRBYTE-CLIENT-ID
+  client_secret      = module.infrastructure_secrets.map.AIRBYTE-CLIENT-SECRET
+  repl_password      = module.infrastructure_secrets.map.AIRBYTE-REPLICATION-PASSWORD
+
+  server_url         = "https://airbyte-${var.namespace}.${module.cluster_data.ingress_domain}"
+  connection_status  = var.connection_status
+  connection_streams = local.connection_streams
+
+  cluster           = var.cluster
+  namespace         = var.namespace
+  gcp_taxonomy_id   = "4953620268226784157"
+  gcp_policy_tag_id = "2818002780538442239"
+  gcp_keyring       = "trs-key-ring"
+  gcp_key           = "trs-key"
+  # gcp_bq_sa         = data.azurerm_key_vault_secret.airbyte_bq_sa[0].value
+  gcp_bq_sa         = module.infrastructure_secrets.map.AIRBYTE-BQ-SA
+
+  gcp_dataset_internal = "airbyte_internal"
+
+  # config_map_ref = module.application_configuration.kubernetes_config_map_name
+  # secret_ref     = module.application_configuration.kubernetes_secret_name
+  config_map_ref = module.worker_application_configuration.kubernetes_config_map_name
+  secret_ref     = module.worker_application_configuration.kubernetes_secret_name
+  cpu            = module.cluster_data.configuration_map.cpu_min
+
+  use_azure = var.deploy_azure_backing_services
+}
+
+# module "streams_update_job" {
+#   source = "./vendor/modules/aks//aks/job_configuration"
+
+#   count = var.airbyte_enabled ? 1 : 0
+
+#   depends_on = [module.airbyte]
+
+#   namespace    = var.namespace
+#   environment  = local.app_name_suffix
+#   service_name = var.service_name
+#   docker_image = var.app_docker_image
+#   commands     = ["/bin/sh"]
+#   arguments    = ["-c", "rake dfe:analytics:airbyte_deploy_tasks"]
+#   job_name     = "airbyte-stream-update"
+#   enable_logit = true
+
+#   config_map_ref = module.application_configuration.kubernetes_config_map_name
+#   secret_ref     = module.airbyte_application_configuration[0].kubernetes_secret_name
+#   cpu            = module.cluster_data.configuration_map.cpu_min
+# }
+
+## Airbyte module variables
+
+variable "airbyte_enabled" { default = false }
+
+variable "connection_status" {
+  type = string
+  default = "inactive"
+  description = "Connectin status, either active or inactive"
+}
+
+locals {
+  connection_streams = var.airbyte_enabled ? file("config/airbyte_stream_config.json") : null
+  # gcp_dataset_name   = replace("${var.service_short}_airbyte_${local.app_name_suffix}", "-", "_")
+  gcp_dataset_name   = replace("${var.service_short_name}_airbyte_${local.app_name_suffix}", "-", "_")
+}
--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -23,3 +23,13 @@ provider "kubernetes" {
 provider "statuscake" {
   api_token = module.infrastructure_secrets.map.STATUSCAKE-API-TOKEN
 }
+
+provider "airbyte" {
+  # Configuration options
+  server_url = var.airbyte_enabled ? "https://airbyte-${var.namespace}.${module.cluster_data.ingress_domain}/api/public/v1" : ""
+
+  # client_id = var.airbyte_enabled ? data.azurerm_key_vault_secret.airbyte_client_id[0].value : ""
+  # client_secret = var.airbyte_enabled ? data.azurerm_key_vault_secret.airbyte_client_secret[0].value: ""
+  client_id     = var.airbyte_enabled ? module.infrastructure_secrets.map.AIRBYTE-CLIENT-ID : ""
+  client_secret = var.airbyte_enabled ? module.infrastructure_secrets.map.AIRBYTE-CLIENT-SECRET: ""
+}
