31781dcdc019ef85144f456497732fb6460541b9
CLAWLOR <catherine.lawlor@education.gov.uk>
2026-01-06 17:10:22 +0000

    inductin exemption wording

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/InductionExemptions/ExemptionReasonCategory.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/InductionExemptions/ExemptionReasonCategory.cs
@@ -4,9 +4,9 @@ namespace TeachingRecordSystem.Core.Services.InductionExemptions;
 
 public enum ExemptionReasonCategory
 {
-    [Display(Name = "Miscellaneous exemptions")]
+    [Display(Name = "General exemptions")]
     Miscellaneous = 1,
-    [Display(Name = "Exemptions for historical qualification routes")]
+    [Display(Name = "Historical qualification route exemptions")]
     HistoricalQualificationRoute = 2,
     [Display(Name = "Induction completed outside England")]
     InductionCompletedOutsideEngland = 3
