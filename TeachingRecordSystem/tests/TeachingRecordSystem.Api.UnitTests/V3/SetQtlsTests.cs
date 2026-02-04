6f1f8ccb0a5135d1e23530e505d3dcd3a28b365d
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2026-02-04 12:26:10 +0000

    Set qtls holds date cuttoff (#3065)
    https://trello.com/c/6PgixRIr/2386-scenario-7-qtls-set-displaying-earliest-permissible-date-on-aytq-amend-api
    
    Change to the api setqtls endpoint to:
    
    - If there are no other routes, and qtls passed to the api is prior to
    1/4/2012, set holds date to 1/4/2012
    - If there are other routes, with holds dates _**prior**_ to 01/04/2012,
    and the qtls date passed to the api is before to 01/4/2012 update or
    create the qtls route with the api date.
    - If there are other routes, with holds dates **_after_** to 01/04/2012,
    and the qtls date passed to the api is prior to 01/4/2012 update or
    create the qtls route with the api date,and update the route holds date
    to 01/04/2012.
    
    Include any useful information needed to review this change.
    Include any dependencies that are required for this change.
    
    ### Checklist
    
    -   [x] Attach to Trello card
    -   [x] Rebased master
    -   [x] Cleaned commit history
    -   [x] Tested by running locally

--- TeachingRecordSystem/tests/TeachingRecordSystem.Api.UnitTests/V3/SetQtlsTests.cs
+++ TeachingRecordSystem/tests/TeachingRecordSystem.Api.UnitTests/V3/SetQtlsTests.cs
@@ -6,6 +6,8 @@ namespace TeachingRecordSystem.Api.UnitTests.V3;
 
 public class SetQtlsTests(OperationTestFixture operationTestFixture) : OperationTestBase(operationTestFixture)
 {
+    private static readonly DateOnly QtsCutoff = new(2012, 4, 1);
+
     [Fact]
     public async Task HandleAsync_PersonDoesNotExist_ReturnsError()
     {
@@ -123,7 +125,7 @@ public class SetQtlsTests(OperationTestFixture operationTestFixture) : Operation
     }
 
     [Fact]
-    public async Task HandleAsync_NonNullQtsDateAndExistingRouteHoldFromDoesNotMatch_UpdatesRoute()
+    public async Task HandleAsync_newQtlsDateNonNullQtsDateAndExistingRouteHoldFromDoesNotMatch_UpdatesRoute()
     {
         // Arrange
         var existingQtsDate = new DateOnly(2025, 4, 1);
@@ -155,6 +157,264 @@ public class SetQtlsTests(OperationTestFixture operationTestFixture) : Operation
             Assert.True(updatedEvent.Changes.HasFlag(RouteToProfessionalStatusUpdatedEventChanges.HoldsFrom));
         });
     }
+
+    [Fact]
+    public async Task HandleAsync_newQtlsDateBeforeCuttoffUpdatesToCuttoff()
+    {
+        // Arrange
+        var existingQtsDate = new DateOnly(2025, 4, 1);
+        var person = await TestData.CreatePersonAsync(p => p.WithQtls(existingQtsDate));
+
+        var newQtlsDate = QtsCutoff.AddYears(-1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(route);
+        Assert.Equal(QtsCutoff, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(e =>
+        {
+            var updatedEvent = Assert.IsType<RouteToProfessionalStatusUpdatedEvent>(e);
+            Assert.Equal(Clock.UtcNow, updatedEvent.CreatedUtc);
+            Assert.Equal(person.PersonId, updatedEvent.PersonId);
+            Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), updatedEvent.RaisedBy);
+            Assert.True(updatedEvent.Changes.HasFlag(RouteToProfessionalStatusUpdatedEventChanges.HoldsFrom));
+        });
+    }
+
+    [Fact]
+    public async Task HandleAsync_newQtlsDateAfterCuttoffUpdatesToNewQtlsDate()
+    {
+        // Arrange
+        var existingQtsDate = new DateOnly(2025, 4, 1);
+        var person = await TestData.CreatePersonAsync(p => p.WithQtls(existingQtsDate));
+
+        var newQtlsDate = QtsCutoff.AddYears(1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(route);
+        Assert.Equal(newQtlsDate, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(e =>
+        {
+            var updatedEvent = Assert.IsType<RouteToProfessionalStatusUpdatedEvent>(e);
+            Assert.Equal(Clock.UtcNow, updatedEvent.CreatedUtc);
+            Assert.Equal(person.PersonId, updatedEvent.PersonId);
+            Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), updatedEvent.RaisedBy);
+            Assert.True(updatedEvent.Changes.HasFlag(RouteToProfessionalStatusUpdatedEventChanges.HoldsFrom));
+        });
+    }
+
+    [Fact]
+    public async Task HandleAsync_newQtls_SetsExistingRouteHoldsDateToCuttoff()
+    {
+        // Arrange
+        var existingQtsDate = new DateOnly(2025, 4, 1);
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithRouteToProfessionalStatus(s => s
+                .WithRouteType(RouteToProfessionalStatusType.AssessmentOnlyRouteId)
+                .WithStatus(RouteToProfessionalStatusStatus.Holds)
+                .WithHoldsFrom(existingQtsDate)));
+
+        var newQtlsDate = QtsCutoff.AddYears(-1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(route);
+        Assert.Equal(QtsCutoff, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(
+            e1 =>
+            {
+                var createdQtlsEventEvent = Assert.IsType<RouteToProfessionalStatusCreatedEvent>(e1);
+
+                Assert.Equal(
+                    RouteToProfessionalStatusType.QtlsAndSetMembershipId,
+                    createdQtlsEventEvent.RouteToProfessionalStatus.RouteToProfessionalStatusTypeId
+                );
+                Assert.Equal(Clock.UtcNow, createdQtlsEventEvent.CreatedUtc);
+                Assert.Equal(person.PersonId, createdQtlsEventEvent.PersonId);
+                Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), createdQtlsEventEvent.RaisedBy);
+                Assert.True(createdQtlsEventEvent.Changes.HasFlag(RouteToProfessionalStatusCreatedEventChanges.PersonQtsDate));
+            });
+    }
+
+    [Fact]
+    public async Task HandleAsync_existingQtlsAndQts_SetsExistingRouteHoldsDateToCuttoff()
+    {
+        // Arrange
+        var existingQtsDate = new DateOnly(2025, 4, 1);
+        var existingQtlsDate = new DateOnly(2025, 11, 1);
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithRouteToProfessionalStatus(s => s
+                .WithRouteType(RouteToProfessionalStatusType.AssessmentOnlyRouteId)
+                .WithStatus(RouteToProfessionalStatusStatus.Holds)
+                .WithHoldsFrom(existingQtsDate))
+            .WithRouteToProfessionalStatus(s => s
+                .WithRouteType(RouteToProfessionalStatusType.QtlsAndSetMembershipId)
+                .WithStatus(RouteToProfessionalStatusStatus.Holds)
+                .WithHoldsFrom(existingQtlsDate)));
+
+        var newQtlsDate = QtsCutoff.AddYears(-1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(route);
+        Assert.Equal(QtsCutoff, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(e1 =>
+        {
+            var updatedQtlsEvent = Assert.IsType<RouteToProfessionalStatusUpdatedEvent>(e1);
+
+            Assert.Equal(
+                RouteToProfessionalStatusType.QtlsAndSetMembershipId,
+                updatedQtlsEvent.RouteToProfessionalStatus.RouteToProfessionalStatusTypeId
+            );
+            Assert.Equal(Clock.UtcNow, updatedQtlsEvent.CreatedUtc);
+            Assert.Equal(person.PersonId, updatedQtlsEvent.PersonId);
+            Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), updatedQtlsEvent.RaisedBy);
+            Assert.True(updatedQtlsEvent.Changes.HasFlag(RouteToProfessionalStatusUpdatedEventChanges.PersonQtsDate));
+        });
+    }
+
+    [Fact]
+    public async Task HandleAsync_existingQtls_SetsExistingRouteHoldsDateToCuttoff()
+    {
+        // Arrange
+        var existingQtlsDate = new DateOnly(2025, 11, 1);
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithRouteToProfessionalStatus(s => s
+                .WithRouteType(RouteToProfessionalStatusType.QtlsAndSetMembershipId)
+                .WithStatus(RouteToProfessionalStatusStatus.Holds)
+                .WithHoldsFrom(existingQtlsDate)));
+
+        var newQtlsDate = QtsCutoff.AddYears(-1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(route);
+        Assert.Equal(QtsCutoff, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(e1 =>
+        {
+            var updatedQtlsEvent = Assert.IsType<RouteToProfessionalStatusUpdatedEvent>(e1);
+
+            Assert.Equal(
+                RouteToProfessionalStatusType.QtlsAndSetMembershipId,
+                updatedQtlsEvent.RouteToProfessionalStatus.RouteToProfessionalStatusTypeId
+            );
+            Assert.Equal(Clock.UtcNow, updatedQtlsEvent.CreatedUtc);
+            Assert.Equal(person.PersonId, updatedQtlsEvent.PersonId);
+            Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), updatedQtlsEvent.RaisedBy);
+            Assert.True(updatedQtlsEvent.Changes.HasFlag(RouteToProfessionalStatusUpdatedEventChanges.PersonQtsDate));
+        });
+    }
+
+    [Fact]
+    public async Task HandleAsync_newQtls_WithExistingQtsWithHoldsDateBeforeCuttOffDoesNotUpdate()
+    {
+        // Arrange
+        var existingQtsDate = new DateOnly(2001, 4, 1);
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithRouteToProfessionalStatus(s => s
+                .WithRouteType(RouteToProfessionalStatusType.AssessmentOnlyRouteId)
+                .WithStatus(RouteToProfessionalStatusStatus.Holds)
+                .WithHoldsFrom(existingQtsDate)));
+
+        var newQtlsDate = QtsCutoff.AddYears(-1);
+        var command = new SetQtlsCommand(person.Trn, newQtlsDate);
+
+        LegacyEventObserver.Clear();
+
+        // Act
+        var result = await ExecuteCommandAsync(command);
+
+        // Assert
+        AssertSuccess(result);
+
+        var aorRoute = await GetRoute(person.PersonId, RouteToProfessionalStatusType.AssessmentOnlyRouteId);
+        var route = await GetQtlsRoute(person.PersonId);
+        Assert.NotNull(aorRoute);
+        Assert.Equal(existingQtsDate, aorRoute.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, aorRoute.Status);
+
+        Assert.NotNull(route);
+        Assert.Equal(QtsCutoff, route.HoldsFrom);
+        Assert.Equal(RouteToProfessionalStatusStatus.Holds, route.Status);
+        Assert.Equal(Clock.UtcNow, route.UpdatedOn);
+
+        LegacyEventObserver.AssertEventsSaved(e1 =>
+        {
+            var createdQtlsEventEvent = Assert.IsType<RouteToProfessionalStatusCreatedEvent>(e1);
+
+            Assert.Equal(
+                RouteToProfessionalStatusType.QtlsAndSetMembershipId,
+                createdQtlsEventEvent.RouteToProfessionalStatus.RouteToProfessionalStatusTypeId
+            );
+            Assert.Equal(Clock.UtcNow, createdQtlsEventEvent.CreatedUtc);
+            Assert.Equal(person.PersonId, createdQtlsEventEvent.PersonId);
+            Assert.Equal(CurrentUserProvider.GetCurrentApplicationUserId(), createdQtlsEventEvent.RaisedBy);
+            Assert.False(
+                createdQtlsEventEvent.Changes.HasFlag(RouteToProfessionalStatusCreatedEventChanges.PersonQtsDate));
+        });
+    }
+
+    private Task<RouteToProfessionalStatus?> GetRoute(Guid personId, Guid routeToProfessionalStatusTypeId) =>
+        WithDbContextAsync(dbContext =>
+            dbContext.Qualifications
+                .OfType<RouteToProfessionalStatus>()
+                .IgnoreQueryFilters()
+                .SingleOrDefaultAsync(q => q.PersonId == personId && q.RouteToProfessionalStatusTypeId == routeToProfessionalStatusTypeId));
 
     private Task<RouteToProfessionalStatus?> GetQtlsRoute(Guid personId) =>
         WithDbContextAsync(dbContext =>
