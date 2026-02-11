beca5b608161ba8d8ebecf9c1a9af95d3a4f00a7
James Gunn <james@gunn.io>
2026-02-11 11:03:12 +0000

    Populate PersonIds on OneLoginUser events

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/OneLoginUserCreatedEvent.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/OneLoginUserCreatedEvent.cs
@@ -3,6 +3,6 @@ namespace TeachingRecordSystem.Core.Events;
 public record OneLoginUserCreatedEvent : IEvent
 {
     public required Guid EventId { get; init; }
-    public Guid[] PersonIds => [];
+    public Guid[] PersonIds => IEvent.CoalescePersonIds(OneLoginUser.PersonId);
     public required EventModels.OneLoginUser OneLoginUser { get; init; }
 }
