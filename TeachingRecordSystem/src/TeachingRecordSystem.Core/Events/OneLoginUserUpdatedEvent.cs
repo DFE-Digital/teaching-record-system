72ec42153f301e6e82e35044c7bf570c87a27332
James Gunn <james@gunn.io>
2026-02-25 09:44:17 +0000

    Fix Changes deduction on OneLoginUserUpdatedEvent (#3157)
    Collection equality checks weren't completely correct before.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/OneLoginUserUpdatedEvent.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Events/OneLoginUserUpdatedEvent.cs
@@ -17,17 +17,48 @@ public record OneLoginUserUpdatedEvent : IEvent
         EventModels.OneLoginUser oldOneLoginUser,
         EventModels.OneLoginUser newOneLoginUser)
     {
+        var verifiedNamesChanged = oldOneLoginUser.VerifiedNames is not null && newOneLoginUser.VerifiedNames is not null
+            ? !oldOneLoginUser.VerifiedNames.SequenceEqual(newOneLoginUser.VerifiedNames, new StringArrayEqualityComparer())
+            : oldOneLoginUser.VerifiedNames != newOneLoginUser.VerifiedNames;
+
+        var verifiedDatesOfBirthChanged = oldOneLoginUser.VerifiedDatesOfBirth is not null && newOneLoginUser.VerifiedDatesOfBirth is not null
+            ? !oldOneLoginUser.VerifiedDatesOfBirth.SequenceEqual(newOneLoginUser.VerifiedDatesOfBirth)
+            : oldOneLoginUser.VerifiedDatesOfBirth != newOneLoginUser.VerifiedDatesOfBirth;
+
+        var matchedAttributesChanged = oldOneLoginUser.MatchedAttributes is not null && newOneLoginUser.MatchedAttributes is not null
+            ? !oldOneLoginUser.MatchedAttributes.SequenceEqual(newOneLoginUser.MatchedAttributes, new MatchedAttributeEqualityComparer())
+            : oldOneLoginUser.MatchedAttributes != newOneLoginUser.MatchedAttributes;
+
         return OneLoginUserUpdatedEventChanges.None |
             (oldOneLoginUser.EmailAddress != newOneLoginUser.EmailAddress ? OneLoginUserUpdatedEventChanges.EmailAddress : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.PersonId != newOneLoginUser.PersonId ? OneLoginUserUpdatedEventChanges.PersonId : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.VerifiedOn != newOneLoginUser.VerifiedOn ? OneLoginUserUpdatedEventChanges.VerifiedOn : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.VerificationRoute != newOneLoginUser.VerificationRoute ? OneLoginUserUpdatedEventChanges.VerificationRoute : OneLoginUserUpdatedEventChanges.None) |
-            (!oldOneLoginUser.VerifiedNames.SequenceEqual(newOneLoginUser.VerifiedNames, new StringArrayEqualityComparer()) ? OneLoginUserUpdatedEventChanges.VerifiedNames : OneLoginUserUpdatedEventChanges.None) |
-            (!oldOneLoginUser.VerifiedDatesOfBirth.SequenceEqual(newOneLoginUser.VerifiedDatesOfBirth) ? OneLoginUserUpdatedEventChanges.VerifiedDatesOfBirth : OneLoginUserUpdatedEventChanges.None) |
+            (verifiedNamesChanged ? OneLoginUserUpdatedEventChanges.VerifiedNames : OneLoginUserUpdatedEventChanges.None) |
+            (verifiedDatesOfBirthChanged ? OneLoginUserUpdatedEventChanges.VerifiedDatesOfBirth : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.MatchedOn != newOneLoginUser.MatchedOn ? OneLoginUserUpdatedEventChanges.MatchedOn : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.MatchRoute != newOneLoginUser.MatchRoute ? OneLoginUserUpdatedEventChanges.MatchRoute : OneLoginUserUpdatedEventChanges.None) |
-            (oldOneLoginUser.MatchedAttributes != newOneLoginUser.MatchedAttributes ? OneLoginUserUpdatedEventChanges.MatchedAttributes : OneLoginUserUpdatedEventChanges.None) |
+            (matchedAttributesChanged ? OneLoginUserUpdatedEventChanges.MatchedAttributes : OneLoginUserUpdatedEventChanges.None) |
             (oldOneLoginUser.VerifiedByApplicationUserId != newOneLoginUser.VerifiedByApplicationUserId ? OneLoginUserUpdatedEventChanges.VerifiedByApplicationUserId : OneLoginUserUpdatedEventChanges.None);
+    }
+
+    private class MatchedAttributeEqualityComparer : IEqualityComparer<KeyValuePair<PersonMatchedAttribute, string>>
+    {
+        public bool Equals(KeyValuePair<PersonMatchedAttribute, string> x, KeyValuePair<PersonMatchedAttribute, string> y)
+        {
+            return x.Key == y.Key && x.Value == y.Value;
+        }
+
+        public int GetHashCode(KeyValuePair<PersonMatchedAttribute, string> obj)
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + obj.Key.GetHashCode();
+                hash = hash * 23 + (obj.Value?.GetHashCode() ?? 0);
+                return hash;
+            }
+        }
     }
 
     private class StringArrayEqualityComparer : IEqualityComparer<string[]>
