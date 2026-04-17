fd0d0435e7f41deb87d8f170ee980c54ed67eb0a
James Gunn <james@gunn.io>
2026-04-17 13:25:45 +0100

    Remove ExportWorkforceDataJob (#3295)
    This is no longer needed.

--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/WorkforceData/Google/IStorageClientProvider.cs
+++
@@ -1,8 +0,0 @@
-using Google.Cloud.Storage.V1;
-
-namespace TeachingRecordSystem.Core.Services.WorkforceData.Google;
-
-public interface IStorageClientProvider
-{
-    ValueTask<StorageClient> GetStorageClientAsync();
-}
--- TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/WorkforceData/ServiceCollectionExtensions.cs
+++ TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/WorkforceData/ServiceCollectionExtensions.cs
@@ -1,7 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using TeachingRecordSystem.Core.Services.Establishments.Tps;
-using TeachingRecordSystem.Core.Services.WorkforceData.Google;
 
 namespace TeachingRecordSystem.Core.Services.WorkforceData;
 
@@ -13,9 +11,6 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<TpsCsvExtractFileImporter>();
         services.AddSingleton<TpsCsvExtractProcessor>();
         services.AddSingleton<TpsEstablishmentRefresher>();
-        services.AddSingleton<IConfigureOptions<WorkforceDataExportOptions>, WorkforceDataExportConfigureOptions>();
-        services.AddSingleton<IStorageClientProvider, OptionsStorageClientProvider>();
-        services.AddSingleton<WorkforceDataExporter>();
 
         return services;
     }
