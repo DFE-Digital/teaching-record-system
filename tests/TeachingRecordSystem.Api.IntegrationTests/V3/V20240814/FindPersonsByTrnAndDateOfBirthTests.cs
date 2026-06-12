145bfb48248cd495f32ce78c7a021a1d621f411a
James Gunn <james@gunn.io>
2026-06-12 15:17:12 +0100

    FindPerson endpoints

--- tests/TeachingRecordSystem.Api.IntegrationTests/V3/V20240814/FindPersonsByTrnAndDateOfBirthTests.cs
+++ tests/TeachingRecordSystem.Api.IntegrationTests/V3/V20240814/FindPersonsByTrnAndDateOfBirthTests.cs
@@ -1,230 +1,136 @@
-// using System.Diagnostics;
-//
-// namespace TeachingRecordSystem.Api.IntegrationTests.V3.V20240814;
-//
-// [Collection(nameof(DisableParallelization))]
-// public class FindPersonsByTrnAndDateOfBirthTests : TestBase
-// {
-//     public FindPersonsByTrnAndDateOfBirthTests(HostFixture hostFixture)
-//         : base(hostFixture)
-//     {
-//         XrmFakedContext.DeleteAllEntities<Contact>();
-//         SetCurrentApiClient([ApiRoles.GetPerson]);
-//     }
-//
-//     [Theory, RoleNamesData(except: [ApiRoles.GetPerson])]
-//     public async Task Get_ClientDoesNotHavePermission_ReturnsForbidden(string[] roles)
-//     {
-//         // Arrange
-//         SetCurrentApiClient(roles);
-//
-//         var dateOfBirth = new DateOnly(1990, 1, 1);
-//
-//         var person = await TestData.CreatePersonAsync(p => p
-//             .WithTrn()
-//             .WithDateOfBirth(dateOfBirth));
-//
-//         var request = new HttpRequestMessage(HttpMethod.Post, $"/v3/persons/find")
-//         {
-//             Content = JsonContent.Create(new
-//             {
-//                 persons = new[]
-//                 {
-//                     new
-//                     {
-//                         trn = person.Trn,
-//                         dateOfBirth = person.DateOfBirth
-//                     }
-//                 }
-//             })
-//         };
-//
-//         // Act
-//         var response = await GetHttpClientWithApiKey().SendAsync(request);
-//
-//         // Assert
-//         Assert.Equal(StatusCodes.Status403Forbidden, (int)response.StatusCode);
-//     }
-//
-//     [Fact]
-//     public async Task Get_TooManyPeopleRequested_ReturnsError()
-//     {
-//         // Arrange
-//         var limit = 500;
-//
-//         var request = new HttpRequestMessage(HttpMethod.Post, $"/v3/persons/find")
-//         {
-//             Content = JsonContent.Create(new
-//             {
-//                 persons = await Enumerable.Range(0, limit + 1)
-//                     .ToAsyncEnumerable()
-//                     .SelectAwait(async i => new
-//                     {
-//                         trn = "0000000",
-//                         dateOfBirth = new DateOnly(1990, 1, 1).AddDays(i)
-//                     })
-//                     .ToArrayAsync()
-//             })
-//         };
-//
-//         // Act
-//         var response = await GetHttpClientWithApiKey().SendAsync(request);
-//
-//         // Assert
-//         await AssertEx.JsonResponseHasValidationErrorForPropertyAsync(response, "persons", "Only 500 persons or less can be specified.");
-//     }
-//
-//     [Fact]
-//     public async Task Get_IncorrectDateOfBirth_DoesNotReturnRecord()
-//     {
-//         // Arrange
-//         var dateOfBirth = new DateOnly(1990, 1, 1);
-//
-//         var person = await TestData.CreatePersonAsync(p => p
-//             .WithTrn()
-//             .WithDateOfBirth(dateOfBirth)
-//             .WithQts(holdsFrom: new(2021, 7, 1))
-//             .WithEyts(eytsDate: new(2021, 8, 1), eytsStatusValue: "222"));
-//
-//         var request = new HttpRequestMessage(HttpMethod.Post, $"/v3/persons/find")
-//         {
-//             Content = JsonContent.Create(new
-//             {
-//                 persons = new[]
-//                 {
-//                     new
-//                     {
-//                         trn = person.Trn,
-//                         dateOfBirth = person.DateOfBirth.AddDays(1)
-//                     }
-//                 }
-//             })
-//         };
-//
-//         // Act
-//         var response = await GetHttpClientWithApiKey().SendAsync(request);
-//
-//         // Assert
-//         await AssertEx.JsonResponseEqualsAsync(
-//             response,
-//             new
-//             {
-//                 total = 0,
-//                 results = Array.Empty<object>()
-//             });
-//     }
-//
-//     [Fact]
-//     public async Task Get_ValidRequest_ReturnsMatchedRecord()
-//     {
-//         // Arrange
-//         var dateOfBirth = new DateOnly(1990, 1, 1);
-//
-//         var alertTypes = await TestData.ReferenceDataCache.GetAlertTypesAsync();
-//         var alertType = alertTypes.Where(at => Api.V3.Constants.LegacyExposableSanctionCodes.Contains(at.DqtSanctionCode)).RandomOne();
-//
-//         var person = await TestData.CreatePersonAsync(p => p
-//             .WithTrn()
-//             .WithDateOfBirth(dateOfBirth)
-//             .WithAlert(a => a.WithAlertTypeId(alertType.AlertTypeId).WithEndDate(null))
-//             .WithInductionStatus(i => i.WithStatus(InductionStatus.Passed).WithStartDate(new(2022, 1, 1)).WithCompletedDate(new DateOnly(2023, 1, 1)))
-//             .WithQts(holdsFrom: new(2021, 7, 1))
-//             .WithEyts(eytsDate: new(2021, 8, 1), eytsStatusValue: "222"));
-//
-//         var request = new HttpRequestMessage(HttpMethod.Post, $"/v3/persons/find")
-//         {
-//             Content = JsonContent.Create(new
-//             {
-//                 persons = new[]
-//                 {
-//                     new
-//                     {
-//                         trn = person.Trn,
-//                         dateOfBirth = person.DateOfBirth
-//                     }
-//                 }
-//             })
-//         };
-//
-//         // Act
-//         var response = await GetHttpClientWithApiKey().SendAsync(request);
-//
-//         // Assert
-//         await AssertEx.JsonResponseEqualsAsync(
-//             response,
-//             new
-//             {
-//                 total = 1,
-//                 results = new[]
-//                 {
-//                     new
-//                     {
-//                         trn = person.Trn,
-//                         dateOfBirth = person.DateOfBirth,
-//                         firstName = person.FirstName,
-//                         middleName = person.MiddleName ?? "",
-//                         lastName = person.LastName,
-//                         sanctions = new[]
-//                         {
-//                             new
-//                             {
-//                                 code = person.Alerts.Single().AlertType!.DqtSanctionCode,
-//                                 startDate = person.Alerts.Single().StartDate
-//                             }
-//                         },
-//                         previousNames = Array.Empty<object>(),
-//                         inductionStatus = (object?)new
-//                         {
-//                             status = "Pass",
-//                             statusDescription = "Pass"
-//                         },
-//                         qts = (object?)new
-//                         {
-//                             awarded = person.QtsDate,
-//                             statusDescription = "Qualified"
-//                         },
-//                         eyts = (object?)new
-//                         {
-//                             awarded = person.EytsDate,
-//                             statusDescription = "Early years professional status"
-//                         }
-//                     }
-//                 }
-//             });
-//     }
-//
-//     [Fact]
-//     public async Task Get_NonExposableSanctionCode_IsNotReturned()
-//     {
-//         // Arrange
-//         var dateOfBirth = new DateOnly(1990, 1, 1);
-//
-//         var sanctionCode = "A17";
-//         Debug.Assert(!Api.V3.Constants.LegacyExposableSanctionCodes.Contains(sanctionCode));
-//         var alertType = await TestData.ReferenceDataCache.GetAlertTypeByDqtSanctionCodeAsync(sanctionCode);
-//         var person = await TestData.CreatePersonAsync(p => p.WithTrn().WithDateOfBirth(dateOfBirth).WithAlert(a => a.WithAlertTypeId(alertType.AlertTypeId)));
-//
-//         var request = new HttpRequestMessage(HttpMethod.Post, $"/v3/persons/find")
-//         {
-//             Content = JsonContent.Create(new
-//             {
-//                 persons = new[]
-//                 {
-//                     new
-//                     {
-//                         trn = person.Trn,
-//                         dateOfBirth = person.DateOfBirth
-//                     }
-//                 }
-//             })
-//         };
-//
-//         // Act
-//         var response = await GetHttpClientWithApiKey().SendAsync(request);
-//
-//         // Assert
-//         var jsonResponse = await AssertEx.JsonResponseAsync(response);
-//         var responseSanctions = jsonResponse.RootElement.GetProperty("results").EnumerateArray().First().GetProperty("sanctions").EnumerateArray();
-//         Assert.Empty(responseSanctions);
-//     }
-// }
+namespace TeachingRecordSystem.Api.IntegrationTests.V3.V20240814;
+
+[ClearDbBeforeTest, Collection(nameof(DisableParallelization))]
+public class FindPersonsByTrnAndDateOfBirthTests : TestBase
+{
+    public FindPersonsByTrnAndDateOfBirthTests(HostFixture hostFixture) : base(hostFixture)
+    {
+        SetCurrentApiClient([ApiRoles.GetPerson]);
+    }
+
+    [Fact]
+    public async Task Post_UnauthenticatedRequest_ReturnsUnauthorized()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Post, "/v3/persons/find")
+        {
+            Content = CreateJsonContent(new { persons = new[] { new { trn = "1234567", dateOfBirth = new DateOnly(1990, 1, 1) } } })
+        };
+
+        // Act
+        var response = await GetHttpClient(Version).SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status401Unauthorized, (int)response.StatusCode);
+    }
+
+    [Theory, RoleNamesData(except: [ApiRoles.GetPerson])]
+    public async Task Post_ClientDoesNotHavePermission_ReturnsForbidden(string[] roles)
+    {
+        // Arrange
+        SetCurrentApiClient(roles);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/v3/persons/find")
+        {
+            Content = CreateJsonContent(new { persons = new[] { new { trn = "1234567", dateOfBirth = new DateOnly(1990, 1, 1) } } })
+        };
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status403Forbidden, (int)response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_NoMatch_ReturnsEmptyResults()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(HttpMethod.Post, "/v3/persons/find")
+        {
+            Content = CreateJsonContent(new { persons = new[] { new { trn = "1234567", dateOfBirth = new DateOnly(1990, 1, 1) } } })
+        };
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseEqualsAsync(
+            response,
+            new
+            {
+                total = 0,
+                results = Array.Empty<object>()
+            });
+    }
+
+    [Fact]
+    public async Task Post_ValidRequestWithMatch_ReturnsExpectedResult()
+    {
+        // Arrange
+        var person = await TestData.CreatePersonAsync();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/v3/persons/find")
+        {
+            Content = CreateJsonContent(new { persons = new[] { new { trn = person.Trn, dateOfBirth = person.DateOfBirth } } })
+        };
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseEqualsAsync(
+            response,
+            new
+            {
+                total = 1,
+                results = new[]
+                {
+                    new
+                    {
+                        trn = person.Trn,
+                        dateOfBirth = person.DateOfBirth,
+                        firstName = person.FirstName,
+                        middleName = person.MiddleName,
+                        lastName = person.LastName,
+                        sanctions = Array.Empty<object>(),
+                        previousNames = Array.Empty<object>(),
+                        inductionStatus = (object?)null,
+                        qts = (object?)null,
+                        eyts = (object?)null
+                    }
+                }
+            });
+    }
+
+    [Fact]
+    public async Task Post_PersonWithInduction_ReturnsExpectedInductionStatus()
+    {
+        // Arrange
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithInductionStatus(i => i
+                .WithStatus(InductionStatus.Passed)
+                .WithStartDate(new DateOnly(1996, 2, 3))
+                .WithCompletedDate(new DateOnly(1996, 6, 7))));
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "/v3/persons/find")
+        {
+            Content = CreateJsonContent(new { persons = new[] { new { trn = person.Trn, dateOfBirth = person.DateOfBirth } } })
+        };
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        var jsonResponse = await AssertEx.JsonResponseAsync(response);
+        var responseInductionStatus = jsonResponse.RootElement.GetProperty("results").EnumerateArray().Single().GetProperty("inductionStatus");
+
+        AssertEx.JsonObjectEquals(
+            new
+            {
+                status = "Pass",
+                statusDescription = "Pass"
+            },
+            responseInductionStatus);
+    }
+}
--- tests/TeachingRecordSystem.Api.IntegrationTests/V3/V20240920/FindPersonByLastNameAndDateOfBirthTests.cs
+++ tests/TeachingRecordSystem.Api.IntegrationTests/V3/V20240920/FindPersonByLastNameAndDateOfBirthTests.cs
@@ -9,6 +9,182 @@ public class FindPersonByLastNameAndDateOfBirthTests : TestBase
     }
 
     [Fact]
+    public async Task Get_UnauthenticatedRequest_ReturnsUnauthorized()
+    {
+        // Arrange
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            "/v3/persons?findBy=LastNameAndDateOfBirth&lastName=Smith&dateOfBirth=1990-01-01");
+
+        // Act
+        var response = await GetHttpClient(Version).SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status401Unauthorized, (int)response.StatusCode);
+    }
+
+    [Theory, RoleNamesData(except: [ApiRoles.GetPerson])]
+    public async Task Get_ClientDoesNotHavePermission_ReturnsForbidden(string[] roles)
+    {
+        // Arrange
+        SetCurrentApiClient(roles);
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            "/v3/persons?findBy=LastNameAndDateOfBirth&lastName=Smith&dateOfBirth=1990-01-01");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status403Forbidden, (int)response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("", "Invalid matching policy.")]
+    [InlineData("BadFindBy", "The value 'BadFindBy' is not valid for FindBy.")]
+    public async Task Get_InvalidFindBy_ReturnsError(string findBy, string expectedErrorMessage)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"/v3/persons?findBy={findBy}&lastName=Smith&dateOfBirth=1990-01-01");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseHasValidationErrorForPropertyAsync(response, "findBy", expectedErrorMessage);
+    }
+
+    [Theory]
+    [InlineData("", "1990-01-01", "lastName", "A value is required when findBy is 'LastNameAndDateOfBirth'.")]
+    [InlineData("Smith", "", "dateOfBirth", "A value is required when findBy is 'LastNameAndDateOfBirth'.")]
+    public async Task Get_MissingPropertiesForFindBy_ReturnsError(
+        string lastName,
+        string dateOfBirth,
+        string expectedErrorPropertyName,
+        string expectedErrorMessage)
+    {
+        // Arrange
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"/v3/persons?findBy=LastNameAndDateOfBirth&lastName={lastName}&dateOfBirth={dateOfBirth}");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseHasValidationErrorForPropertyAsync(response, expectedErrorPropertyName, expectedErrorMessage);
+    }
+
+    [Fact]
+    public async Task Get_NoMatch_ReturnsEmptyResults()
+    {
+        // Arrange
+        var findBy = "LastNameAndDateOfBirth";
+        var lastName = TestData.GenerateLastName();
+        var dateOfBirth = new DateOnly(1990, 1, 1);
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"/v3/persons?findBy={findBy}&lastName={lastName}&dateOfBirth={dateOfBirth:yyyy-MM-dd}");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseEqualsAsync(
+            response,
+            new
+            {
+                total = 0,
+                query = new { findBy, lastName, dateOfBirth },
+                results = Array.Empty<object>()
+            });
+    }
+
+    [Fact]
+    public async Task Get_ValidRequestWithMatch_ReturnsExpectedResult()
+    {
+        // Arrange
+        var findBy = "LastNameAndDateOfBirth";
+        var lastName = "Smith";
+        var dateOfBirth = new DateOnly(1990, 1, 1);
+
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithLastName(lastName)
+            .WithDateOfBirth(dateOfBirth));
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"/v3/persons?findBy={findBy}&lastName={lastName}&dateOfBirth={dateOfBirth:yyyy-MM-dd}");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        await AssertEx.JsonResponseEqualsAsync(
+            response,
+            new
+            {
+                total = 1,
+                query = new { findBy, lastName, dateOfBirth },
+                results = new[]
+                {
+                    new
+                    {
+                        trn = person.Trn,
+                        dateOfBirth = person.DateOfBirth,
+                        firstName = person.FirstName,
+                        middleName = person.MiddleName,
+                        lastName = person.LastName,
+                        previousNames = Array.Empty<object>(),
+                        inductionStatus = (object?)null,
+                        qts = (object?)null,
+                        eyts = (object?)null,
+                        alerts = Array.Empty<object>()
+                    }
+                }
+            });
+    }
+
+    [Fact]
+    public async Task Get_ValidRequestWithMatchOnPersonWithInduction_ReturnsExpectedInductionStatus()
+    {
+        // Arrange
+        var lastName = "Smith";
+        var dateOfBirth = new DateOnly(1990, 1, 1);
+
+        var person = await TestData.CreatePersonAsync(p => p
+            .WithLastName(lastName)
+            .WithDateOfBirth(dateOfBirth)
+            .WithInductionStatus(i => i
+                .WithStatus(InductionStatus.Passed)
+                .WithStartDate(new DateOnly(1996, 2, 3))
+                .WithCompletedDate(new DateOnly(1996, 6, 7))));
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"/v3/persons?findBy=LastNameAndDateOfBirth&lastName={lastName}&dateOfBirth={dateOfBirth:yyyy-MM-dd}");
+
+        // Act
+        var response = await GetHttpClientWithApiKey().SendAsync(request);
+
+        // Assert
+        var jsonResponse = await AssertEx.JsonResponseAsync(response);
+        var responseInductionStatus = jsonResponse.RootElement.GetProperty("results").EnumerateArray().Single().GetProperty("inductionStatus");
+
+        AssertEx.JsonObjectEquals(
+            new
+            {
+                status = "Pass",
+                statusDescription = "Pass"
+            },
+            responseInductionStatus);
+    }
+
+    [Fact]
     public async Task Get_ValidRequestWithMatchOnPersonWithAlerts_ReturnsExpectedAlertsContent()
     {
         // Arrange
