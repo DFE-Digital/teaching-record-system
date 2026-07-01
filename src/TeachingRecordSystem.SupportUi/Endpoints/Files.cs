e4ef7df75bbbfb100843e67a510427e7b023cbce
James Gunn <james@gunn.io>
2026-07-01 10:30:47 +0100

    Fix files endpoint to not send requests to arbitrary URLs

--- src/TeachingRecordSystem.SupportUi/Endpoints/Files.cs
+++ src/TeachingRecordSystem.SupportUi/Endpoints/Files.cs
@@ -1,5 +1,7 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
 
 namespace TeachingRecordSystem.SupportUi.Endpoints;
 
@@ -7,13 +9,37 @@ public static class Files
 {
     public static IEndpointConventionBuilder MapFiles(this IEndpointRouteBuilder endpoints)
     {
-        // This endpoint is used to proxy files from external URLs (e.g. Azure Blob Storage SAS URLs) to allow us to use the filename with extension in the URL
+        // This endpoint is used to proxy files to Azure Blob Storage SAS URLs to allow us to use the filename with extension in the URL
         // which helps the browser choose an appropriate app to open the file with (in particular for Outlook .msg files).
-        return endpoints.MapGet("/files/{fileName}", [Authorize] async (HttpClient httpClient, [FromRoute] string fileName, [FromQuery] string fileUrl) =>
-        {
-            var response = await httpClient.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead);
-            var stream = await response.Content.ReadAsStreamAsync();
-            return Results.Stream(stream, contentType: response.Content.Headers.ContentType?.ToString() ?? "application/octet-stream");
-        }).WithName("Files");
+        return endpoints.MapGet(
+            "/files/{fileName}",
+            [Authorize] async (
+                HttpClient httpClient,
+                [FromRoute] string fileName,
+                [FromQuery] string fileUrl,
+                [FromServices] IAzureClientFactory<BlobServiceClient> blobClientFactory) =>
+            {
+                var clients = new[]
+                {
+                    blobClientFactory.CreateClient("Default"),
+                    blobClientFactory.CreateClient("safe")
+                };
+
+                if (!clients.Any(c => fileUrl.StartsWith(c.Uri.AbsoluteUri)))
+                {
+                    return Results.BadRequest();
+                }
+
+                var response = await httpClient.GetAsync(fileUrl, HttpCompletionOption.ResponseHeadersRead);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return Results.StatusCode((int)response.StatusCode);
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync();
+                return Results.Stream(stream, contentType: response.Content.Headers.ContentType?.ToString() ?? "application/octet-stream");
+            })
+            .WithName("Files");
     }
 }
