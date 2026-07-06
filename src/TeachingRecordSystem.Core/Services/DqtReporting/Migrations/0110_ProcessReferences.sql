8fef51173da4bdff306b53e5f7473e060b94b474
James Gunn <james@gunn.io>
2026-07-06 10:16:21 +0100

    Add new columns to trs_processes and trs_process_events

---
+++ src/TeachingRecordSystem.Core/Services/DqtReporting/Migrations/0110_ProcessReferences.sql
@@ -0,0 +1,3 @@
+alter table trs_processes add one_login_user_subjects nvarchar(max)
+alter table trs_processes add support_task_references nvarchar(max)
+alter table trs_process_events add support_task_references nvarchar(max)
